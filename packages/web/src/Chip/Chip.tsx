959bb90196a2306c1737c24cb3d3558971ad8c77
Vicente Lyrio <vicentelyrio@gmail.com>
2019-10-09 11:19:01 -0300

    WIP(web): removing webpack and building with tsc
    affects: @natds/web

--- packages/web/src/Chip/Chip.tsx
+++ packages/web/src/Chip/Chip.tsx
@@ -1,34 +1,23 @@
-import React from 'react';
+import React, { FunctionComponent, useState } from 'react';
 
-interface IProps {
+export interface IProps {
   labelOn: string;
   labelOff: string;
 };
 
-interface IState {
-  isChecked: boolean;
+const Chip: FunctionComponent<IProps> = (props: IProps) => {
+  const [isOn, toggleCheck] = useState(false);
+
+  return (
+    <label>
+      <input
+        type="checkbox"
+        checked={isOn}
+        onChange={() => toggleCheck(!isOn)}
+      />
+      {isOn ? props.labelOn : props.labelOff}
+    </label>
+  );
 };
 
-export class Chip extends React.Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props);
-    this.state = { isChecked: false };
-  }
-
-  private onChange = () => {
-    this.setState({ isChecked: !this.state.isChecked });
-  }
-
-  public render() {
-    return (
-      <label>
-        <input
-          type="checkbox"
-          checked={this.state.isChecked}
-          onChange={this.onChange}
-        />
-        {this.state.isChecked ? this.props.labelOn : this.props.labelOff}
-      </label>
-    );
-  }
-};
+export default Chip;
