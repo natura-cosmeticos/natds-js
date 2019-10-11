117e43d244dbc56c4aae38c79159dc47fcbc3f2c
Vicente Lyrio <vicentelyrio@gmail.com>
2019-10-10 21:08:29 -0300

    WIP(docs): creating Design Tokens
    affects: @natds/docs

--- packages/docs/.storybook/config.js
+++ packages/docs/.storybook/config.js
@@ -1,3 +1,22 @@
-import { configure } from '@storybook/react';
+import { configure, addParameters } from '@storybook/react';
 
-configure(require.context('../stories', true, /\.tsx?$/), module);
+function mapCSSTokens(filename) {
+  return {
+    filename,
+    content: cssReq(filename).default
+  }
+}
+
+const stories = require.context('../stories', true, /\.tsx?$/);
+const cssReq = require.context('!!raw-loader!../styles', true, /.\.css$/);
+const cssTokenFiles = cssReq.keys().map(mapCSSTokens);
+
+addParameters({
+  designToken: {
+    files: {
+      css: cssTokenFiles
+    }
+  }
+});
+
+configure(stories, module);
