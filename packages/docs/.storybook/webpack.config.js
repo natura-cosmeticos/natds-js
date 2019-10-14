9efcd4c6fa9710cc17e313384563ac5badbb086b
Matheus Robert Lichtnow <matheusrobert8@gmail.com>
2019-10-14 17:57:45 -0300

    WIP(docs): added Chip component to native storybook
    affects: @natds/docs

--- packages/docs/.storybook/webpack.config.js
+++ packages/docs/.storybook/webpack.config.js
@@ -14,5 +14,31 @@ module.exports = ({ config }) => {
     'react-native': require.resolve('react-native-web'),
     '@storybook/react-native': require.resolve('@storybook/react')
   };
+
+  config.module.rules.push({
+    test: /\.js$/,
+    exclude: /node_modules[/\\](?!react-native-paper|react-native-vector-icons|react-native-safe-area-view)/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        // Disable reading babel configuration
+        babelrc: false,
+        configFile: false,
+
+        // The configration for compilation
+        presets: ['@babel/preset-react', '@babel/preset-flow'],
+        plugins: [
+          '@babel/plugin-proposal-class-properties',
+          '@babel/plugin-proposal-object-rest-spread'
+        ]
+      }
+    }
+  });
+
+  config.module.rules.push({
+    test: /\.(jpg|png|woff|woff2|eot|ttf|svg)$/,
+    loader: 'file-loader'
+  });
+
   return config;
 };
--- packages/docs/stories/Components/Chip.tsx
+++ packages/docs/stories/Components/Chip.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
+import { Platform } from 'react-native';
 import { Chip } from '@natds/web';
-import Button from '@natds/mobile/dist/Components/Button';
+import { Provider as PaperProvider } from 'react-native-paper';
+import NativeChip from '@natds/mobile/dist/Components/Chip/Chip';
 
 export default {
   title: 'Components|Chip',
@@ -11,4 +13,18 @@ export const WithDeleteAction = () => (
   <Chip label="Some Label" onDelete={() => {}} />
 );
 
-export const Native = () => <Button />;
+export const Native = () => (
+  <PaperProvider>
+    <React.Fragment>
+      {Platform.OS === 'web' ? (
+        <style type="text/css">{`
+    @font-face {
+      font-family: 'MaterialCommunityIcons';
+      src: url('${require('react-native-vector-icons/Fonts/MaterialCommunityIcons.ttf')}') format('truetype');
+    }
+  `}</style>
+      ) : null}
+      <NativeChip />
+    </React.Fragment>
+  </PaperProvider>
+);
