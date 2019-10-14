284d54b2cebf1f2a651489d644cfb8f1f601c0f9
Vagner A Saraiva <vagner@vizir.com.br>
2019-10-14 14:49:28 -0300

    chore(native): fixing chip component and including more tests
    affects: @natds/mobile

--- packages/mobile/src/__tests__/Chip.spec.tsx
+++ packages/mobile/src/__tests__/Chip.spec.tsx
@@ -2,38 +2,103 @@ import 'react-native';
 import React from 'react';
 import Chip from '../Components/Chip/Chip';
 
+import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
 
 describe('Chip', () => {
   describe('rendering', () => {
     test('should match to snapshot - Default', () => {
-      const component = shallow(<Chip />);
+      const component = renderer.create(<Chip children />).toJSON();
       expect(component).toMatchSnapshot('Default chip snapshot');
+    });
+
+    test('should match to snapshot - Icon', () => {
+      const component = renderer.create(<Chip children icon="add" />).toJSON();
+      expect(component).toMatchSnapshot('Icon chip snapshot');
+    });
+
+    test('should match to snapshot - Avatar', () => {
+      const component = renderer
+        .create(<Chip children avatar="avatar" />)
+        .toJSON();
+      expect(component).toMatchSnapshot('Avatar chip snapshot');
+    });
+
+    test('should match to snapshot - Selected', () => {
+      const component = renderer
+        .create(<Chip children selected={true} />)
+        .toJSON();
+      expect(component).toMatchSnapshot('Selected chip snapshot');
+    });
+
+    test('should match to snapshot - SelectedColor', () => {
+      const component = renderer
+        .create(<Chip children selectedColor="red" />)
+        .toJSON();
+      expect(component).toMatchSnapshot('SelectedColor chip snapshot');
+    });
+
+    test('should match to snapshot - Disabled', () => {
+      const component = renderer
+        .create(<Chip children disabled={true} />)
+        .toJSON();
+      expect(component).toMatchSnapshot('Disabled chip snapshot');
+    });
+
+    test('should match to snapshot - AccessibilityLabel', () => {
+      const component = renderer
+        .create(<Chip children accessibilityLabel="accessibilityLabel" />)
+        .toJSON();
+      expect(component).toMatchSnapshot('AccessibilityLabel chip snapshot');
+    });
+
+    test('should match to snapshot - TextStyle', () => {
+      const component = renderer
+        .create(<Chip children textStyle={{}} />)
+        .toJSON();
+      expect(component).toMatchSnapshot('TextStyle chip snapshot');
+    });
+
+    test('should match to snapshot - Style', () => {
+      const component = renderer
+        .create(<Chip children style="style" />)
+        .toJSON();
+      expect(component).toMatchSnapshot('Style chip snapshot');
+    });
+
+    test('should match to snapshot - Theme', () => {
+      const component = renderer.create(<Chip children theme={{}} />).toJSON();
+      expect(component).toMatchSnapshot('Theme chip snapshot');
+    });
+
+    test('should match to snapshot - TestID', () => {
+      const component = renderer
+        .create(<Chip children testID="testID" />)
+        .toJSON();
+      expect(component).toMatchSnapshot('TestID chip snapshot');
     });
   });
 
   describe('interaction', () => {
     test('should call onPress', () => {
       const mockOnPress = jest.fn();
-      const component = shallow(<Chip onPress={mockOnPress} />);
+      const component = shallow(<Chip children onPress={mockOnPress} />);
 
-      component.simulate('press');
+      component
+        .props()
+        .children()
+        .props.onPress();
 
       expect(mockOnPress).toHaveBeenCalled();
     });
-    test('should call onLongPress', () => {
-      const mockOnLongPress = jest.fn();
-      const component = shallow(<Chip onLongPress={mockOnLongPress} />);
-
-      component.simulate('longPress');
-
-      expect(mockOnLongPress).toHaveBeenCalled();
-    });
     test('should call onClose', () => {
       const mockOnClose = jest.fn();
-      const component = shallow(<Chip onClose={mockOnClose} />);
+      const component = shallow(<Chip children onClose={mockOnClose} />);
 
-      component.simulate('close');
+      component
+        .props()
+        .children()
+        .props.onClose();
 
       expect(mockOnClose).toHaveBeenCalled();
     });
