ed5905580b95d4a62e38e5e22c4451273fa17393
Vicente Lyrio <vicentelyrio@gmail.com>
2019-10-21 10:09:56 -0300

    chore(docs): including Viewports addon
    affects: @naturacosmeticos/natds-docs

--- packages/docs/.storybook/config.js
+++ packages/docs/.storybook/config.js
@@ -1,10 +1,18 @@
-import { configure, addDecorator } from '@storybook/react';
+import { configure, addDecorator, addParameters } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+
+addParameters({
+  viewport: {
+    viewports: INITIAL_VIEWPORTS
+  }
+});
+
+addDecorator(withKnobs)
 
 const tokens = require.context('../src/tokens', true, /\.tsx?$/);
 const web = require.context('../src/web', true, /\.tsx?$/);
 const mobile = require.context('../src/mobile', true, /\.tsx?$/);
 
-addDecorator(withKnobs)
 
 configure([tokens, web, mobile], module);
