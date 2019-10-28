29e43b15f6faf6be529f812b79c80c1e6236f31b
Matheus Robert Lichtnow <matheusrobert8@gmail.com>
2019-10-28 09:10:35 -0300

    chore(provider): added tests to web provider
    affects: @naturacosmeticos/natds-web

---
+++ packages/web/src/__tests__/Provider.spec.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import * as themes from '../Themes';
+import { Provider } from '../Provider';
+import { createMuiTheme } from '@material-ui/core';
+
+describe('Provider component', () => {
+  test('It should have the natura theme', () => {
+    const provider = mount(<Provider >Mocked</Provider>);
+
+    expect(provider.find('ThemeProvider').prop('theme')).toStrictEqual(themes.natura);
+  });
+
+  test('It should have the provided theme', () => {
+    const mockedTheme = createMuiTheme();
+    mockedTheme.palette.primary.dark = 'MOCKED';
+
+    const provider = mount(<Provider theme={mockedTheme}>Mocked</Provider>);
+
+    expect(provider.find('ThemeProvider').prop('theme')).toStrictEqual(mockedTheme);
+  });
+});
