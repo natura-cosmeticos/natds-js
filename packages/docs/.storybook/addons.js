3382d215b850f298f52e2984c6b4bd13c32b6207
Vicente Lyrio <vicentelyrio@gmail.com>
2019-10-30 09:55:51 -0300

    chore(docs): creating Versions Addon
    affects: @naturacosmeticos/natds-docs

--- packages/docs/.storybook/addons.js
+++ packages/docs/.storybook/addons.js
@@ -4,3 +4,5 @@ import '@storybook/addon-a11y/register';
 import '@storybook/addon-jest/register';
 import '@storybook/addon-knobs/register';
 import '@storybook/addon-backgrounds/register';
+import '@storybook/addon-viewport/register';
+import '../addons/versions/register';
---
+++ packages/docs/addons/versions/register.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react';
+import { addons, types } from '@storybook/addons';
+import { useParameter } from '@storybook/api';
+import { AddonPanel } from '@storybook/components';
+import { RenderOptions } from '@storybook/api/dist/modules/addons';
+
+import './styles.css';
+import readConfig from './readConfig';
+
+const ADDON_ID = 'version';
+const PANEL_ID = `${ADDON_ID}/panel`;
+const TITLE = 'Versions';
+
+function errorReadingFile() {
+  console.log('Unable to get Package version list');
+}
+
+function Panel() {
+  const [versions, setVersions] = useState([]);
+  const isLocal = window.location.hostname === 'localhost';
+
+  useEffect(() => {
+    if (!isLocal && !versions.length) {
+      console.log(isLocal)
+      readConfig().then(setVersions).catch(errorReadingFile);
+    }
+  });
+
+  return (
+    <div className="versions">
+      {isLocal ? <LocalhostWarning /> : versions.map(createVersionType)}
+    </div>
+  )
+}
+
+function LocalhostWarning() {
+  return (
+    <h4 className="versions__warning">Versions don't work in localhost environments</h4>
+  )
+}
+
+function createVersionType({ versions, name }, key) {
+  return (
+    <div className="versions__type" key={key}>
+      <h4 className="versions__type__name">{name}</h4>
+      {versions.map(createVersionsLinks, { name })}
+    </div>
+  )
+}
+
+function createVersionsLinks(version, key) {
+  const { name } = this
+
+  return (
+    <button
+      onClick={() => changeVersion(version, name)}
+      className="versions__type__button"
+      key={key}>
+      {version}
+    </button>
+  )
+}
+
+function changeVersion(version, name) {
+  const { origin, search } = window.location;
+  const newPath = `${origin}/${name}/${version}/${search}`
+
+  window.location.href = newPath;
+}
+
+addons.register(ADDON_ID, () => {
+  addons.add(PANEL_ID, {
+    type: types.PANEL,
+    title: TITLE,
+    render: ({ active, key }) => (
+      <AddonPanel active={active} key={key}>
+        <Panel />
+      </AddonPanel>
+    )
+  })
+})
