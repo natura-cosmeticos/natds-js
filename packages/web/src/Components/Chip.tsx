ee7eb10054d26dd8aa9352f44db8411cc67f449a
Vicente Lyrio <vicentelyrio@gmail.com>
2019-10-31 14:47:20 -0300

    chore(docs): fixing types
    affects: @naturacosmeticos/natds-docs, @naturacosmeticos/natds-web

--- packages/web/src/Components/Chip.tsx
+++ packages/web/src/Components/Chip.tsx
@@ -1,38 +1,9 @@
 import React, { FunctionComponent } from 'react';
 
-import MaterialChip from '@material-ui/core/Chip';
+import MaterialChip, { ChipProps } from '@material-ui/core/Chip';
 import { withTheme } from '@material-ui/styles';
 
-enum Colors {
-  default = 'default',
-  primary = 'primary',
-  secondary = 'secondary'
-}
-
-enum Variants {
-  default = 'default',
-  outlined = 'outlined'
-}
-
-enum Sizes {
-  small = 'small',
-  medium = 'medium'
-}
-
-export interface IProps {
-  avatar?: React.ReactElement;
-  clickable?: boolean;
-  color?: Colors;
-  deleteIcon?: React.ReactElement;
-  disabled?: boolean;
-  icon?: React.ReactElement;
-  label?: React.ReactNode;
-  onDelete?: React.EventHandler<any>;
-  size?: Sizes;
-  variant?: Variants;
-}
-
-const Chip: FunctionComponent<IProps> = (props: IProps) => {
+const Chip: FunctionComponent<ChipProps> = (props: ChipProps) => {
   return (
     <MaterialChip {...props} />
   );
