d51698edf93307860557ad51ac3a2d4de1f6884e
Matheus Robert Lichtnow <matheusrobert8@gmail.com>
2019-10-31 16:00:57 -0300

    chore(storybook): added redirects to netlify nginx and fixed build scripts
    affects: @naturacosmeticos/natds-docs

--- scripts/bump-alpha-version.sh
+++ scripts/bump-alpha-version.sh
@@ -16,7 +16,7 @@ VERSION_WO_ALPHA=`node scripts/helpers/removeAlphaFromVersion.js ${NEW_VERSION}`
 git remote rm origin
 # This remote is using a contributor account and an OAuth key from github
 # Adapted from https://stackoverflow.com/questions/23277391/how-to-publish-to-github-pages-from-travis-ci
-git remote add origin https://robertLichtnow:$GITHUB_API_KEY@github.com/natura-cosmeticos/natds
+git remote add origin https://$GITHUB_API_USER:$GITHUB_API_KEY@github.com/natura-cosmeticos/natds
 
 git fetch
 
