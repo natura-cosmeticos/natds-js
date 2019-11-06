ea807701144255d031d338fa0d3ab366971fc58e
Vicente Lyrio <vicentelyrio@gmail.com>
2019-11-06 15:44:02 -0300

    chore(web): creating base Button component
    affects: @naturacosmeticos/natds-web

--- packages/web/src/index.ts
+++ packages/web/src/index.ts
@@ -2,6 +2,7 @@ export { Provider } from './Provider';
 import { themes as webThemes } from './Themes';
 
 export { default as Chip } from './Components/Chip';
+export { default as Button } from './Components/Button';
 
 export const themes = {
   ...webThemes
