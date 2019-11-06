aba9976c6801076464a5c784642435bfdb62c73b
Vicente Lyrio <vicentelyrio@gmail.com>
2019-11-06 18:35:10 -0300

    chore(web): creating IconButton Component
    affects: @naturacosmeticos/natds-web

--- packages/web/src/index.ts
+++ packages/web/src/index.ts
@@ -6,6 +6,7 @@ export { default as Chip } from './Components/Chip';
 export { default as Button } from './Components/Button';
 export { default as GridContainer } from './Components/GridContainer';
 export { default as GridItem } from './Components/GridItem';
+export { default as IconButton } from './Components/IconButton';
 
 export const themes = {
   ...webThemes
