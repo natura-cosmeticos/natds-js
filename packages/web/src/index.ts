0eb25116cf568fdc92b7edf7502b339ffb5e525b
Vicente Lyrio <vicentelyrio@gmail.com>
2019-11-06 21:52:26 -0300

    chore(web): creating ButtonGroup component
    affects: @naturacosmeticos/natds-web

--- packages/web/src/index.ts
+++ packages/web/src/index.ts
@@ -7,6 +7,7 @@ import { themes as webThemes } from './Themes';
 export { default as Chip } from './Components/Chip';
 export { default as Button } from './Components/Button';
 export { default as IconButton } from './Components/IconButton';
+export { default as ButtonGroup } from './Components/ButtonGroup';
 
 export const themes = {
   ...webThemes,
