ca0218a501ebd6889cf0a4758ed7720fdfda7caf
Matheus Robert Lichtnow <matheusrobert8@gmail.com>
2019-11-07 16:21:06 -0300

    WIP(font-sizes): added font sizes to the mobile package theme
    affects: @naturacosmeticos/natds-rn

--- packages/mobile/src/__tests__/Themes/index.spec.ts
+++ packages/mobile/src/__tests__/Themes/index.spec.ts
@@ -6,7 +6,59 @@ const iOSTypography: any = {
   fontWeightLight: 300,
   fontWeightRegular: 400,
   fontWeightMedium: 500,
-  fontWeightBold: 700
+  fontWeightBold: 700,
+  h1: {
+    fontSize: "96px",
+    lineHeight: 1
+  },
+  h2: {
+    fontSize: "60px",
+    lineHeight: 1
+  },
+  h3: {
+    fontSize: "48px",
+    lineHeight: 1
+  },
+  h4: {
+    fontSize: "34px",
+    lineHeight: 1
+  },
+  h5: {
+    fontSize: "24px",
+    lineHeight: 1
+  },
+  h6: {
+    fontSize: "20px",
+    lineHeight: 1
+  },
+  subtitle1: {
+    fontSize: "16px",
+    lineHeight: 1
+  },
+  subtitle2: {
+    fontSize: "14px",
+    lineHeight: 1
+  },
+  body1: {
+    fontSize: "16px",
+    lineHeight: 1
+  },
+  body2: {
+    fontSize: "14px",
+    lineHeight: 1
+  },
+  button: {
+    fontSize: "14px",
+    lineHeight: 1
+  },
+  caption: {
+    fontSize: "12px",
+    lineHeight: 1
+  },
+  overline: {
+    fontSize: "12px",
+    lineHeight: 1
+  }
 };
 
 const androidTypography: any = {
@@ -15,7 +67,59 @@ const androidTypography: any = {
   fontWeightLight: 300,
   fontWeightRegular: 400,
   fontWeightMedium: 500,
-  fontWeightBold: 700
+  fontWeightBold: 700,
+  h1: {
+    fontSize: "96px",
+    lineHeight: 1
+  },
+  h2: {
+    fontSize: "60px",
+    lineHeight: 1
+  },
+  h3: {
+    fontSize: "48px",
+    lineHeight: 1
+  },
+  h4: {
+    fontSize: "34px",
+    lineHeight: 1
+  },
+  h5: {
+    fontSize: "24px",
+    lineHeight: 1
+  },
+  h6: {
+    fontSize: "20px",
+    lineHeight: 1
+  },
+  subtitle1: {
+    fontSize: "16px",
+    lineHeight: 1
+  },
+  subtitle2: {
+    fontSize: "14px",
+    lineHeight: 1
+  },
+  body1: {
+    fontSize: "16px",
+    lineHeight: 1
+  },
+  body2: {
+    fontSize: "14px",
+    lineHeight: 1
+  },
+  button: {
+    fontSize: "14px",
+    lineHeight: 1
+  },
+  caption: {
+    fontSize: "12px",
+    lineHeight: 1
+  },
+  overline: {
+    fontSize: "12px",
+    lineHeight: 1
+  }
 };
 
 describe('Importing themes', () => {
