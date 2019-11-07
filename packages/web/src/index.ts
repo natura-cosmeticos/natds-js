59d57034d5623d5ad2d364e57dc705d789efbb8d
Vicente Lyrio <vicentelyrio@gmail.com>
2019-11-07 11:55:25 -0300

    chore(web): fixing tests for ButtonGroup component
    affects: @naturacosmeticos/natds-web

--- packages/web/src/index.ts
+++ packages/web/src/index.ts
@@ -1,10 +1,9 @@
-export { default as Chip } from './Components/Chip';
-export { default as GridContainer } from './Components/GridContainer';
-export { default as GridItem } from './Components/GridItem';
 export { Provider } from './Provider';
 import { themes as webThemes } from './Themes';
 
 export { default as Chip } from './Components/Chip';
+export { default as GridContainer } from './Components/GridContainer';
+export { default as GridItem } from './Components/GridItem';
 export { default as Button } from './Components/Button';
 export { default as IconButton } from './Components/IconButton';
 export { default as ButtonGroup } from './Components/ButtonGroup';
