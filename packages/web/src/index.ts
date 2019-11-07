a491d9f1289ea68182f9b8515edaae58e25ecf63
Vicente Lyrio <vicentelyrio@gmail.com>
2019-11-06 22:16:11 -0300

    chore(docs): creating stories for Web Buttons
    affects: @naturacosmeticos/natds-docs

--- packages/web/src/index.ts
+++ packages/web/src/index.ts
@@ -1,13 +1,11 @@
 import { themes as webThemes } from './Themes';
 
 export { Provider } from './Provider';
+export const themes = { ...webThemes };
+
 export { default as Button } from './Components/Button';
 export { default as ButtonGroup } from './Components/ButtonGroup';
 export { default as Chip } from './Components/Chip';
 export { default as GridContainer } from './Components/GridContainer';
 export { default as GridItem } from './Components/GridItem';
 export { default as IconButton } from './Components/IconButton';
-
-export const themes = {
-  ...webThemes
-};
