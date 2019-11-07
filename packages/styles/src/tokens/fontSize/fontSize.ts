deec230c4bb35f86d409002abcf627290e09b7fe
Matheus Robert Lichtnow <matheusrobert8@gmail.com>
2019-11-07 15:37:22 -0300

    WIP(font-sizes): added font size to the typography namespace in the themes
    affects: @naturacosmeticos/natds-styles

---
+++ packages/styles/src/tokens/fontSize/fontSize.ts
@@ -0,0 +1,60 @@
+import { IFont } from "./IFont";
+
+export const fontSize = {
+  h1: {
+    fontSize: "96px",
+    lineHeight: 1
+  } as IFont,
+  h2: {
+    fontSize: "60px",
+    lineHeight: 1
+  } as IFont,
+  h3: {
+    fontSize: "48px",
+    lineHeight: 1
+  } as IFont,
+  h4: {
+    fontSize: "34px",
+    lineHeight: 1
+  } as IFont,
+  h5: {
+    fontSize: "24px",
+    lineHeight: 1
+  } as IFont,
+  h6: {
+    fontSize: "20px",
+    lineHeight: 1
+  } as IFont,
+  subtitle1: {
+    fontSize: "16px",
+    lineHeight: 1
+  } as IFont,
+  subtitle2: {
+    fontSize: "14px",
+    lineHeight: 1
+  } as IFont,
+  body1: {
+    fontSize: "16px",
+    lineHeight: 1
+  } as IFont,
+  body2: {
+    fontSize: "14px",
+    lineHeight: 1
+  } as IFont,
+  button: {
+    fontSize: "14px",
+    lineHeight: 1
+  } as IFont,
+  caption: {
+    fontSize: "12px",
+    lineHeight: 1
+  } as IFont,
+  overline: {
+    fontSize: "12px",
+    lineHeight: 1
+  } as IFont,
+  support: {
+    fontSize: "10px",
+    lineHeight: 1
+  } as IFont
+};
