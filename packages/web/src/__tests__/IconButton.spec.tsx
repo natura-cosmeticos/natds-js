97465d09dd9cf3d728410e8bf1b36beea37929ab
Vicente Lyrio <vicentelyrio@gmail.com>
2019-11-07 11:29:17 -0300

    chore(web): removing dependency from MaterialUI Icons from web and mocking tests
    affects: @naturacosmeticos/natds-web

--- packages/web/src/__tests__/IconButton.spec.tsx
+++ packages/web/src/__tests__/IconButton.spec.tsx
@@ -3,19 +3,24 @@ import { shallow } from 'enzyme';
 import { expect } from 'chai';
 import { assert, spy } from 'sinon';
 import renderer from 'react-test-renderer';
-import Delete from '@material-ui/icons/Delete';
 
 import IconButton from '../Components/IconButton';
+
+const MockedIcon = () => {
+  return (
+    <i />
+  );
+};
 
 describe('IconButton component', () => {
   describe('rendering sizes', () => {
     test('should match snapshot - Size Small', () => {
-      const component = renderer.create(<IconButton size="small" children={<Delete />} />).toJSON();
+      const component = renderer.create(<IconButton size="small" children={<MockedIcon />} />).toJSON();
       expect(component).matchSnapshot('Small IconButton snapshot');
     });
 
     test('should match snapshot - Size Medium', () => {
-      const component = renderer.create(<IconButton size="medium" children={<Delete />} />).toJSON();
+      const component = renderer.create(<IconButton size="medium" children={<MockedIcon />} />).toJSON();
       expect(component).matchSnapshot('Medium IconButton snapshot');
     });
   });
@@ -23,7 +28,7 @@ describe('IconButton component', () => {
   describe('interaction', () => {
     test('should call onClick', () => {
       const mockOnClick = spy();
-      const component = shallow(<IconButton onClick={mockOnClick} children={<Delete />} />);
+      const component = shallow(<IconButton onClick={mockOnClick} children={<MockedIcon />} />);
 
       component.simulate('click');
 
