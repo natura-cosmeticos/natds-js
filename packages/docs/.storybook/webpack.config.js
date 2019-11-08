65b4e7d32c09a2cd4a2c6cd82384000bd0af681a
Vicente Lyrio <vicentelyrio@gmail.com>
2019-11-08 08:01:32 -0300

    chore(docs): making Storybook work with mdx files
    affects: @naturacosmeticos/natds-docs

--- packages/docs/.storybook/webpack.config.js
+++ packages/docs/.storybook/webpack.config.js
@@ -78,7 +78,7 @@ module.exports = ({ config }) => {
   });
 
   config.module.rules.push({
-    test: /\.story.(ts|js)x?$/,
+    test: /\.(stories|story).(ts|js)x?$/,
     exclude: [/node_modules/],
     loaders: [
       {
@@ -89,15 +89,8 @@ module.exports = ({ config }) => {
     enforce: 'pre'
   });
 
-  config.resolve.extensions.push('.ts', '.tsx', '.js', '.jsx', '.mdx');
-
-  config.resolve.alias = {
-    'react-native': require.resolve('react-native-web'),
-    '@storybook/react-native': require.resolve('@storybook/react'),
-  };
-
   config.module.rules.push({
-    test: /\.mdx$/,
+    test: /\.(stories|story).mdx$/,
     use: [
       {
         loader: require.resolve("babel-loader"),
@@ -117,6 +110,33 @@ module.exports = ({ config }) => {
       }
     ]
   });
+
+  config.module.rules.push({
+    test: /\.mdx$/,
+    exclude: /\.(stories|story).mdx$/,
+    use: [
+      {
+        loader: require.resolve("babel-loader"),
+        options: {
+          presets: [
+            require("@babel/preset-typescript").default,
+            require("@babel/preset-react").default
+          ],
+          plugins: ['@babel/plugin-transform-react-jsx']
+        }
+      },
+      {
+        loader: '@mdx-js/loader'
+      }
+    ]
+  });
+
+  config.resolve.extensions.push('.ts', '.tsx', '.js', '.jsx', '.mdx');
+
+  config.resolve.alias = {
+    'react-native': require.resolve('react-native-web'),
+    '@storybook/react-native': require.resolve('@storybook/react'),
+  };
 
   config.resolve.plugins = [new TsconfigPathsPlugin()];
 
