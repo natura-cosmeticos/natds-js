ad18196f59d88bcf610802bd6f28656491ce762b
Vicente Lyrio <vicentelyrio@gmail.com>
2019-11-08 14:46:04 -0300

    chore(docs): creating mdx for Buttons
    affects: @naturacosmeticos/natds-docs, @naturacosmeticos/natds-web

--- packages/web/src/index.ts
+++ packages/web/src/index.ts
@@ -2,6 +2,7 @@ import { themes as webThemes } from './Themes';
 
 export const themes = { ...webThemes };
 export { Provider } from './Provider';
+
 export { default as Button } from './Components/Button';
 export { default as ButtonGroup } from './Components/ButtonGroup';
 export { default as Chip } from './Components/Chip';
