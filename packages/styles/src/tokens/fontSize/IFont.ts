990ca80699b3c5069569cf9eb28c6d3433dc08e0
Matheus Robert Lichtnow <matheusrobert8@gmail.com>
2019-11-11 10:05:46 -0300

    chore(font): removed line height from theme
    affects: @naturacosmeticos/natds-rn, @naturacosmeticos/natds-styles, @naturacosmeticos/natds-web

--- packages/styles/src/tokens/fontSize/IFont.ts
+++ packages/styles/src/tokens/fontSize/IFont.ts
@@ -1,4 +1,3 @@
 export interface IFont {
   fontSize: string;
-  lineHeight: number;
 }
--- packages/styles/src/tokens/fontSize/fontSize.ts
+++ packages/styles/src/tokens/fontSize/fontSize.ts
@@ -2,59 +2,45 @@ import { IFont } from "./IFont";
 
 export const fontSize = {
   h1: {
-    fontSize: "96px",
-    lineHeight: 1
+    fontSize: "96px"
   } as IFont,
   h2: {
-    fontSize: "60px",
-    lineHeight: 1
+    fontSize: "60px"
   } as IFont,
   h3: {
-    fontSize: "48px",
-    lineHeight: 1
+    fontSize: "48px"
   } as IFont,
   h4: {
-    fontSize: "34px",
-    lineHeight: 1
+    fontSize: "34px"
   } as IFont,
   h5: {
-    fontSize: "24px",
-    lineHeight: 1
+    fontSize: "24px"
   } as IFont,
   h6: {
-    fontSize: "20px",
-    lineHeight: 1
+    fontSize: "20px"
   } as IFont,
   subtitle1: {
-    fontSize: "16px",
-    lineHeight: 1
+    fontSize: "16px"
   } as IFont,
   subtitle2: {
-    fontSize: "14px",
-    lineHeight: 1
+    fontSize: "14px"
   } as IFont,
   body1: {
-    fontSize: "16px",
-    lineHeight: 1
+    fontSize: "16px"
   } as IFont,
   body2: {
-    fontSize: "14px",
-    lineHeight: 1
+    fontSize: "14px"
   } as IFont,
   button: {
-    fontSize: "14px",
-    lineHeight: 1
+    fontSize: "14px"
   } as IFont,
   caption: {
-    fontSize: "12px",
-    lineHeight: 1
+    fontSize: "12px"
   } as IFont,
   overline: {
-    fontSize: "12px",
-    lineHeight: 1
+    fontSize: "12px"
   } as IFont,
   support: {
-    fontSize: "10px",
-    lineHeight: 1
+    fontSize: "10px"
   } as IFont
 };
