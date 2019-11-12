768ce13e324ceb48121e7a074b1422fdc8d96b53
Matheus Robert Lichtnow <matheusrobert8@gmail.com>
2019-11-12 10:12:25 -0300

    chore(typography): added missing props for web Typography and it tests
    affects: @naturacosmeticos/natds-web

--- packages/web/src/Components/Typography.tsx
+++ packages/web/src/Components/Typography.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Typography as MaterialTypography } from '@material-ui/core';
 import { withTheme } from '@material-ui/styles';
+import { TypographyClassKey } from '@material-ui/core/Typography';
 
 type TypographyVariant =
   | 'h1'
@@ -17,11 +18,43 @@ type TypographyVariant =
   | 'caption'
   | 'overline';
 
+type TypographyAlign =
+  | 'inherit'
+  | 'left'
+  | 'center'
+  | 'right'
+  | 'justify';
+
+type TypographyColor =
+  | 'initial'
+  | 'inherit'
+  | 'primary'
+  | 'secondary'
+  | 'textPrimary'
+  | 'textSecondary'
+  | 'error';
+
 interface ITypographyProps {
   /**
    * Variant to be used with the theme definitions for typography, defaulting to 'body1'
    */
   variant?: TypographyVariant;
+  /**
+   * Set the text-align on the component. Defaults to 'inherit'
+   */
+  align?: TypographyAlign;
+  /**
+   * Override or extend the styles applied to the component
+   */
+  classes?: Partial<Record<TypographyClassKey, string>>;
+  /**
+   * The color of the component. It supports those theme colors that make sense for this component
+   */
+  color?: TypographyColor;
+  /**
+   * If true, the text will not wrap, but instead will truncate with an ellipsis
+   */
+  noWrap?: boolean;
   /**
    * Children nodes to apply the typography
    */
@@ -30,8 +63,22 @@ interface ITypographyProps {
 
 const Typography:React.FunctionComponent<ITypographyProps> = (props:ITypographyProps) => {
   const variant = props.variant ? props.variant : 'body1';
+  const align = props.align ? props.align : 'inherit';
+  const classes = props.classes ? props.classes : {};
+  const color = props.color ? props.color : undefined;
+  const noWrap = props.noWrap === undefined || props.noWrap === null ? false : props.noWrap;
 
-  return (<MaterialTypography variant={variant}>{props.children}</MaterialTypography>);
+  return (
+    <MaterialTypography
+      variant={variant}
+      align={align}
+      classes={classes}
+      color={color}
+      noWrap={noWrap}
+    >
+      {props.children}
+    </MaterialTypography>
+  );
 };
 
 export default withTheme(Typography);
--- packages/web/src/__tests__/Typography.spec.tsx
+++ packages/web/src/__tests__/Typography.spec.tsx
@@ -5,20 +5,51 @@ import { Typography } from '../index';
 
 describe('Typography Component', () => {
   test('It should use the default variant and pass children forward', () => {
-    const component = mount(<Typography>teste</Typography>);
+    const component = mount(<Typography>test</Typography>);
 
     const materialTypography = component.find('ForwardRef(Typography)');
 
     expect(materialTypography.prop('variant')).toBe('body1');
-    expect(materialTypography.prop('children')).toBe('teste');
+    expect(materialTypography.prop('children')).toBe('test');
   });
 
-  test('It should use provided variant and pass children forward', () => {
-    const component = mount(<Typography variant="h1">teste</Typography>);
+  test('It should use provided variant', () => {
+    const component = mount(<Typography variant="h1">test</Typography>);
 
     const materialTypography = component.find('ForwardRef(Typography)');
 
     expect(materialTypography.prop('variant')).toBe('h1');
-    expect(materialTypography.prop('children')).toBe('teste');
+  });
+
+  test('It should use provided align', () => {
+    const component = mount(<Typography align="center">test</Typography>);
+
+    const materialTypography = component.find('ForwardRef(Typography)');
+
+    expect(materialTypography.prop('align')).toBe('center');
+  });
+
+  test('It should use provided classes', () => {
+    const component = mount(<Typography classes={{h1: 'teste'}} variant="h1">test</Typography>);
+
+    const materialTypography = component.find('h1');
+
+    expect(materialTypography.prop('className')).toContain('teste');
+  });
+
+  test('It should use provided color', () => {
+    const component = mount(<Typography color="secondary">test</Typography>);
+
+    const materialTypography = component.find('ForwardRef(Typography)');
+
+    expect(materialTypography.prop('color')).toBe('secondary');
+  });
+
+  test('It should use provided noWrap', () => {
+    const component = mount(<Typography noWrap={true}>test</Typography>);
+
+    const materialTypography = component.find('ForwardRef(Typography)');
+
+    expect(materialTypography.prop('noWrap')).toBe(true);
   });
 });
