e4b17a898ddf8db245f692845faf17e3d276025b
Vagner A Saraiva <vagner@vizir.com.br>
2019-11-14 16:34:40 -0300

    WIP(mobile): creating avatar component
    affects: @naturacosmeticos/natds-rn

---
+++ packages/mobile/src/Components/Avatar.tsx
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import { Avatar as PaperAvatar, withTheme } from 'react-native-paper';
+import {
+  ImageSourcePropType,
+  StyleProp,
+  ViewStyle,
+  Surface,
+  TextStyle,
+} from 'react-native';
+import { IThemeShape } from '../Provider/IThemeShape';
+import { IconSource } from 'react-native-paper/lib/typescript/src/components/Icon';
+
+type AvatarType = 'image' | 'text' | 'icon';
+declare type AvatarProps = React.ComponentProps<typeof Surface> & {
+  /**
+   * Type of Avatar (image, text or icon).
+   */
+  type: AvatarType;
+  /**
+   * Image to display for the `Avatar`. Use only in Image Type.
+   */
+  source: ImageSourcePropType;
+  /**
+   * Size of the avatar.
+   */
+  size?: number;
+  /**
+   * React Native style object.
+   */
+  style?: StyleProp<ViewStyle>;
+  /**
+   * @optional
+   */
+  theme: IThemeShape;
+  /**
+   * Initials to show as the text in the `Avatar`. Use only in Text Type.
+   */
+  label: string;
+  /**
+   * Custom color for the text.
+   */
+  color?: string;
+  /**
+   * Style for the title. Use only in Text Type.
+   */
+  labelStyle?: StyleProp<TextStyle>;
+  /**
+   * Icon to display for the `Avatar`. Use only in Icon Type.
+   */
+  icon: IconSource;
+};
+
+const Avatar: React.FunctionComponent<Omit<AvatarProps, 'height' | 'width'>> = (
+  props: Omit<AvatarProps, 'height' | 'width'>
+) => {
+  const types = {
+    image: <PaperAvatar.Image {...props} />,
+    text: <PaperAvatar.Text {...props} />,
+    icon: <PaperAvatar.Icon {...props} />,
+  };
+  return types[props.type];
+};
+
+export default withTheme(Avatar);
--- packages/mobile/src/index.ts
+++ packages/mobile/src/index.ts
@@ -6,3 +6,4 @@ export const themes = { ...mobileThemes };
 export { default as Chip } from './Components/Chip';
 export { default as Button } from './Components/Button';
 export { default as Typography } from './Components/Typography';
+export { default as Avatar } from './Components/Avatar';
