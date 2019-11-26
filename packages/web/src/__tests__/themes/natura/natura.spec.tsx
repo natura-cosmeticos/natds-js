29616b482e3093ff5c49c9629b9971962f17c914
Matheus Robert Lichtnow <matheusrobert8@gmail.com>
2019-11-26 14:27:51 -0300

    chore(theme): altered dark natura theme color scheme
    affects: @naturacosmeticos/natds-styles, @naturacosmeticos/natds-web

--- packages/web/src/__tests__/themes/natura/natura.spec.tsx
+++ packages/web/src/__tests__/themes/natura/natura.spec.tsx
@@ -14,18 +14,4 @@ describe('Themes', () => {
 
     expect(light).toStrictEqual(naturaLight);
   });
-
-  test('it should match the natura dark theme snapshot', () => {
-    const naturaDark = require('./naturaDarkThemeParsed.json');
-    const {
-      natura: { dark },
-    } = themes;
-
-    /**
-     * This is due to the json file limitation. It can't have a property with value undefined.
-     */
-    naturaDark.typography.fontFamilyBrand3 = undefined;
-
-    expect(dark).toStrictEqual(naturaDark);
-  });
 });
