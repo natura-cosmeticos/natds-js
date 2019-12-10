177dfacc4e7b0de78986bef8608a58e3e148adb0
Vagner A Saraiva <vagner@vizir.com.br>
2019-12-10 17:28:10 -0300

    feat(dialog): fixing docs mdx and interactive canvas
    affects: @naturacosmeticos/natds-docs

--- packages/docs/src/web/Dialog/Dialog.story.tsx
+++ packages/docs/src/web/Dialog/Dialog.story.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { select, boolean } from '@storybook/addon-knobs';
 import withJest from '@decorators/jest/jest';
 import withContainer from '@decorators/container/container';
 import {
@@ -8,6 +9,7 @@ import {
   DialogContentText,
   DialogActions,
   Button,
+  Radio,
 } from '@naturacosmeticos/natds-web';
 
 import DialogDocs from './Dialog.docs.mdx';
@@ -26,11 +28,101 @@ export default {
   },
 };
 
-export const Interactive = () => (
-  <Dialog open={true}>
+const maxWidth: any = {
+  xs: 'xs',
+  sm: 'sm',
+  md: 'md',
+  lg: 'lg',
+  xl: 'xl',
+};
+
+export const Interactive = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Open Dialog
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        fullWidth={boolean('fullWidth', false) ? true : false}
+        maxWidth={select('maxWidth', maxWidth, maxWidth.sm)}
+      >
+        <DialogTitle>Lorem ipsum</DialogTitle>
+        <DialogContent dividers={boolean('dividers', false) ? true : false}>
+          <DialogContentText>
+            <Radio /> Option list 1
+          </DialogContentText>
+          <DialogContentText>
+            <Radio /> Option list 1
+          </DialogContentText>
+          <DialogContentText>
+            <Radio /> Option list 1
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Ok</Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
+
+export const Alert = () => (
+  <Dialog open={true} maxWidth="xs">
+    <DialogContent>
+      <DialogContentText>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.?
+      </DialogContentText>
+    </DialogContent>
+    <DialogActions>
+      <Button>Cancel</Button>
+      <Button>Action</Button>
+    </DialogActions>
+  </Dialog>
+);
+
+export const Simple = () => (
+  <Dialog open={true} maxWidth="xs" fullWidth={true}>
     <DialogTitle>Lorem ipsum</DialogTitle>
     <DialogContent>
-      <DialogContentText>Test</DialogContentText>
+      <DialogContentText>
+        <Radio /> Option list 1
+      </DialogContentText>
+      <DialogContentText>
+        <Radio /> Option list 1
+      </DialogContentText>
+      <DialogContentText>
+        <Radio /> Option list 1
+      </DialogContentText>
+    </DialogContent>
+  </Dialog>
+);
+
+export const Confirmation = () => (
+  <Dialog open={true} fullWidth={true} maxWidth="xs">
+    <DialogTitle>Lorem ipsum</DialogTitle>
+    <DialogContent dividers>
+      <DialogContentText>
+        <Radio /> Option list 1
+      </DialogContentText>
+      <DialogContentText>
+        <Radio /> Option list 1
+      </DialogContentText>
+      <DialogContentText>
+        <Radio /> Option list 1
+      </DialogContentText>
     </DialogContent>
     <DialogActions>
       <Button>Cancel</Button>
