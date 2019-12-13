fcb514a143bda82c85ed8fe5bf7039e318d720b1
Vagner A Saraiva <vagner@vizir.com.br>
2019-12-13 11:11:25 -0300

    fix(web): removing props translate to evict warning when consuming package
    affects: @naturacosmeticos/natds-docs, @naturacosmeticos/natds-web

--- packages/web/src/Components/IconButton.tsx
+++ packages/web/src/Components/IconButton.tsx
@@ -1,12 +1,14 @@
 import React, { FunctionComponent } from 'react';
 import { withTheme } from '@material-ui/styles';
-import MaterialIconButton, { IconButtonProps as IIconButtonProps } from '@material-ui/core/IconButton';
+import MaterialIconButton, {
+  IconButtonProps as IIconButtonProps,
+} from '@material-ui/core/IconButton';
 export { IconButtonProps as IIconButtonProps } from '@material-ui/core/IconButton';
 
-export const IconButton: FunctionComponent<IIconButtonProps> = (props: IIconButtonProps) => {
-  return (
-    <MaterialIconButton {...props} />
-  );
+export const IconButton: FunctionComponent<Omit<IIconButtonProps,'translate'>> = (
+  props: Omit<IIconButtonProps, 'translate'>
+) => {
+  return <MaterialIconButton {...props} />;
 };
 
 export default withTheme(IconButton);
