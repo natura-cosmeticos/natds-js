7144ea5014c313d79b5ba96ad48f3595e047ef90
Matheus Lichtnow <matheusrobert8@gmail.com>
2019-12-23 15:20:44 -0800

    docs(storybook): fixing Switch sections
    affects: @naturacosmeticos/natds-docs

--- packages/docs/src/components/Switch/sections.tsx
+++ packages/docs/src/components/Switch/sections.tsx
@@ -1,35 +1,23 @@
 import React from 'react';
-import { Checkbox, Radio, Switch } from '@naturacosmeticos/natds-web';
+import { Switch } from '@naturacosmeticos/natds-web';
 
 export const colors = [
   {
     title: 'Primary',
     component: (
-      <>
-        <Checkbox color="primary" checked={true} />
-        <Radio color="primary" checked={true} />
-        <Switch color="primary" checked={true} />
-      </>
+      <Switch color="primary" checked={true} />
     ),
   },
   {
     title: 'Secondary',
     component: (
-      <>
-        <Checkbox color="secondary" checked={true} />
-        <Radio color="secondary" checked={true} />
-        <Switch color="secondary" checked={true} />
-      </>
+      <Switch color="secondary" checked={true} />
     ),
   },
   {
     title: 'Default',
     component: (
-      <>
-        <Checkbox color="default" checked={true} />
-        <Radio color="default" checked={true} />
-        <Switch color="default" checked={true} />
-      </>
+      <Switch color="default" checked={true} />
     ),
   },
 ];
@@ -39,10 +27,6 @@ export const attributes = [
     title: 'Enabled',
     component: (
       <>
-        <Checkbox checked={false} />
-        <Checkbox checked />
-        <Radio checked={false} />
-        <Radio checked />
         <Switch checked={false} />
         <Switch checked />
       </>
@@ -52,10 +36,6 @@ export const attributes = [
     title: 'Disabled',
     component: (
       <>
-        <Checkbox disabled checked={false} />
-        <Checkbox disabled checked />
-        <Radio disabled checked={false} />
-        <Radio disabled checked />
         <Switch disabled checked={false} />
         <Switch disabled checked />
       </>
