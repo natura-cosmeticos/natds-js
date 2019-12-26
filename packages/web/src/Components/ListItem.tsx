70d4a53f176dddc00062064b157bc6f3b741492c
Vagner A Saraiva <vagner@vizir.com.br>
2019-12-26 09:49:51 -0300

    feat(list): fixing ListItem to pass props correctly
    affects: @naturacosmeticos/natds-web

--- packages/web/src/Components/ListItem.tsx
+++ packages/web/src/Components/ListItem.tsx
@@ -1,17 +1,4 @@
-import React, { FunctionComponent } from 'react';
-import { withTheme } from '@material-ui/styles';
-import MaterialListItem, {
-  ListItemProps as IListItemProps,
-} from '@material-ui/core/ListItem';
+import MaterialListItem from '@material-ui/core/ListItem';
 export { ListItemProps as IListItemProps } from '@material-ui/core/ListItem';
 
-const ListItem: FunctionComponent<IListItemProps> = (props: IListItemProps) => {
-  const { button, ...rest } = props;
-  return button ? (
-    <MaterialListItem button>{props.children}</MaterialListItem>
-  ) : (
-    <MaterialListItem {...rest} />
-  );
-};
-
-export default withTheme(ListItem);
+export default MaterialListItem;
