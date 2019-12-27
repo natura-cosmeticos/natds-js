bfdbe2a32d27e9a57c2a853c5e0c8673735e75fe
Vagner A Saraiva <vagner@vizir.com.br>
2019-12-27 14:30:58 -0300

    feat(paper): including snapshot test to paper component
    affects: @naturacosmeticos/natds-web

---
+++ packages/web/src/__tests__/Paper.spec.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { expect } from 'chai';
+
+import { Paper } from '..';
+
+describe('Paper components', () => {
+  describe('rendering variants', () => {
+    test('should match to snapshot - default Paper', () => {
+      const component = mount(<Paper />);
+      expect(component).matchSnapshot('Paper default snapshot');
+    });
+
+    test('should match to snapshot - with elevation', () => {
+      const component = mount(<Paper elevation={3} />);
+      expect(component).matchSnapshot('Paper with elevation snapshot');
+    });
+
+    test('should match to snapshot - with variant outlined', () => {
+      const component = mount(<Paper variant="outlined" />);
+      expect(component).matchSnapshot('Paper with variant outlined snapshot');
+    });
+
+    test('should match to snapshot - with variant outlined square', () => {
+      const component = mount(<Paper variant="outlined" square />);
+      expect(component).matchSnapshot(
+        'Paper with variant outlined square snapshot'
+      );
+    });
+  });
+});
