3cc215b080fbf8245f6932de4f567bb330099f44
Travis CI <travis@travis-ci.org>
2020-01-03 16:47:44 +0000

    Travis Commit: Generating docs for production versions 0.2.0

---
+++ packages/icons/dist/index.js
@@ -0,0 +1,27 @@
+"use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var iconStyles = __importStar(require("./natds-icons.css"));
+exports.iconStyles = iconStyles;
+var natds_icons_json_1 = __importDefault(require("./natds-icons.json"));
+exports.iconNames = natds_icons_json_1.default;
+var NatdsIconsEot = __importStar(require("./fonts/natds-icons.eot"));
+exports.NatdsIconsEot = NatdsIconsEot;
+var NatdsIconsSvg = __importStar(require("./fonts/natds-icons.svg"));
+exports.NatdsIconsSvg = NatdsIconsSvg;
+var NatdsIconsTtf = __importStar(require("./fonts/natds-icons.ttf"));
+exports.NatdsIconsTtf = NatdsIconsTtf;
+var NatdsIconsWoff = __importStar(require("./fonts/natds-icons.woff"));
+exports.NatdsIconsWoff = NatdsIconsWoff;
+var NatdsIconsWoff2 = __importStar(require("./fonts/natds-icons.woff2"));
+exports.NatdsIconsWoff2 = NatdsIconsWoff2;
+//# sourceMappingURL=index.js.map
