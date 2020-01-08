41e34b7460e5557960b378d58df2e8b365260ff1
Matheus Lichtnow <matheusrobert8@gmail.com>
2020-01-08 08:27:22 -0800

    fix(AppBar): fixes AppBar paddings
    affects: @naturacosmeticos/natds-styles, @naturacosmeticos/natds-web

--- packages/styles/src/themes/natura/natura-light.ts
+++ packages/styles/src/themes/natura/natura-light.ts
@@ -106,15 +106,27 @@ export const naturaLight: ITheme = {
       }
     },
     MuiToolbar: {
+      root: {
+        padding: '0 16px',
+        '@media (min-width: 600px)': {
+          padding: '0 16px',
+        }
+      },
+      gutters: {
+        padding: '0 16px',
+        '@media (min-width: 600px)': {
+          padding: '0 16px',
+        }
+      },
       regular: {
         minHeight: "56px",
-        '@media (min-width: 600px)' : {
+        '@media (min-width: 600px)': {
           minHeight: "56px",
         }
       },
       dense: {
         minHeight: "56px",
-        '@media (min-width: 600px)' : {
+        '@media (min-width: 600px)': {
           minHeight: "56px",
         }
       },
