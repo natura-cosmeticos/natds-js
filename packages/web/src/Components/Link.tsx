307ecae7a5a9970ffdc1b6bc85003210763e056b
Vicente Lyrio <vicentelyrio@Vicentes-MacBook-Pro.local>
2020-01-27 16:15:53 -0300

    fix(link): including link complementary color for Link component
    affects: @naturacosmeticos/natds-docs, @naturacosmeticos/natds-web

--- packages/web/src/Components/Link.tsx
+++ packages/web/src/Components/Link.tsx
@@ -1,10 +1,64 @@
 import React, { FunctionComponent } from 'react';
-import MaterialLink, { LinkProps as ILinkProps, } from '@material-ui/core/Link';
+import MaterialLink, { LinkProps } from '@material-ui/core/Link';
 import { withTheme } from '@material-ui/core';
-export { LinkProps as ILinkProps } from '@material-ui/core/Link';
+import { IThemeWeb } from 'Themes';
+import { getDefaultTheme } from './shared';
 
-const Link: FunctionComponent<ILinkProps> = (props: ILinkProps) => (
-  <MaterialLink {...props} />
-);
+type LinkColor =
+  | 'default'
+  | 'initial'
+  | 'inherit'
+  | 'primary'
+  | 'secondary'
+  | 'textPrimary'
+  | 'textSecondary'
+  | 'error';
+
+export interface ILinkProps extends Omit<LinkProps, "color"> {
+  /**
+   * @optional
+   * @default 'default'
+   * Color of the Link. Defaults to 'primary'
+   */
+  color?: LinkColor;
+  /**
+   * @optional
+   */
+  theme?: IThemeWeb | unknown;
+}
+
+const Link: FunctionComponent<ILinkProps> = (props: ILinkProps) => {
+  const {
+    color = 'default',
+    theme: providerTheme,
+    style
+  } = props;
+
+  const defaultColor = color === 'default';
+  const validColor = defaultColor ? 'inherit' : color;
+
+  const theme = React.useMemo(() => {
+    return getDefaultTheme(providerTheme);
+  }, [providerTheme]);
+
+  const colorMap = React.useMemo(() => {
+    const { palette } = theme as any;
+
+    return {
+      default: {
+        color: palette.complementary.link
+      }
+    };
+  }, [theme]);
+
+  return <MaterialLink
+    {...props}
+    color={validColor as any}
+    style={{
+      ...(defaultColor ? colorMap[color] : {}),
+      ...style
+    }}
+  />;
+};
 
 export default withTheme(Link);
