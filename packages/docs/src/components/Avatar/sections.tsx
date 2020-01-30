587fbc0176053e1ab62f10f92a6c4d7b919efb7e
Vicente Lyrio <vicentelyrio@gmail.com>
2020-01-30 11:53:37 -0300

    fix(chip): updating Chip component with new Specs
    affects: @naturacosmeticos/natds-docs, @naturacosmeticos/natds-styles, @naturacosmeticos/natds-web

--- packages/docs/src/components/Avatar/sections.tsx
+++ packages/docs/src/components/Avatar/sections.tsx
@@ -2,40 +2,55 @@ import React from 'react';
 import { Avatar } from '@naturacosmeticos/natds-web';
 import Favorite from '@material-ui/icons/Favorite';
 
+export const colors = [
+  {
+    title: 'Primary',
+    component: <Avatar children="XD" size="standard" color="primary" />
+  },
+  {
+    title: 'Secondary',
+    component: <Avatar children="XD" size="standard" color="secondary" />
+  },
+  {
+    title: 'Default',
+    component: <Avatar children="XD" size="standard" color="default" />
+  }
+];
+
 export const types = [
   {
     title: 'Image',
-    component: <Avatar src={require('../../assets/images/1.png')} />
+    component: <Avatar src={require('../../assets/images/1.png')} size="standard" color="primary" />
   },
   {
     title: 'Letter',
-    component: <Avatar children="XD" />
+    component: <Avatar children="XD" size="standard" color="primary" />
   },
   {
     title: 'Icon',
-    component: <Avatar children={<Favorite />} />
+    component: <Avatar children={<Favorite />} size="standard" color="primary" />
   }
 ];
 
 export const sizes = [
   {
     title: 'Tiny',
-    component: <Avatar src={require('../../assets/images/1.png')} size="tiny" />
+    component: <Avatar src={require('../../assets/images/1.png')} size="tiny" color="primary" />
   },
   {
     title: 'Small',
-    component: <Avatar src={require('../../assets/images/1.png')} size="small" />
+    component: <Avatar src={require('../../assets/images/1.png')} size="small" color="primary" />
   },
   {
     title: 'Standard',
-    component: <Avatar src={require('../../assets/images/1.png')} size="standard" />
+    component: <Avatar src={require('../../assets/images/1.png')} size="standard" color="primary" />
   },
   {
     title: 'Large',
-    component: <Avatar src={require('../../assets/images/1.png')} size="large" />
+    component: <Avatar src={require('../../assets/images/1.png')} size="large" color="primary" />
   },
   {
     title: 'Huge',
-    component: <Avatar src={require('../../assets/images/1.png')} size="huge" />
+    component: <Avatar src={require('../../assets/images/1.png')} size="huge" color="primary" />
   }
 ];
