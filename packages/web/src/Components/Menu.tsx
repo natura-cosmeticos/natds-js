5bcc9e7ddba7bd445e208b532764079872955783
Vicente Lyrio <vicentelyrio@gmail.com>
2020-02-11 11:15:14 -0300

    fix(menu): updating menu and list hover color
    affects: @naturacosmeticos/natds-docs, @naturacosmeticos/natds-styles, @naturacosmeticos/natds-web

--- packages/web/src/Components/Menu.tsx
+++ packages/web/src/Components/Menu.tsx
@@ -3,8 +3,11 @@ import { withTheme } from '@material-ui/styles';
 import MaterialMenu, { MenuProps as IMenuProps } from '@material-ui/core/Menu';
 export { MenuProps as IMenuProps } from '@material-ui/core/Menu';
 
-const Menu: FunctionComponent<IMenuProps> = (props: IMenuProps) => (
-  <MaterialMenu {...props} />
-);
+const Menu: FunctionComponent<IMenuProps> = React.forwardRef((
+    props: IMenuProps,
+    ref: any
+  ) => (
+    <MaterialMenu {...props} ref={ref} />
+));
 
 export default withTheme(Menu);
--- packages/web/src/Components/MenuItem.tsx
+++ packages/web/src/Components/MenuItem.tsx
@@ -3,11 +3,18 @@ import { withTheme } from '@material-ui/styles';
 import MaterialMenuItem, { MenuItemProps as IMenuItemProps } from '@material-ui/core/MenuItem';
 export { MenuItemProps as IMenuItemProps } from '@material-ui/core/MenuItem';
 
-const MenuItem: FunctionComponent<IMenuItemProps> = (props: IMenuItemProps) => {
+const MenuItem: FunctionComponent<IMenuItemProps> = React.forwardRef((
+    props: IMenuItemProps,
+    ref: any
+  ) => {
   const { button } = props;
   return (
-    <MaterialMenuItem {...props} button={button as true | undefined} />
+    <MaterialMenuItem
+      {...props}
+      button={button as true | undefined}
+      ref={ref}
+    />
   );
-};
+});
 
 export default withTheme(MenuItem);
