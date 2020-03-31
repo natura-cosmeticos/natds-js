4e8dffdbcbf29b7281a948d3dbe234579488563b
Vagner A Saraiva <vagner@vizir.com.br>
2020-03-31 13:41:09 -0300

    fix(popover): fixing spec test for popover component
    affects: @naturacosmeticos/natds-web

--- packages/web/src/__tests__/Popover.spec.tsx
+++ packages/web/src/__tests__/Popover.spec.tsx
@@ -5,29 +5,44 @@ import { mount } from 'enzyme';
 import Popover from '../Components/Popover';
 
 const placements: any = {
-  bottomEnd: 'bottom-end',
-  bottomStart: 'bottom-start',
-  bottom: 'bottom',
-  leftEnd: 'left-end',
-  leftStart: 'left-start',
-  left: 'left',
-  rightEnd: 'right-end',
-  rightStart: 'right-start',
-  right: 'right',
-  topEnd: 'top-end',
-  topStart: 'top-start',
-  top: 'top',
+  'bottom-end': 'bottom-end',
+  'bottom-start': 'bottom-start',
+  'bottom': 'bottom',
+  'left-end': 'left-end',
+  'left-start': 'left-start',
+  'left': 'left',
+  'right-end': 'right-end',
+  'right-start': 'right-start',
+  'right': 'right',
+  'top-end': 'top-end',
+  'top-start': 'top-start',
+  'top': 'top',
 };
 
 describe('Popover component', () => {
+  /*
+    Due to Material-UI not handling the anchorEl property correctly,
+    this mock was created disabling the console.error unnecessary.
+    For more information, follow
+    https://github.com/mui-org/material-ui/blob/master/packages/material-ui/src/Popper/Popper.js#L238
+  */
+
+  // tslint:disable-next-line: no-console
+  const originalConsoleError = console.error;
+  // tslint:disable-next-line: no-console
+  afterEach(() => (console.error = originalConsoleError));
+
+  // tslint:disable-next-line: no-empty
+  const mockedConsoleError = () => {};
+  // tslint:disable-next-line: no-console
+  beforeEach(() => (console.error = mockedConsoleError));
+
   Object.keys(placements).forEach((placement: string) => {
     test(`should match snapshot - Any valid Popover with ${placement} placement`, () => {
-      const component = mount(
-        <Popover open={true} placement={placement as any}>
-          content
-        </Popover>
-      );
-      expect(component).matchSnapshot(
+      const wrapper = mount(<Popover open={true} placement={placement as any}>
+        content
+      </Popover>);
+      expect(wrapper).matchSnapshot(
         `Popover snapshot - with ${placement} placement`
       );
     });
