c9559a4549280ddbb5df7ab18342cbd245670982
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-06-17 17:11:21 -0300

    refactor(docs): remove old package/docs/ package

--- packages/docs/addons/versions/shared.js
+++
@@ -1,5 +0,0 @@
-export const TITLE = 'Versions';
-export const PARAM_KEY = 'versions';
-export const ADDON_ID = 'natds/versions';
-export const PANEL_ID = `${ADDON_ID}/panel`;
-export const CHANGE = `${ADDON_ID}/change`;
