9c42bb6548cca2066128b5cb007454f526b3bc73
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-07-09 10:34:31 -0300

    ci(docs): fix version suffix and output directory name

--- scripts/build-storybook.sh
+++ scripts/build-storybook.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-echo "Opening project root directory..."
+echo "STORYBOOK Opening project root directory..."
 cd "$TRAVIS_BUILD_DIR"
 
-echo "Fetching packages version..."
+echo "STORYBOOK Fetching packages version..."
 VERSION=`cat lerna.json \
   | grep version \
   | head -1 \
@@ -13,44 +13,44 @@ VERSION=`cat lerna.json \
   | tr -d '[[:space:]]'n`
 
 if [ -z "${VERSION}" ]; then
-  echo "Can not build Storybook. Could not find version information at lerna.json file."
+  echo "STORYBOOK Can not build Storybook. Could not find version information at lerna.json file."
   exit 0
 else
-  echo "Generating Storybook for version ${VERSION}..."
+  echo "STORYBOOK Generating Storybook for version ${VERSION}..."
 fi
 
-echo "Creating temp directory for Storybook bundle..."
+echo "STORYBOOK Creating temp directory for bundle..."
 mkdir -p ../tmp
 
-echo "Opening Storybook directory..."
+echo "STORYBOOK Opening docs package directory..."
 cd "$TRAVIS_BUILD_DIR/packages/docs"
 
-echo "Generating JSON report for Jest addon..."
+echo "STORYBOOK Generating JSON report for Jest addon..."
 yarn lerna run test:packages
 
-echo "Building Storybook..."
+echo "STORYBOOK Building Storybook..."
 yarn build -o "${TRAVIS_BUILD_DIR}/../tmp/v${VERSION}" --quiet
 
-echo "Going back to project root directory..."
+echo "STORYBOOK Going back to project root directory..."
 cd "$TRAVIS_BUILD_DIR"
 
-echo "Moving to docs branch..."
+echo "STORYBOOK Moving to docs branch..."
 git checkout .
 git checkout master-docs
 
-echo "Adding version ${VERSION} to versions JSON file..."
+echo "STORYBOOK Adding version ${VERSION} to versions JSON file..."
 cd scripts
-node helpers/addVersionOnConfig.js "v${VERSION}"
+node helpers/addVersionOnConfig.js "${VERSION}"
 
-echo "Going back to project root directory..."
+echo "STORYBOOK Going back to project root directory..."
 cd "${TRAVIS_BUILD_DIR}"
 
-echo "Copying generated Storybook to releases directory..."
+echo "STORYBOOK Copying generated Storybook to releases directory..."
 cp -r "${TRAVIS_BUILD_DIR}/../tmp/v${VERSION}" packages/docs/dist/releases
 
-echo "Committing changes..."
+echo "STORYBOOK Committing changes..."
 git add --all
 git commit -m "docs: generating storybook for version ${VERSION} [skip ci]"
 
-echo "Updating master-docs branch..."
+echo "STORYBOOK Updating master-docs branch..."
 git push -f -u origin master-docs
