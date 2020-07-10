ad9a11ce36f86e4a52c122478598d2cb69b517cc
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-07-10 20:55:46 -0300

    chore(web): add reminder to migrate from ExpansionPanel to Accordion

--- packages/web/src/Components/ExpansionPanel.tsx
+++ packages/web/src/Components/ExpansionPanel.tsx
@@ -1,10 +1,14 @@
 import React, { FunctionComponent, forwardRef } from 'react';
 import { withTheme } from '@material-ui/styles';
+
+/**
+ * @todo fix(web): import Accordion from '@material-ui/core/Accordion' instead of the old ExpansionPanel component
+ */
 import MaterialExpansionPanel, {
-  ExpansionPanelProps as IExpansionPanelProps 
+  ExpansionPanelProps as IExpansionPanelProps
 } from '@material-ui/core/ExpansionPanel';
-export { 
-  ExpansionPanelProps as IExpansionPanelProps 
+export {
+  ExpansionPanelProps as IExpansionPanelProps
 } from '@material-ui/core/ExpansionPanel';
 
 export const ExpansionPanel: FunctionComponent<IExpansionPanelProps> = forwardRef((
