670fdfc4737647a85ce74f6c52af245fba730ccb
Victor Maciel <victor@vizir.com.br>
2020-07-16 15:13:56 -0300

    refactor(web): move TableSortLabel override style to packages/styles and update snapshots
    affects: @naturacosmeticos/natds-web

--- packages/web/src/Components/Table/TableSortLabel.tsx
+++ packages/web/src/Components/Table/TableSortLabel.tsx
@@ -1,35 +1,15 @@
 import React, { FunctionComponent, forwardRef } from 'react';
 import MaterialTableSortLabel, {
-  TableSortLabelProps as MaterialTableRowProps,
+  TableSortLabelProps as ITableSortLabelProps,
 } from '@material-ui/core/TableSortLabel';
-import { withTheme, makeStyles } from '@material-ui/core/styles';
-import { IThemeWeb } from '../../Themes';
-import { getDefaultTheme } from '../shared';
-
-export interface ITableSortLabelProps extends MaterialTableRowProps {
-  theme?: IThemeWeb | unknown;
-}
+import { withTheme } from '@material-ui/core/styles';
+export { TableSortLabelProps as ITableSortLabelProps } from '@material-ui/core/TableSortLabel';
 
 export const TableSortLabel: FunctionComponent<ITableSortLabelProps> = forwardRef((
   props: ITableSortLabelProps,
   ref: any
-) => {
-  const { 
-    theme: providerTheme
-  } = props;
-
-  const theme: any  = React.useMemo(() => getDefaultTheme(providerTheme), [providerTheme]);
-  const useStyles = React.useMemo(() => makeStyles({
-    icon: {
-      marginLeft: `8px !important`
-    }
-  }), [theme]);
-
-  const customClasses = useStyles();
-
-  return (
-    <MaterialTableSortLabel classes={{icon: customClasses.icon}} {...props} ref={ref} />
-  );
-});
+) => (
+  <MaterialTableSortLabel {...props} ref={ref} />
+));
 
 export default withTheme(TableSortLabel);
