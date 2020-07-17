932d1f6a0f89b5a84add17debe934a48d8ab97db
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-07-17 16:07:23 -0300

    style(icons): format config files with eslint rules

--- packages/icons/scripts/svgToFont.js
+++ packages/icons/scripts/svgToFont.js
@@ -1,84 +1,107 @@
 #!/usr/bin/env node
-const webfont = require('webfont').default;
-const fs = require('fs');
+const webfont = require("webfont").default;
+const fs = require("fs");
 
-const distMetada = './src/';
-const distFont = './src/fonts/';
-const files = './src/assets/cleaned/**/*.svg';
-const fontName = 'natds-icons';
-const types = ['eot', 'ttf', 'woff', 'woff2', 'svg'];
+const config = {
+  "files": "./src/assets/cleaned/**/*.svg",
+  "fontHeight": 600,
+  "fontName": "natds-icons",
+  "normalize": true,
+  "template": "css",
+  "templateFontPath": "./fonts"
+};
 
-function createSwiftOutput(metadata) {
-  const capitalizeWord = word => `${word[0].toUpperCase()}${word.slice(1)}`;
+const distMetadata = "./src/";
+const distFont = "./src/fonts/";
+const types = [
+  "eot",
+  "ttf",
+  "woff",
+  "woff2",
+  "svg"
+];
 
-  const toCamelCase = word => word
-    .split('-')
-    .map((item, index) => index === 0 ? item : capitalizeWord(item))
-    .join('')
+const onError = (error) => {
+  if (error) {
+    console.log(error);
+  }
+};
 
-  const toIosUnicode = (unicode) => `${unicode.replace('%u', '\\u{')}}`
+const createSwiftOutput = (metadata) => {
+
+  const FIRST_CHAR_INDEX = 0;
+  const SECOND_CHAR_INDEX = 0;
+  const capitalizeWord = (word) => `${word[FIRST_CHAR_INDEX].toUpperCase()}${word.slice(SECOND_CHAR_INDEX)}`;
+
+  const toCamelCaseMapper = (item, index) => {
+    if (index === FIRST_CHAR_INDEX) {
+      return item;
+    }
+
+    return capitalizeWord(item);
+  };
+
+  const toCamelCase = (word) => word.
+    split("-").
+    map(toCamelCaseMapper).
+    join("");
+
+  const toIosUnicode = (unicode) => `${unicode.replace("%u", "\\u{")}}`;
 
   const formattedMetadataSwift = `
 public enum Icon: String, CaseIterable {
   ${
-    Object
-      .keys(metadata)
-      .map(iconName => `case ${toCamelCase(iconName)} = "${iconName}"\n`)
-      .join('')
-    }
+  Object.
+    keys(metadata).
+    map((iconName) => `case ${toCamelCase(iconName)} = "${iconName}"\n`).
+    join("")
+}
 }
 
 extension Icon {
   var unicode: String {
       switch self {
       ${
-    Object
-      .keys(metadata)
-      .map(iconName => `case .${toCamelCase(iconName)}: return "${toIosUnicode(metadata[iconName])}"\n`)
-      .join('')
-    }
+  Object.
+    keys(metadata).
+    map((iconName) => `case .${toCamelCase(iconName)}: return "${toIosUnicode(metadata[iconName])}"\n`).
+    join("")
+}
       }
   }
 }
-`
-  fs.writeFile(distMetada + fontName + '.swift', formattedMetadataSwift, onError);
-}
+`;
 
-function onError(error) {
-  if (error) console.log(error);
-}
+  fs.writeFile(`${distMetadata + config.fontName}.swift`, formattedMetadataSwift, onError);
+};
 
-function onSuccess(result) {
-  const { config: { fontName }, template, glyphsData } = result;
+const INDEX_INCREMENT = 1;
+
+const onSuccess = (result) => {
+  const { "config": { fontName }, template, glyphsData } = result;
   const metadata = {};
 
-  glyphsData.forEach(({ metadata: { name, unicode } }) => {
+  glyphsData.forEach(({ "metadata": { name, unicode } }) => {
     const escapedUnicode = escape(unicode);
+
     Object.assign(metadata, { [name]: escapedUnicode });
   });
 
-  fs.writeFile(distMetada + fontName + '.css', template, onError);
-  const formattedMetadataJson = JSON.stringify(metadata, null, "\t") + "\n";
-  fs.writeFile(distMetada + fontName + '.json', formattedMetadataJson, onError);
+  fs.writeFile(`${distMetadata + fontName}.css`, template, onError);
+  const formattedMetadataJson = `${JSON.stringify(metadata, null, "\t")}\n`;
 
-  for (let index = 0; index < types.length; index++) {
+  fs.writeFile(`${distMetadata + fontName}.json`, formattedMetadataJson, onError);
+
+  for (let index = 0; index < types.length; index += INDEX_INCREMENT) {
     const element = types[index];
-    const filename = distFont + fontName + '.' + element;
+    const filename = `${distFont + fontName}.${element}`;
     const font = result[element];
 
     fs.writeFile(filename, font, onError);
   }
 
   createSwiftOutput(metadata);
-}
-
-const config = {
-  files,
-  fontName,
-  template: 'css',
-  fontHeight: 600,
-  normalize: true,
-  templateFontPath: './fonts'
 };
 
-webfont(config).then(onSuccess).catch(onError);
+webfont(config).then(onSuccess).
+  catch(onError);
