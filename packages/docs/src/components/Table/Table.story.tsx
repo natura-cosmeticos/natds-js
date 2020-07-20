f47f25987a805ee2f29668b5bc164e0cad815bab
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-07-20 17:44:50 -0300

    style(docs): format Table.story.tsx

--- packages/docs/src/components/Table/Table.story.tsx
+++ packages/docs/src/components/Table/Table.story.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import * as React from "react";
 import withJest from "@decorators/jest/jest";
 import withContainer from "@decorators/container/withContainer";
 import {
