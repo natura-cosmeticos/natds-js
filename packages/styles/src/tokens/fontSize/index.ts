eb76549e5405915c389d971af526e0ca35a9798d
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-07-24 12:07:48 -0300

    fix(styles): set correct font weights to typography tokens

--- packages/styles/src/tokens/fontSize/index.ts
+++ packages/styles/src/tokens/fontSize/index.ts
@@ -1,57 +1,63 @@
 /* eslint-disable sort-keys */
 import { IFont } from "./IFont";
 
+const REGULAR = 400;
+const MEDIUM = 500;
+
+/**
+ * @todo refactor: rename `fontSize` to typography
+ */
 export const fontSize = {
   h1: {
     fontSize: "6rem",
-    fontWeight: 300,
+    fontWeight: REGULAR,
   } as IFont,
   h2: {
     fontSize: "3.75rem",
-    fontWeight: 300,
+    fontWeight: REGULAR,
   } as IFont,
   h3: {
     fontSize: "3rem",
-    fontWeight: 400,
+    fontWeight: REGULAR,
   } as IFont,
   h4: {
     fontSize: "2.125rem",
-    fontWeight: 400,
+    fontWeight: REGULAR,
   } as IFont,
   h5: {
     fontSize: "1.5rem",
-    fontWeight: 400,
+    fontWeight: REGULAR,
   } as IFont,
   h6: {
     fontSize: "1.25rem",
-    fontWeight: 600,
+    fontWeight: MEDIUM,
   } as IFont,
   subtitle1: {
     fontSize: "1rem",
-    fontWeight: 400,
+    fontWeight: MEDIUM,
   } as IFont,
   subtitle2: {
     fontSize: "0.875rem",
-    fontWeight: 500,
+    fontWeight: MEDIUM,
   } as IFont,
   body1: {
     fontSize: "1rem",
-    fontWeight: 400,
+    fontWeight: REGULAR,
   } as IFont,
   body2: {
     fontSize: "0.875rem",
-    fontWeight: 400,
+    fontWeight: REGULAR,
   } as IFont,
   button: {
     fontSize: "0.875rem",
-    fontWeight: 600,
+    fontWeight: MEDIUM,
   } as IFont,
   caption: {
     fontSize: "0.75rem",
-    fontWeight: 400,
+    fontWeight: REGULAR,
   } as IFont,
   overline: {
     fontSize: "0.75rem",
-    fontWeight: 400,
+    fontWeight: MEDIUM,
   } as IFont,
 };
