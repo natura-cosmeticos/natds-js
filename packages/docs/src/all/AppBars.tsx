7fcb9462452e855e12ff851bbb240b325fd04f29
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-07-24 21:54:46 -0300

    perf(docs): add lazy loading to AppBars imported components

--- packages/docs/src/all/AppBars.tsx
+++ packages/docs/src/all/AppBars.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
-import {
-  AppBar,
-  Toolbar,
-  IconButton,
-  Icon,
-  Typography,
-  Badge,
-} from "@naturacosmeticos/natds-web";
+import Skeleton from "@material-ui/lab/Skeleton";
 import { useStyles } from "./styles";
+
+const AppBar = React.lazy(() => import("@naturacosmeticos/natds-web/dist/Components/AppBar"));
+const Badge = React.lazy(() => import("@naturacosmeticos/natds-web/dist/Components/Badge"));
+const IconButton = React.lazy(() => import("@naturacosmeticos/natds-web/dist/Components/IconButton"));
+const Icon = React.lazy(() => import("@naturacosmeticos/natds-web/dist/Components/Icon"));
+const Toolbar = React.lazy(() => import("@naturacosmeticos/natds-web/dist/Components/Toolbar"));
+const Typography = React.lazy(() => import("@naturacosmeticos/natds-web/dist/Components/Typography"));
 
 type colors = "primary" | "secondary" | "default" | "inherit";
 
@@ -16,27 +16,49 @@ export const WrappedAppBar = ({ color }: { color: colors }) => {
 
   return (
     <div className={classes.appBar}>
-      <AppBar position="relative" color={color}>
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <Icon name="filled-navigation-menu" size="tiny" />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            AppBar
-          </Typography>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <Icon name="outlined-action-search" size="tiny" />
-          </IconButton>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <Badge color="error" badgeContent="1">
-              <Icon name="outlined-action-filter" size="tiny" />
-            </Badge>
-          </IconButton>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <Icon name="outlined-social-myprofile" size="tiny" />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
+      <React.Suspense fallback={<Skeleton variant={"rect"} />}>
+        <AppBar position="relative" color={color}>
+          <React.Suspense fallback={<Skeleton variant={"rect"} />}>
+            <Toolbar>
+              <React.Suspense fallback={<Skeleton variant={"rect"} />}>
+                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                  <React.Suspense fallback={<Skeleton variant={"circle"} />}>
+                    <Icon name="filled-navigation-menu" size="tiny" />
+                  </React.Suspense>
+                </IconButton>
+              </React.Suspense>
+              <React.Suspense fallback={<Skeleton variant="text" />}>
+                <Typography variant="h6" className={classes.title}>
+              AppBar
+                </Typography>
+              </React.Suspense>
+              <React.Suspense fallback={<Skeleton variant={"rect"} />}>
+                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                  <React.Suspense fallback={<Skeleton variant={"circle"} />}>
+                    <Icon name="outlined-action-search" size="tiny" />
+                  </React.Suspense>
+                </IconButton>
+              </React.Suspense>
+              <React.Suspense fallback={<Skeleton variant={"rect"} />}>
+                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                  <Badge color="error" badgeContent="1">
+                    <React.Suspense fallback={<Skeleton variant={"circle"} />}>
+                      <Icon name="outlined-action-filter" size="tiny" />
+                    </React.Suspense>
+                  </Badge>
+                </IconButton>
+              </React.Suspense>
+              <React.Suspense fallback={<Skeleton variant={"rect"} />}>
+                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                  <React.Suspense fallback={<Skeleton variant={"circle"} />}>
+                    <Icon name="outlined-social-myprofile" size="tiny" />
+                  </React.Suspense>
+                </IconButton>
+              </React.Suspense>
+            </Toolbar>
+          </React.Suspense>
+        </AppBar>
+      </React.Suspense>
     </div>
   );
 };
