bfb1aada8b9284dbebf35fdba93de7cb5bd22876
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-07-30 14:12:58 -0300

    refactor(web): move test and snapshot to component folder

--- packages/web/src/Components/Container/index.test.tsx
+++ packages/web/src/Components/Container/index.test.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as renderer from "react-test-renderer";
 
-import Container from "../../Components/Container";
+import Container from ".";
 
 describe("Container component", () => {
   it("matches default snapshot", () => {
