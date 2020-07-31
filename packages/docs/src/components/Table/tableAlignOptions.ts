eb51e70001670c3ba899fdfbd52cef68a1f579ab
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-07-31 10:14:07 -0300

    refactor(docs): delete old Storybook folder

--- packages/docs/src/components/Table/tableAlignOptions.ts
+++
@@ -1,7 +0,0 @@
-export const tableAlignOptions: { left: string; justify: string; center: string; inherit: string; right: string } = {
-  center: "center",
-  inherit: "inherit",
-  justify: "justify",
-  left: "left",
-  right: "right",
-};
