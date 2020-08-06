70bdaf15cde11a7a63a9b95e22c424cd86c6d131
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-08-06 20:31:52 -0300

    style(storybook): format with eslint

--- packages/web/.storybook/preview.js
+++ packages/web/.storybook/preview.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { addDecorator } from '@storybook/react';
-import {addParameters} from "@storybook/client-api";
+import { addParameters } from "@storybook/client-api";
 import { withKnobs } from '@storybook/addon-knobs';
 import { withA11y } from '@storybook/addon-a11y';
 import Container from "./decorators/Container";
