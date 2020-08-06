ca5dccf07dcd157358d52a2ac38b790fda62b591
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-08-06 20:49:24 -0300

    fix(skeleton): add missing ISkeletonProps export

--- packages/web/src/Components/Skeleton/index.tsx
+++ packages/web/src/Components/Skeleton/index.tsx
@@ -3,6 +3,8 @@ import * as MaterialSkeleton from "@material-ui/lab/Skeleton";
 import React from "react";
 import {ISkeletonProps} from "./ISkeletonProps";
 
+export {ISkeletonProps} from "./ISkeletonProps";
+
 export const Skeleton : (props: ISkeletonProps, ref: any) => JSX.Element = (props: ISkeletonProps, ref: any) => <MaterialSkeleton.default {...props} ref={ref} />;
 const DefaultSkeleton = React.forwardRef<HTMLElement, ISkeletonProps>(Skeleton);
 
