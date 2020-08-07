4119965b46ab665a10438501a224846622dc94a2
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-08-07 16:20:01 -0300

    fix(skeleton): add missing "children" property

--- packages/web/src/Components/Skeleton/ISkeletonProps.ts
+++ packages/web/src/Components/Skeleton/ISkeletonProps.ts
@@ -3,6 +3,7 @@ import * as MaterialSkeleton from "@material-ui/lab/Skeleton";
 export type ISkeletonProps = Pick<
   MaterialSkeleton.SkeletonProps,
   "animation" |
+  "children" |
   "height" |
   "variant" |
   "width"
