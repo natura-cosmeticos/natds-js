a71a446a6785a77dcd2655924dd136f595e0e7db
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-08-10 17:25:11 -0300

    style(storybook): remove unused code and smelly comments

--- packages/web/.storybook/webpack.config.js
+++ packages/web/.storybook/webpack.config.js
@@ -1,18 +1,8 @@
-const {fontsRule} = require("./rules/fontsRule");
-const {imagesRule} = require("./rules/imagesRule");
-const {sassRule} = require("./rules/sassRule");
-const {javascriptRule} = require("./rules/javascriptRule");
 const {storyRule} = require("./rules/storyRule");
 const {markdownRule} = require("./rules/markdownRule");
 const {typescriptRule} = require("./rules/typescriptRule");
-const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
 module.exports = ({config}) => {
-  // config.module.rules = [];
-  // config.module.rules.push(fontsRule);
-  // config.module.rules.push(imagesRule);
-  // config.module.rules.push(sassRule);
-  // config.module.rules.push(javascriptRule);
   config.module.rules.push(storyRule);
   config.module.rules.push(markdownRule);
   config.module.rules.push(typescriptRule);
