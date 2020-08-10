2925972b3bdaadc0010d5a7c9401cc622e31661a
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-08-10 19:54:59 -0300

    style(select): format with eslint

--- packages/web/src/Components/Select/index.tsx
+++ packages/web/src/Components/Select/index.tsx
@@ -1,14 +1,18 @@
-import * as React from 'react';
-import { withTheme } from '@material-ui/styles';
+import * as React from "react";
+import { withTheme } from "@material-ui/styles";
 import MaterialSelect, {
   SelectProps as ISelectProps,
-} from '@material-ui/core/Select';
+} from "@material-ui/core/Select";
 
 export const Select: React.FunctionComponent<Omit<
   ISelectProps,
-  'translate'
-  >> = React.forwardRef((props: Omit<ISelectProps, 'translate'>, ref: any) => (
-  <MaterialSelect {...props} ref={ref} />
-));
+  "translate"
+  >> = React.forwardRef((props: Omit<ISelectProps, "translate">,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ref: any) => (
+    <MaterialSelect {...props} ref={ref} />
+  ));
+
+Select.displayName = "Select";
 
 export default withTheme(Select);
