33e863cbe5514479a6f1d687f375e5ea4b07d71f
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-08-12 20:59:44 -0300

    refactor(elevation): refactor elevation token

--- packages/styles/src/tokens/elevation/index.ts
+++ packages/styles/src/tokens/elevation/index.ts
@@ -1,16 +1,25 @@
 /* eslint-disable sort-keys */
+import hexToRgba from "hex-to-rgba";
 import { IElevation } from "./IElevation";
+
+/**
+ * Defines 16% opacity for use in color
+ *
+ * Opacities must be a multiple of 8.
+ */
+const shadowColorAlpha = 0.16;
+const shadowColor = hexToRgba("#000000", shadowColorAlpha);
 
 export const elevation: IElevation = {
   0: "none",
-  1: "0 1px 1px 0 rgba(0, 0, 0, 0.14)",
-  2: "0 2px 2px 0 rgba(0, 0, 0, 0.14)",
-  3: "0 3px 4px 0 rgba(0, 0, 0, 0.14)",
-  4: "0 4px 5px 0 rgba(0, 0, 0, 0.14)",
-  6: "0 6px 10px 0 rgba(0, 0, 0, 0.14)",
-  8: "0 8px 10px 0 rgba(0, 0, 0, 0.14)",
-  9: "0 9px 12px 0 rgba(0, 0, 0, 0.14)",
-  12: "0 12px 17px 0 rgba(0, 0, 0, 0.14)",
-  16: "0 16px 24px 0 rgba(0, 0, 0, 0.14)",
-  24: "0 24px 38px 0 rgba(0, 0, 0, 0.14)",
+  1: `0 1px 1px 0 ${shadowColor}`,
+  2: `0 2px 2px 0 ${shadowColor}`,
+  3: `0 3px 4px 0 ${shadowColor}`,
+  4: `0 4px 5px 0 ${shadowColor}`,
+  6: `0 6px 10px 0 ${shadowColor}`,
+  8: `0 8px 10px 0 ${shadowColor}`,
+  9: `0 9px 12px 0 ${shadowColor}`,
+  12: `0 12px 17px 0 ${shadowColor}`,
+  16: `0 16px 24px 0 ${shadowColor}`,
+  24: `0 24px 38px 0 ${shadowColor}`,
 };
