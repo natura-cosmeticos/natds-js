8336540705a196e91aad352ed2da716c4323b20f
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-09-09 19:10:00 -0300

    docs(styles): add todo jsdoc to overlay tokens [DSY-1461]
    DSY-1461

--- packages/styles/src/tokens/overlay/index.ts
+++ packages/styles/src/tokens/overlay/index.ts
@@ -1,6 +1,9 @@
 /* eslint-disable sort-keys */
 import { IOverlay } from "./IOverlay";
 
+/**
+ * @todo refactor/move to `natds-themes`
+ */
 export const overlay: IOverlay = {
   solidBlack: 0.56,
 };
