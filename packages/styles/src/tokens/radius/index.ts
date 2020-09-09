13293e82b2b3dae141e364a6ff7abc6a2d165b75
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-09-09 19:09:31 -0300

    docs(styles): add deprecation and todo jsdocs to radius token [DSY-1461]
    DSY-1461

--- packages/styles/src/tokens/radius/index.ts
+++ packages/styles/src/tokens/radius/index.ts
@@ -1,8 +1,16 @@
 /* eslint-disable sort-keys */
 import { IRadius } from "./IRadius";
 
+/**
+ * @deprecated since 0.19.0.
+ * Please use `borderRadius` directly from your theme
+ */
 export const radius: IRadius = {
   none: 0,
+
+  /**
+   * @todo refactor/move to `natds-themes`
+   */
   circle: "50%",
   small: 2,
   medium: 4,
