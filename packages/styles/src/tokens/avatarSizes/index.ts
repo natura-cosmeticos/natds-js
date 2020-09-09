5da50b2ca3ba0be76d8c55a47a9bc01f0913ba61
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-09-09 19:16:47 -0300

    feat(styles): integrate avatar sizes tokens with `natds-themes` [DSY-1461]
    DSY-1461

--- packages/styles/src/tokens/avatarSizes/index.ts
+++ packages/styles/src/tokens/avatarSizes/index.ts
@@ -2,25 +2,48 @@
 import { IAvatarSizes } from "./IAvatarSizes";
 import { sizes } from "../sizes";
 
+/**
+ * @todo move/refactor to natds-themes
+ */
 export const avatarSizes: IAvatarSizes = {
   tiny: {
+
+    /**
+     * @todo move/refactor to natds-themes
+     */
     fontSize: ".625rem",
     size: sizes.standard,
   },
   small: {
+
+    /**
+     * @todo move/refactor to natds-themes
+     */
     fontSize: ".875rem",
     size: sizes.semi,
   },
   standard: {
+
+    /**
+     * @todo move/refactor to natds-themes
+     */
     fontSize: "1rem",
-    size: sizes.semix,
+    size: sizes.semiX,
   },
   large: {
+
+    /**
+     * @todo move/refactor to natds-themes
+     */
     fontSize: "1rem",
     size: sizes.medium,
   },
   huge: {
+
+    /**
+     * @todo move/refactor to natds-themes
+     */
     fontSize: "1.5rem",
-    size: sizes.largexx,
+    size: sizes.largeXX,
   },
 };
