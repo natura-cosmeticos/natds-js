5e8dc178f6792a65469a7c85eac2dd974c878e4d
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-09-09 18:54:10 -0300

    style(styles): format jest config with `object-curly-spacing` rule

--- packages/styles/jest.config.js
+++ packages/styles/jest.config.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const base = require("../../jest.config.base");
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const {displayName, name} = require("./package.json");
+const { displayName, name } = require("./package.json");
 
 module.exports = {
   ...base,
