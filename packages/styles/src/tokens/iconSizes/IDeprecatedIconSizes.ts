7a62f8269a3a5dfe26e345809a80eb540f7609de
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-09-09 19:03:48 -0300

    feat(styles): add new icon sizes token types with X suffix, deprecate lowercase ones [DSY-1461]
    DSY-1461
    
    New icon sizes token types:
    - largeX
    - largeXX
    - hugeX
    - hugeXX

---
+++ packages/styles/src/tokens/iconSizes/IDeprecatedIconSizes.ts
@@ -0,0 +1,30 @@
+/**
+ * @deprecated size keys `largex`, `largexx`, `hugex` and `hugexx` are deprecated since version 0.19
+ * and will be deleted in v1.0.
+ */
+export interface IDeprecatedIconSizes {
+
+  /**
+   * @deprecated `largex` key is deprecated since version 0.19 and will be deleted in version 1.0.
+   * In case you need `largeX` token, please use `largeX` (with capital X) instead of `largex`.
+   */
+  largex: number;
+
+  /**
+   * @deprecated `largexx` key is deprecated since version 0.19 and will be deleted in version 1.0.
+   * In case you need `largeXX` token, please use `largeXX` (with capital X) instead of `largexx`.
+   */
+  largexx: number;
+
+  /**
+   * @deprecated `hugex` key is deprecated since version 0.19 and will be deleted in version 1.0.
+   * In case you need `hugeX` token, please use `hugeX` (with capital X) instead of `hugex`.
+   */
+  hugex: number;
+
+  /**
+   * @deprecated `hugexx` key is deprecated since version 0.19 and will be deleted in version 1.0.
+   * In case you need `hugeXX` token, please use `hugeXX` (with capital X) instead of `hugexx`.
+   */
+  hugexx: number;
+}
--- packages/styles/src/tokens/iconSizes/IIconSizes.ts
+++ packages/styles/src/tokens/iconSizes/IIconSizes.ts
@@ -1,13 +1,31 @@
-export interface IIconSizes {
+import { IDeprecatedIconSizes } from "./IDeprecatedIconSizes";
+
+export interface IIconSizes extends IDeprecatedIconSizes {
   micro: number;
   tiny: number;
   small: number;
   standard: number;
   medium: number;
   large: number;
-  largex: number;
-  largexx: number;
+
+  /**
+   * @since 0.19.0
+   */
+  largeX: number;
+
+  /**
+   * @since 0.19.0
+   */
+  largeXX: number;
   huge: number;
-  hugex: number;
-  hugexx: number;
+
+  /**
+   * @since 0.19.0
+   */
+  hugeX: number;
+
+  /**
+   * @since 0.19.0
+   */
+  hugeXX: number;
 }
