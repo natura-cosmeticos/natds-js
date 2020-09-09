20f9792e1475eb28ec774293cd75dac27a983966
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-09-09 19:07:05 -0300

    feat(styles): integrate sizes with `natds-themes` [DSY-1461]
    DSY-1461

---
+++ packages/styles/src/tokens/sizes/deprecatedSizes.ts
@@ -0,0 +1,76 @@
+import theme from "@naturacosmeticos/natds-themes";
+import { IDeprecatedSizes } from "./IDeprecatedSizes";
+import { ISizes } from "./ISizes";
+
+const { size } : { size: ISizes } = theme.natura.light;
+
+/**
+ * @deprecated size keys `semix`, `mediumx`, `largex*`, `hugex*` and `veryhuge` are deprecated since version 0.19
+ * and will be deleted in v1.0.
+ */
+export const deprecatedSizes: IDeprecatedSizes = {
+
+  /**
+   * @deprecated `semix` key is deprecated since version 0.19 and will be deleted in version 1.0.
+   *
+   * In case you need `semiX` token, please use `semiX` (with capital X) instead of `semix`.
+   */
+  semix: size.semiX,
+
+  /**
+   * @deprecated `mediumx` key is deprecated since version 0.19 and will be deleted in version 1.0.
+   *
+   * In case you need `mediumX` token, please use `mediumX` (with capital X) instead of `mediumx`.
+   */
+  mediumx: size.mediumX,
+
+  /**
+   * @deprecated `largex` key is deprecated since version 0.19 and will be deleted in version 1.0.
+   *
+   * In case you need `largeX` token, please use `largeX` (with capital X) instead of `largex`.
+   */
+  largex: size.largeX,
+
+  /**
+   * @deprecated `largexx` key is deprecated since version 0.19 and will be deleted in version 1.0.
+   *
+   * In case you need `largeXX` token, please use `largeXX` (with capital X) instead of `largexx`.
+   */
+  largexx: size.largeXX,
+
+  /**
+   * @deprecated `largexxx` key is deprecated since version 0.19 and will be deleted in version 1.0.
+   *
+   * In case you need `largeXXX` token, please use `largeXXX` (with capital X) instead of `largexxx`.
+   */
+  largexxx: size.largeXXX,
+
+  /**
+   * @deprecated `hugex` key is deprecated since version 0.19 and will be deleted in version 1.0.
+   *
+   * In case you need `hugeX` token, please use `hugeX` (with capital X) instead of `hugex`.
+   */
+  hugex: size.hugeX,
+
+  /**
+   * @deprecated `hugexx` key is deprecated since version 0.19 and will be deleted in version 1.0.
+   *
+   * In case you need `hugeXX` token, please use `hugeXX` (with capital X) instead of `hugexx`.
+   */
+  hugexx: size.hugeXX,
+
+  /**
+   * @deprecated `hugexxx` key is deprecated since version 0.19 and will be deleted in version 1.0.
+   *
+   * In case you need `hugeXXX` token, please use `hugeXXX` (with capital X) instead of `hugexxx`.
+   */
+  hugexxx: size.hugeXXX,
+
+  /**
+   * @deprecated `veryhuge` key is deprecated since version 0.19 and will be deleted in version 1.0.
+   *
+   * In case you need `veryHuge` token, please use `veryHuge` (with capital H) instead of `veryhuge`.
+   */
+  veryhuge: size.veryHuge,
+
+};
--- packages/styles/src/tokens/sizes/index.ts
+++ packages/styles/src/tokens/sizes/index.ts
@@ -1,23 +1,11 @@
 /* eslint-disable sort-keys */
+import theme from "@naturacosmeticos/natds-themes";
 import { ISizes } from "./ISizes";
+import { deprecatedSizes } from "./deprecatedSizes";
 
-export const sizes: ISizes = {
-  none: 0,
-  micro: 4,
-  tiny: 8,
-  small: 16,
-  standard: 24,
-  semi: 32,
-  semix: 40,
-  medium: 48,
-  mediumx: 56,
-  large: 64,
-  largex: 72,
-  largexx: 80,
-  largexxx: 88,
-  huge: 96,
-  hugex: 128,
-  hugexx: 144,
-  hugexxx: 192,
-  veryhuge: 256,
+const { size } : { size: ISizes } = theme.natura.light;
+
+export const sizes : ISizes = {
+  ...size,
+  ...deprecatedSizes,
 };
