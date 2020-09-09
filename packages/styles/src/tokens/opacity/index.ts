8a7d22906103540ea829d87a2b4ae60ea73aa60f
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-09-09 19:12:20 -0300

    chore(styles): deprecate opacity number tokens [DSY-1461]
    DSY-1461
    
    @todo add new opacity tokens to `natds-styles`

--- packages/styles/src/tokens/opacity/index.ts
+++ packages/styles/src/tokens/opacity/index.ts
@@ -2,15 +2,70 @@
 import { IOpacity } from "./IOpacity";
 
 export const opacity: IOpacity = {
+
+  /**
+   * @deprecated since 0.19. It will be removed at.
+   * Please use `` key instead of ``.
+   */
   "00": 0,
+
+  /**
+   * @deprecated since 0.19. It will be removed at.
+   * Please use `` key instead of ``.
+   */
   "04": 0.04,
+
+  /**
+   * @deprecated since 0.19. It will be removed at.
+   * Please use `` key instead of ``.
+   */
   "08": 0.08,
+
+  /**
+   * @deprecated since 0.19. It will be removed at.
+   * Please use `` key instead of ``.
+   */
   12: 0.12,
+
+  /**
+   * @deprecated since 0.19. It will be removed at.
+   * Please use `` key instead of ``.
+   */
   16: 0.16,
+
+  /**
+   * @deprecated since 0.19. It will be removed at.
+   * Please use `` key instead of ``.
+   */
   24: 0.24,
+
+  /**
+   * @deprecated since 0.19. It will be removed at.
+   * Please use `` key instead of ``.
+   */
   32: 0.32,
+
+  /**
+   * @deprecated since 0.19. It will be removed at.
+   * Please use `` key instead of ``.
+   */
   48: 0.48,
+
+  /**
+   * @deprecated since 0.19. It will be removed at.
+   * Please use `` key instead of ``.
+   */
   56: 0.56,
+
+  /**
+   * @deprecated since 0.19. It will be removed at.
+   * Please use `` key instead of ``.
+   */
   64: 0.64,
+
+  /**
+   * @deprecated since 0.19. It will be removed at.
+   * Please use `` key instead of ``.
+   */
   80: 0.80,
 };
