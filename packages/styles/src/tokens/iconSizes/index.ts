de54812fb57a417a72e94cd6c71a6d5d8b9cfbdd
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-09-09 19:12:54 -0300

    feat(styles): integrate icon sizes with `natds-themes` [DSY-1461]
    DSY-1461

--- packages/styles/src/tokens/iconSizes/index.ts
+++ packages/styles/src/tokens/iconSizes/index.ts
@@ -8,10 +8,50 @@ export const iconSizes: IIconSizes = {
   small: sizes.semi,
   standard: sizes.medium,
   medium: sizes.large,
-  large: sizes.largex,
+  large: sizes.largeX,
+
+  /**
+   * @deprecated `largex` key is deprecated since version 0.19 and will be deleted in version 1.0.
+   * In case you need `largeX` token, please use `largeX` (with capital X) instead of `largex`.
+   */
   largex: sizes.huge,
-  largexx: sizes.hugex,
-  huge: sizes.hugexx,
-  hugex: sizes.hugexxx,
-  hugexx: sizes.veryhuge,
+
+  /**
+   * @since 0.19.0
+   */
+  largeX: sizes.huge,
+
+  /**
+   * @deprecated `largexx` key is deprecated since version 0.19 and will be deleted in version 1.0.
+   * In case you need `largeXX` token, please use `largeXX` (with capital X) instead of `largexx`.
+   */
+  largexx: sizes.hugeX,
+
+  /**
+   * @since 0.19.0
+   */
+  largeXX: sizes.hugeX,
+  huge: sizes.hugeXX,
+
+  /**
+   * @deprecated `hugex` key is deprecated since version 0.19 and will be deleted in version 1.0.
+   * In case you need `hugeX` token, please use `hugeX` (with capital X) instead of `hugex`.
+   */
+  hugex: sizes.hugeXXX,
+
+  /**
+   * @since 0.19.0
+   */
+  hugeX: sizes.hugeXXX,
+
+  /**
+   * @deprecated `hugexx` key is deprecated since version 0.19 and will be deleted in version 1.0.
+   * In case you need `hugeXX` token, please use `hugeXX` (with capital X) instead of `hugexx`.
+   */
+  hugexx: sizes.veryHuge,
+
+  /**
+   * @since 0.19.0
+   */
+  hugeXX: sizes.veryHuge,
 };
