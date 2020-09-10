f3f1ebbc4a316c3fa4dad126d922290adccfdf94
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-09-10 20:48:58 -0300

    docs(fab): add support for choosing variants, add deprecation message for disabled

--- packages/web/src/Components/Fab/Fab.argTypes.ts
+++ packages/web/src/Components/Fab/Fab.argTypes.ts
@@ -1,6 +1,7 @@
 import { FabProps } from "@material-ui/core";
-import colors from "./__fixtures__/colors";
-import sizes from "./__fixtures__/sizes";
+import { colors } from "./__fixtures__/colors";
+import { sizes } from "./__fixtures__/sizes";
+import { variants } from "./__fixtures__/variants";
 
 export const argTypes : Record<keyof FabProps | string, unknown> = {
   children: {
@@ -14,9 +15,9 @@ export const argTypes : Record<keyof FabProps | string, unknown> = {
       type: "inline-radio",
     },
   },
-  disabled: {
+  href: {
     control: {
-      type: "boolean",
+      type: "text",
     },
   },
   size: {
@@ -24,6 +25,23 @@ export const argTypes : Record<keyof FabProps | string, unknown> = {
       options: sizes,
       type: "inline-radio",
     },
+  },
+  variant: {
+    control: {
+      options: variants,
+      type: "inline-radio",
+    },
+  },
+  disabled: {
+    control: {
+      type: "boolean",
+    },
+    description: `
+\`disabled\` property was deprecated since version 0.19 and will be removed at 1.0.
+
+The FAB promotes action, and should not be displayed in a disabled state.
+If you want to present a FAB that does not perform an action, you should also present an explanation to the user.
+`,
   },
 };
 
