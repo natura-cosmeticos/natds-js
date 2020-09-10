68dd09553444e78663e9d6bd9b7d5d51a1194e86
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-09-09 22:29:57 -0300

    style(snackbar): format with eslint

--- packages/web/src/Components/Snackbar.tsx
+++ packages/web/src/Components/Snackbar.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent, forwardRef } from "react";
 import MaterialSnackbar, { SnackbarProps as ISnackbarProps } from "@material-ui/core/Snackbar";
-import { withTheme} from "@material-ui/core/styles";
+import { withTheme } from "@material-ui/core/styles";
 
 export { SnackbarProps as ISnackbarProps } from "@material-ui/core/Snackbar";
 
