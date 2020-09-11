94fc1bfcff8a92dc2511e2b9811e1e320c6da6b7
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-09-11 10:13:42 -0300

    test(fab): use object notation for getColorProp() args

--- packages/web/src/Components/Fab/getColorProp.test.ts
+++ packages/web/src/Components/Fab/getColorProp.test.ts
@@ -4,21 +4,21 @@ import { getColorProp } from "./getColorProp";
 describe("Fab color property getter", () => {
   describe("when color property is primary", () => {
     it("should return primary color", () => {
-      const actual = getColorProp("primary");
+      const actual = getColorProp({ color: "primary" });
 
       expect(actual).toBe("primary");
     });
   });
   describe("when color property is secondary", () => {
     it("should return secondary color", () => {
-      const actual = getColorProp("secondary");
+      const actual = getColorProp({ color: "secondary" });
 
       expect(actual).toBe("secondary");
     });
   });
   describe("when color property is light", () => {
     it("should return default color", () => {
-      const actual = getColorProp("light");
+      const actual = getColorProp({ color: "light" });
 
       expect(actual).toBe("default");
     });
@@ -26,7 +26,7 @@ describe("Fab color property getter", () => {
   describe("when color property is null", () => {
     it("should return null", () => {
       // @ts-ignore
-      const actual = getColorProp(null);
+      const actual = getColorProp({ color: null });
 
       expect(actual).toBe("default");
     });
@@ -34,7 +34,7 @@ describe("Fab color property getter", () => {
   describe("when color property is undefined", () => {
     it("should return null", () => {
       // @ts-ignore
-      const actual = getColorProp();
+      const actual = getColorProp({});
 
       expect(actual).toBe("default");
     });
@@ -42,7 +42,7 @@ describe("Fab color property getter", () => {
   describe("when color property is not a valid value", () => {
     it("should return null", () => {
       // @ts-ignore
-      const actual = getColorProp("not a valid value");
+      const actual = getColorProp({ color: "not a valid value" });
 
       expect(actual).toBe("default");
     });
