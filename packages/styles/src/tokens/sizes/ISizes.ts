81a6e3b4daf746286e7fb132588678132bc5c55f
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-09-11 18:45:03 -0300

    style(sizes token): fix ISize type syntax

--- packages/styles/src/tokens/sizes/ISizes.ts
+++ packages/styles/src/tokens/sizes/ISizes.ts
@@ -7,62 +7,62 @@ export interface ISizes extends Size {
    *
    * In case you need `semiX` token, please use `semiX` (with capital X) instead of `semix`.
    */
-  semix: number,
+  semix: number;
 
   /**
    * @deprecated `mediumx` key is deprecated since version 0.19 and will be deleted in version 1.0.
    *
    * In case you need `mediumX` token, please use `mediumX` (with capital X) instead of `mediumx`.
    */
-  mediumx: number,
+  mediumx: number;
 
   /**
    * @deprecated `largex` key is deprecated since version 0.19 and will be deleted in version 1.0.
    *
    * In case you need `largeX` token, please use `largeX` (with capital X) instead of `largex`.
    */
-  largex: number,
+  largex: number;
 
   /**
    * @deprecated `largexx` key is deprecated since version 0.19 and will be deleted in version 1.0.
    *
    * In case you need `largeXX` token, please use `largeXX` (with capital X) instead of `largexx`.
    */
-  largexx: number,
+  largexx: number;
 
   /**
    * @deprecated `largexxx` key is deprecated since version 0.19 and will be deleted in version 1.0.
    *
    * In case you need `largeXXX` token, please use `largeXXX` (with capital X) instead of `largexxx`.
    */
-  largexxx: number,
+  largexxx: number;
 
   /**
    * @deprecated `hugex` key is deprecated since version 0.19 and will be deleted in version 1.0.
    *
    * In case you need `hugeX` token, please use `hugeX` (with capital X) instead of `hugex`.
    */
-  hugex: number,
+  hugex: number;
 
   /**
    * @deprecated `hugexx` key is deprecated since version 0.19 and will be deleted in version 1.0.
    *
    * In case you need `hugeXX` token, please use `hugeXX` (with capital X) instead of `hugexx`.
    */
-  hugexx: number,
+  hugexx: number;
 
   /**
    * @deprecated `hugexxx` key is deprecated since version 0.19 and will be deleted in version 1.0.
    *
    * In case you need `hugeXXX` token, please use `hugeXXX` (with capital X) instead of `hugexxx`.
    */
-  hugexxx: number,
+  hugexxx: number;
 
   /**
    * @deprecated `veryhuge` key is deprecated since version 0.19 and will be deleted in version 1.0.
    *
    * In case you need `veryHuge` token, please use `veryHuge` (with capital H) instead of `veryhuge`.
    */
-  veryhuge: number,
+  veryhuge: number;
 
 }
