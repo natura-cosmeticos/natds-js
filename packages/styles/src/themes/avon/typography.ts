087058e50fcb8fd63d80f4d877b4e79aa537b424
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-09-11 21:13:06 -0300

    feat(typography token): integrate theme-bound typography with `natds-themes` [DSY-1462]
    DSY-1462

--- packages/styles/src/themes/avon/typography.ts
+++ packages/styles/src/themes/avon/typography.ts
@@ -1,12 +1,38 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable sort-keys */
+import themes from "@naturacosmeticos/natds-themes";
+
+const { typography: typographyFromTheme } = themes.avon.light;
+
+/**
+ * @deprecated `typography` token without a mode is deprecated since v0.20 and will be removed at v1.0.
+ * Please use `themes.avon.${mode}.typography` from `natds-themes, where `mode` can be `dark` or `light`.
+ */
 export const typography: any = {
-  fontFamily: "\"Roboto\", sans-serif",
-  fontFamilyBrand1: "\"Helvetica Now Display\"",
-  fontFamilyBrand2: "\"Helvetica Now Text\"",
+  fontFamily: `"${typographyFromTheme.fontFamily.primary}", ${typographyFromTheme.fontFamily.secondary}`,
+
+  /**
+   * @todo check the difference between "Helvetica Now" and "Helvetica Now Display" with UI Designers
+   */
+  fontFamilyBrand1: `"${typographyFromTheme.fontFamily.branding} Display"`,
+
+  /**
+   * @todo check the difference between "Helvetica Now" and "Helvetica Now Display" with UI Designers
+   */
+  fontFamilyBrand2: `"${typographyFromTheme.fontFamily.branding} Text"`,
   fontFamilyBrand3: null,
+
+  /**
+   * @deprecated `fontWeightLight` is deprecated since v0.20 and will be removed at v1.0.
+   * Please use `typography.fontWeight.regular` theme token from `natds-themes`.
+   */
   fontWeightLight: 300,
-  fontWeightRegular: 400,
-  fontWeightMedium: 500,
+  fontWeightRegular: typographyFromTheme.fontWeight.regular,
+  fontWeightMedium: typographyFromTheme.fontWeight.medium,
+
+  /**
+   * @deprecated `fontWeightBold` is deprecated since v0.20 and will be removed at v1.0.
+   * Please use `typography.fontWeight.medium` theme token from `natds-themes`.
+   */
   fontWeightBold: 700,
 };
