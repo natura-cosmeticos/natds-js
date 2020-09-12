b9a894203e17fc56906d3d3d1741dc37e562760b
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-09-11 21:25:02 -0300

    style(web): replace `require` by `import` to prevent eslint error

--- packages/web/config/test/setupEnzyme.ts
+++ packages/web/config/test/setupEnzyme.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { configure } = require("enzyme");
+import { configure } from "enzyme";
+
 const Adapter = require("enzyme-adapter-react-16");
 
 configure({ adapter: new Adapter() });
