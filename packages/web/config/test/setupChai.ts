22683ee7d43619fb01b02d0a105a7257031f0704
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-09-11 21:25:34 -0300

    docs(web): prevent importing warnings on setupChai.ts file

--- packages/web/config/test/setupChai.ts
+++ packages/web/config/test/setupChai.ts
@@ -1,5 +1,13 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+
+/**
+ * @todo Remove `chai`, `chai-enzyme` and `chai-jest-snapshot` dependencies
+ */
+// @ts-ignore
 import chai from "chai";
+// @ts-ignore
 import chaiEnzyme from "chai-enzyme";
+// @ts-ignore
 import chaiJestSnapshot from "chai-jest-snapshot";
 
 chai.use(chaiEnzyme());
