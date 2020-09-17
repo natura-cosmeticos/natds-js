b5a67f0542687ca49df1a018e53429f05d433c77
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-09-17 20:32:01 -0300

    docs(storybook): organize example, contributor and utilities stories

--- packages/web/.storybook/main.js
+++ packages/web/.storybook/main.js
@@ -4,8 +4,23 @@ const developerStories = [
   "../docs/using-cdn.stories.mdx",
 ];
 
+const exampleStories = [
+  "../docs/examples/sign-in.stories.tsx",
+];
+
 const contributorStories = [
   "../docs/contributing.pt-br.stories.mdx", "../docs/component-folder.stories.mdx",
+];
+
+const utilitiesStories = [
+  "../src/Components/Container/Container.stories.mdx",
+  "../src/Components/CssBaseline/CssBaseline.stories.mdx",
+  "../src/Components/GridContainer/GridContainer.stories.tsx",
+  "../src/Components/GridItem/GridItem.stories.tsx",
+  "../src/Components/ScopedCssBaseline/ScopedCssBaseline.stories.mdx",
+  "../src/Components/Spacing/Spacing.stories.tsx",
+  "../src/Components/Spacing/Spacing.margin.stories.tsx",
+  "../src/Components/Spacing/Spacing.padding.stories.tsx",
 ];
 
 module.exports = {
@@ -31,26 +46,21 @@ module.exports = {
     },
   ],
 
-  /**
-   * @todo include stories from /docs
-   */
   stories: [
+    ...exampleStories,
     "../src/Components/AppBar/AppBar.stories.tsx",
     "../src/Components/Avatar/Avatar.stories.tsx",
     "../src/Components/Badge/Badge.stories.tsx",
     "../src/Components/Button/Button.stories.tsx",
-    "../src/Components/Container/Container.stories.mdx",
-    "../src/Components/CssBaseline/CssBaseline.stories.mdx",
     "../src/Components/Fab/Fab.stories.tsx",
     "../src/Components/Icon/Icon.stories.tsx",
     "../src/Components/IconButton/IconButton.stories.tsx",
-    "../src/Components/ScopedCssBaseline/ScopedCssBaseline.stories.mdx",
+    "../src/Components/Image/Image.stories.tsx",
+    "../src/Components/Paper/Paper.stories.tsx",
     "../src/Components/Skeleton/Skeleton.stories.tsx",
-    "../src/Components/Spacing/Spacing.stories.tsx",
-    "../src/Components/Spacing/Spacing.margin.stories.tsx",
-    "../src/Components/Spacing/Spacing.padding.stories.tsx",
-    "../src/Components/Toolbar/Toolbar.padding.stories.tsx",
+    "../src/Components/Toolbar/Toolbar.stories.tsx",
     "../src/Components/Typography/Typography.stories.tsx",
+    ...utilitiesStories,
     ...developerStories,
     ...contributorStories,
   ],
