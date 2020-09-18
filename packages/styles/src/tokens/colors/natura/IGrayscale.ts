f2319f54030e62ea6f18209c35e6cfb3d5eae683
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-09-18 15:16:23 -0300

    chore(gitignore): ignore coverage, build and dist folders

---
+++ packages/styles/src/tokens/colors/natura/IGrayscale.ts
@@ -0,0 +1,11 @@
+/**
+ * @deprecated `IGrayscale` is deprecated since v0.20 and will be removed at v1.0.
+ * Please use `themes.natura.light` or `theme.natura.dark` interfaces.
+ */
+export interface IGrayscale {
+  colorBrdBlack: string;
+  colorBrdNatGray: string;
+  colorBrdNatGray1: string;
+  colorBrdNatGray2: string;
+  colorBrdNatGray3: string;
+}
---
+++ packages/styles/src/tokens/colors/natura/IPrimary.ts
@@ -0,0 +1,10 @@
+/**
+ * @deprecated `IPrimary` is deprecated since v0.20 and will be removed at v1.0.
+ * Please use `themes.natura.light` or `theme.natura.dark` interfaces.
+ */
+export interface IPrimary {
+  colorBrdNatPrimaryOrange: string;
+  colorBrdNatPrimaryYellow: string;
+  colorBrdNatPrimaryBrown: string;
+  colorBrdNatPrimaryWhite: string;
+}
