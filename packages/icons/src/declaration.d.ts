4b8a5e24029479eb31b8023eec258ef4b3f75bb5
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-09-18 15:21:04 -0300

    refactor(icons): refactor types declaration file

--- packages/icons/src/declaration.d.ts
+++ packages/icons/src/declaration.d.ts
@@ -1,6 +1,6 @@
-declare module "*.json";
 declare module "*.eot";
-declare module "*.ttf";
+declare module "*.json";
 declare module "*.svg";
+declare module "*.ttf";
 declare module "*.woff";
 declare module "*.woff2";
