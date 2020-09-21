e81fb1f6c4609b87727fcd695043e85f3caf27db
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-09-21 09:50:14 -0300

    fix(styles): integrate radius tokens with `natds-themes` [DSY-1462]
    DSY-1462

--- packages/styles/src/tokens/radius/index.ts
+++ packages/styles/src/tokens/radius/index.ts
@@ -1,18 +1,14 @@
 /* eslint-disable sort-keys */
+import themes from "@naturacosmeticos/natds-themes";
 import { IRadius } from "./IRadius";
+
+const { borderRadius } = themes.natura.light;
 
 /**
  * @deprecated since 0.19.0.
  * Please use `borderRadius` directly from your theme
  */
 export const radius: IRadius = {
-  none: 0,
-
-  /**
-   * @todo refactor/move to `natds-themes`
-   */
+  ...borderRadius,
   circle: "50%",
-  small: 2,
-  medium: 4,
-  large: 8,
 };
