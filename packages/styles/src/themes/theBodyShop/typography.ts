6e880dbe433de345f685306842bc62ca6e0cfc89
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-09-21 09:32:05 -0300

    fix(styles): integrate The Body Shop typography with `natds-themes` [DSY-1462]
    DSY-1462

--- packages/styles/src/themes/theBodyShop/typography.ts
+++ packages/styles/src/themes/theBodyShop/typography.ts
@@ -1,11 +1,38 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable sort-keys */
-export const typography = {
-  fontFamily: "\"Roboto\", sans-serif",
-  fontFamilyBrand1: "\"Helvetica Now Display\"",
-  fontFamilyBrand2: "\"Helvetica Now Text\"",
+import themes from "@naturacosmeticos/natds-themes";
+
+const { typography: typographyFromTheme } = themes.theBodyShop.light;
+
+/**
+ * @deprecated `typography` token without a mode is deprecated since v0.20 and will be removed at v1.0.
+ * Please use `themes.theBodyShop.${mode}.typography` from `natds-themes, where `mode` can be `dark` or `light`.
+ */
+export const typography: any = {
+  fontFamily: `"${typographyFromTheme.fontFamily.primary}", ${typographyFromTheme.fontFamily.secondary}`,
+
+  /**
+   * @todo check the difference between "Helvetica Now" and "Helvetica Now Display" with UI Designers
+   */
+  fontFamilyBrand1: `"${typographyFromTheme.fontFamily.branding} Display"`,
+
+  /**
+   * @todo check the difference between "Helvetica Now" and "Helvetica Now Display" with UI Designers
+   */
+  fontFamilyBrand2: `"${typographyFromTheme.fontFamily.branding} Text"`,
   fontFamilyBrand3: null,
+
+  /**
+   * @deprecated `fontWeightLight` is deprecated since v0.20 and will be removed at v1.0.
+   * Please use `typography.fontWeight.regular` theme token from `natds-themes`.
+   */
   fontWeightLight: 300,
-  fontWeightRegular: 400,
-  fontWeightMedium: 500,
+  fontWeightRegular: typographyFromTheme.fontWeight.regular,
+  fontWeightMedium: typographyFromTheme.fontWeight.medium,
+
+  /**
+   * @deprecated `fontWeightBold` is deprecated since v0.20 and will be removed at v1.0.
+   * Please use `typography.fontWeight.medium` theme token from `natds-themes`.
+   */
   fontWeightBold: 700,
 };
