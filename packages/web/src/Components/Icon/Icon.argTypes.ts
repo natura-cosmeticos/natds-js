db5dc26293446d40c69952e3f661a65e350e7702
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-10-28 21:24:25 -0300

    feat(icon): add support to "error" as color property

--- packages/web/src/Components/Icon/Icon.argTypes.ts
+++ packages/web/src/Components/Icon/Icon.argTypes.ts
@@ -1,42 +1,18 @@
-import * as iconNames from "@naturacosmeticos/natds-icons/dist/natds-icons.json";
+import { iconNames } from "@naturacosmeticos/natds-icons";
 import { tokens } from "@naturacosmeticos/natds-styles";
 import { IIconProps } from "./Icon.props";
-import { colors } from "./__fixtures__/colors";
 
 export const argTypes : Record<keyof IIconProps | string, unknown> = {
-  color: {
-    control: {
-
-      /**
-       * @todo add support to color tokens from Natura & Co. themes
-       */
-      options: colors,
-      type: "select",
-    },
-  },
   name: {
     control: {
       options: Object.keys(iconNames),
       type: "select",
-    },
-    description: "The Icon name",
-    table: {
-      defaultValue: {
-        summary: "\"filled-default-mockup\"",
-      },
-    },
-    type: {
-      required: true,
     },
   },
   size: {
     control: {
       options: Object.keys(tokens.iconSizes),
       type: "select",
-    },
-    description: "The Icon size",
-    type: {
-      required: false,
     },
   },
 };
--- packages/web/src/Components/Icon/Icon.stories.tsx
+++ packages/web/src/Components/Icon/Icon.stories.tsx
@@ -1,7 +1,7 @@
-import * as React from "react";
 import { Story } from "@storybook/react";
 import { Icon, IIconProps } from "./Icon";
 import { argTypes } from "./Icon.argTypes";
+import { Template, TemplateWithMultipleSizes } from "./Template";
 
 export default {
   argTypes,
@@ -9,86 +9,22 @@ export default {
   title: "Components/Icon",
 };
 
-const Template : Story<IIconProps> = (args) => <Icon {...args} />;
-
 export const Playground : Story<IIconProps> = Template.bind({});
-Playground.args = {};
-
-export const StandardAddIcon : Story<IIconProps> = Template.bind({});
-StandardAddIcon.args = {
-  name: "outlined-action-add",
-  size: "standard",
-};
 
 export const SmallIcon : Story<IIconProps> = Template.bind({});
 SmallIcon.args = {
-  ...Playground.args,
   size: "small",
-};
-
-export const SmallNaturaIcon : Story<IIconProps> = Template.bind({});
-SmallNaturaIcon.args = {
-  ...SmallIcon.args,
-  name: "filled-brand-naturarosacea",
-};
-
-export const SmallCloseIcon : Story<IIconProps> = Template.bind({});
-SmallCloseIcon.args = {
-  ...SmallIcon.args,
-  name: "outlined-navigation-close",
-};
-
-export const SmallAddIcon : Story<IIconProps> = Template.bind({});
-SmallAddIcon.args = {
-  ...SmallIcon.args,
-  name: "outlined-action-add",
 };
 
 export const TinyIcon: Story<IIconProps> = Template.bind({});
 TinyIcon.args = {
-  ...Playground.args,
   size: "tiny",
 };
 
-export const TinyMenuIcon: Story<IIconProps> = Template.bind({});
-TinyMenuIcon.args = {
-  ...TinyIcon.args,
-  name: "filled-navigation-menu",
+export const MicroIcon: Story<IIconProps> = Template.bind({});
+MicroIcon.args = {
+  size: "micro",
 };
 
-export const OutlinedPersonIcon : Story<IIconProps> = Template.bind({});
-OutlinedPersonIcon.args = {
-  name: "outlined-social-person",
-};
-
-export const OutlinedFavoriteIcon : Story<IIconProps> = Template.bind({});
-OutlinedFavoriteIcon.args = {
-  name: "outlined-action-favorite",
-};
-
-export const OutlinedDeleteIcon : Story<IIconProps> = Template.bind({});
-OutlinedDeleteIcon.args = {
-  name: "outlined-action-delete",
-};
-
-export const OutlinedEmailIcon : Story<IIconProps> = Template.bind({});
-OutlinedEmailIcon.args = {
-  name: "outlined-communication-email",
-};
-
-export const Sizes : Story<IIconProps> = (args) => (
-  <React.Fragment>
-    <Icon {...args} size={"micro"} />
-    <Icon {...args} size={"tiny"} />
-    <Icon {...args} size={"small"} />
-    <Icon {...args} size={"standard"} />
-    <Icon {...args} size={"medium"} />
-    <Icon {...args} size={"large"} />
-    <Icon {...args} size={"largex"} />
-    <Icon {...args} size={"largexx"} />
-    <Icon {...args} size={"huge"} />
-    <Icon {...args} size={"hugex"} />
-    <Icon {...args} size={"hugexx"} />
-  </React.Fragment>
-);
+export const Sizes : Story<IIconProps> = TemplateWithMultipleSizes.bind({});
 Sizes.args = Playground.args;
