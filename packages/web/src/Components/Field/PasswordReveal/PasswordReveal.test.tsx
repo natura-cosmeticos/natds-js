c536ae0e1864cd1cf5176dfc2852abe4b136f69c
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-10-29 01:44:00 -0300

    test(field): add more tests

--- packages/web/src/Components/Field/PasswordReveal/PasswordReveal.test.tsx
+++ packages/web/src/Components/Field/PasswordReveal/PasswordReveal.test.tsx
@@ -1,9 +1,25 @@
 import * as React from "react";
 import * as TestRenderer from "react-test-renderer";
+import { IPasswordRevealProps } from "./PasswordReveal.props";
 import PasswordReveal from "./PasswordReveal";
-import { Icon } from "../../Icon";
 
-describe("PasswordReveal component", () => {
+const getProps = (props: Partial<IPasswordRevealProps> = {}) => {
+  const {
+    hidePasswordIcon = () => <i className="hide" />,
+    onTogglePasswordReveal = jest.fn(),
+    showing = false,
+    showPasswordIcon = () => <i className="show" />,
+  } = props;
+
+  return {
+    hidePasswordIcon,
+    onTogglePasswordReveal,
+    showPasswordIcon,
+    showing,
+  };
+};
+
+describe("TextField PasswordReveal component", () => {
 
   describe("when showing property is false", () => {
     it("should match snapshot", () => {
@@ -59,4 +75,41 @@ describe("PasswordReveal component", () => {
     });
   });
 
+  it("should render received showPasswordIcon", () => {
+    const props = getProps({ showing: false });
+    const testRenderer = TestRenderer.create(<PasswordReveal {...props} />);
+
+    expect(testRenderer).toMatchSnapshot();
+  });
+
+  it("should render received hidePasswordIcon", () => {
+    const props = getProps({ showing: true });
+    const testRenderer = TestRenderer.create(<PasswordReveal {...props} />);
+
+    expect(testRenderer).toMatchSnapshot();
+  });
+
+  /**
+   * @todo rewrite test to support null type assignment
+   */
+  it("should render default showPasswordIcon", () => {
+    const props = getProps({ showing: false });
+    // eslint-disable-next-line no-undefined
+    const testRenderer = TestRenderer.create(<PasswordReveal {...props} showPasswordIcon={undefined} hidePasswordIcon={undefined} />);
+
+    expect(testRenderer).toMatchSnapshot();
+  });
+
+  /**
+   * @todo rewrite test to support null type assignment
+   */
+  test("should render default hidePasswordIcon", () => {
+    const props = getProps({ showing: true });
+    // eslint-disable-next-line no-undefined
+    const testRenderer = TestRenderer.create(<PasswordReveal {...props} showPasswordIcon={undefined} hidePasswordIcon={undefined} />);
+
+    expect(testRenderer).toMatchSnapshot();
+  });
+
+  it.todo("should call onTogglePasswordReveal callback after click");
 });
