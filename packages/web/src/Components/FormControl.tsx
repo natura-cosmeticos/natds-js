6c26187f4d21a168fcaf06073027483e28206e8d
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-10-28 21:23:28 -0300

    refactor(form control): refactor component, docs and tests

--- packages/web/src/Components/FormControl.tsx
+++
@@ -1,5 +0,0 @@
-import MaterialFormControl from "@material-ui/core/FormControl";
-
-export { FormControlProps as IFormControlProps } from "@material-ui/core/FormControl";
-
-export default MaterialFormControl;
---
+++ packages/web/src/Components/FormControl/FormControl.props.ts
@@ -0,0 +1,3 @@
+import { FormControlProps } from "@material-ui/core/FormControl";
+
+export type IFormControlProps = FormControlProps;
