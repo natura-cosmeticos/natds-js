f756d62319be30a3603b08c4e2b138516996b2d1
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-10-28 21:15:32 -0300

    refactor(divider): refactor component, docs and tests

---
+++ packages/web/src/Components/Divider/__fixtures__/variants.ts
@@ -0,0 +1,9 @@
+import { IDividerProps } from "../Divider.props";
+
+export const variants: Record<IDividerProps["variant"], IDividerProps["variant"]> = {
+  fullWidth: "fullWidth",
+  inset: "inset",
+  middle: "middle",
+};
+
+export default variants;
---
+++ packages/web/src/Components/Divider/index.ts
@@ -0,0 +1,5 @@
+import Divider from "./Divider";
+
+export { IDividerProps } from "./Divider.props";
+export { Divider };
+export default Divider;
