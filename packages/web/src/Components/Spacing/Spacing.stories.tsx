49ed9d8724f548ca345305cb792a6045a8297f25
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-10-29 01:36:51 -0300

    feat(spacing): add support to `display` and `style` properties

--- packages/web/src/Components/Spacing/Spacing.stories.tsx
+++ packages/web/src/Components/Spacing/Spacing.stories.tsx
@@ -2,6 +2,7 @@ import { Meta, Story } from "@storybook/react";
 import { ISpacingProps } from "./Spacing.props";
 import Template from "./Template";
 import meta from "./Spacing.stories.meta";
+import { LargeXHorizontalMargin, SmallHorizontalMargin, TinyVerticalMargin } from "./Spacing.margin.stories";
 
 export default {
   ...meta,
@@ -11,4 +12,16 @@ export default {
 export const Playground : Story<ISpacingProps> = Template.bind({});
 Playground.args = {
   children: "Try the margin and padding controls",
+};
+
+export const ForFullWidthSubheader : Story<ISpacingProps> = Template.bind({});
+ForFullWidthSubheader.args = {
+  ...SmallHorizontalMargin.args,
+  ...TinyVerticalMargin.args,
+};
+
+export const ForInsetSubheader : Story<ISpacingProps> = Template.bind({});
+ForInsetSubheader.args = {
+  ...LargeXHorizontalMargin.args,
+  ...TinyVerticalMargin.args,
 };
