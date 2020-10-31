343e257b69af0fa8c1f3eb2023b1660806dc3643
Jimmy Andrade <contato@jimmyandrade.com>
2020-10-31 14:25:47 -0300

    test(provider): remove enzyme dependency

--- packages/web/src/Provider/Provider.test.tsx
+++ packages/web/src/Provider/Provider.test.tsx
@@ -1,33 +1,43 @@
+import * as TestRenderer from "react-test-renderer";
+// eslint-disable-next-line no-use-before-define
 import * as React from "react";
-
-import { createMuiTheme, ThemeOptions } from "@material-ui/core";
-import { mount } from "enzyme";
 import { themes } from "../Themes";
 import { Provider } from "./Provider";
 
 describe("Provider component", () => {
-  it("should have the natura theme", () => {
-    const provider = mount(<Provider>Mocked</Provider>);
-    const muiTheme = createMuiTheme(themes.natura.light as unknown as ThemeOptions);
-    const theme = provider.find("ThemeProvider").prop("theme");
+  describe("when no theme was provided", () => {
+    it("should match snapshot", () => {
+      const testRenderer : TestRenderer.ReactTestRenderer = TestRenderer.create(
+        <Provider>Provider content</Provider>,
+      );
 
-    expect(JSON.stringify(theme)).toStrictEqual(JSON.stringify(muiTheme));
+      expect(testRenderer).toMatchSnapshot();
+    });
   });
+  describe("when Natura light theme is provided", () => {
+    it("should match snapshot", () => {
+      const testRenderer : TestRenderer.ReactTestRenderer = TestRenderer.create(
+        <Provider theme={themes.natura.light}>Provided content</Provider>,
+      );
 
-  it("should have the provided theme", () => {
-    const mockedTheme = themes.natura.dark;
+      expect(testRenderer).toMatchSnapshot();
+    });
+  });
+  describe("when customized Natura dark theme is provided", () => {
+    it("should match snapshot", () => {
+      const theme = themes.natura.dark;
 
-    mockedTheme.palette = {
-      ...mockedTheme.palette,
-      primary: {
-        main: "#000000",
-      },
-    };
+      theme.palette = {
+        ...theme.palette,
+        primary: {
+          main: "#000000",
+        },
+      };
+      const testRenderer : TestRenderer.ReactTestRenderer = TestRenderer.create(
+        <Provider theme={theme}>Provider content</Provider>,
+      );
 
-    const provider = mount(<Provider theme={mockedTheme}>Mocked</Provider>);
-    const theme = provider.find("ThemeProvider").prop("theme");
-
-    expect(JSON.stringify(theme)).toStrictEqual(JSON.stringify(createMuiTheme(mockedTheme as unknown as ThemeOptions)));
-
+      expect(testRenderer).toMatchSnapshot();
+    });
   });
 });
