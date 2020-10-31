7dd6a6d7f853267d12e40dd4a953cafa5b49be09
Jimmy Andrade <contato@jimmyandrade.com>
2020-10-31 15:58:00 -0300

    docs(tooltip): improve children prop description

--- packages/web/src/Components/Tooltip/Tooltip.props.ts
+++ packages/web/src/Components/Tooltip/Tooltip.props.ts
@@ -1,3 +1,15 @@
 import { TooltipProps } from "@material-ui/core/Tooltip";
 
-export type ITooltipProps = TooltipProps;
+export interface ITooltipProps extends TooltipProps {
+
+  /**
+   * Tooltip reference element.
+   *
+   * If you forget to set children for this element, the error
+   * `Cannot read property 'ref' of undefined` can occur.
+   *
+   * @required
+   * @type element
+   */
+  children: TooltipProps["children"]
+}
