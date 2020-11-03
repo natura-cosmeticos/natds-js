2a59bb6aad19168ee6ad505e70924b6aa1d33699
Jimmy Andrade <contato@jimmyandrade.com>
2020-11-03 00:14:05 -0300

    fix(list item icon): fix icon color for dark mode

--- packages/web/src/Components/ListItemIcon/ListItemIcon.tsx
+++ packages/web/src/Components/ListItemIcon/ListItemIcon.tsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-use-before-define
 import * as React from "react";
 import MaterialListItemIcon from "@material-ui/core/ListItemIcon";
 import { IListItemIconProps } from "./ListItemIcon.props";
