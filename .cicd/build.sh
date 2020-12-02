a5a8efeffb3cb0405fdb1250d91f588d588e94ac
Yasminn Vaz <yasminnvaz@gmail.com>
2020-12-02 15:35:54 -0300

    chore(pipe): adds some echos to track what is happen

--- .cicd/build.sh
+++ .cicd/build.sh
@@ -7,9 +7,12 @@ yarn install
 yarn build:storybook
 mkdir -p build
 
+echo "Verifying $1"
+
 if [ -z $1 ]; then
     OUTPUT_PATH=build/${FOLDER_NAME}
     mkdir -p ${OUTPUT_PATH}
+    echo "Created ${FOLDER_NAME} folder"
 fi
 
 cp -r packages/web/docs/dist/* ${OUTPUT_PATH}
