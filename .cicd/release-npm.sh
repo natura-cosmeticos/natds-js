46bfb1ced6784eb9c80701ea422d9a92b360a406
Yasminn Vaz <yasminnvaz@gmail.com>
2020-12-07 19:38:27 -0300

    chore: adds validation branch to release

--- .cicd/release-npm.sh
+++ .cicd/release-npm.sh
@@ -2,11 +2,22 @@
 
 echo "release NPM"
 
-BRANCH="DSY-1796"
-RELEASE_BRANCH="alpha.${BRANCH}.$BUILD_NUMBER"
+[[ -z "${CHANGE_BRANCH}" ]] && BRANCH=$BRANCH_NAME || BRANCH=$CHANGE_BRANCH
 
 NPM_TOKEN=$NATDS_NPM_TOKEN
 GH_TOKEN=$NATDS_GH_TOKEN
+RELEASE_BRANCH="alpha.${BRANCH}.$BUILD_NUMBER"
 
-yarn lerna:prerelease:version:ci --preid ${RELEASE_BRANCH}
-yarn lerna:prerelease:publish:ci --pre-dist-tag ${BRANCH}
+echo $BRANCH
+
+if [[ $BRANCH =~ ^DSY-([0-9]+)$ ]]; then
+    echo "$BRANCH pre-release"
+    yarn lerna:prerelease:version:ci --preid ${RELEASE_BRANCH}
+    yarn lerna:prerelease:publish:ci --pre-dist-tag ${BRANCH}
+elif [[ $BRANCH = "main" ]]; then
+    echo "Release"
+    yarn lerna:release:version:ci
+    yarn lerna:release:publish:ci
+else 
+    echo "Nothing to publish"
+fi
