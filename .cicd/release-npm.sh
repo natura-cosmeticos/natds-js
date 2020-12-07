29499c4fc3cfeb0f19e9a8fb9cf311611905fc25
Yasminn Vaz <yasminnvaz@gmail.com>
2020-12-07 17:15:13 -0300

    chore: fixes npm config

--- .cicd/release-npm.sh
+++ .cicd/release-npm.sh
@@ -8,7 +8,7 @@ RELEASE_BRANCH="alpha.${BRANCH}.$BUILD_NUMBER"
 NPM_TOKEN=$NATDS_NPM_TOKEN
 GH_TOKEN=$NATDS_GH_TOKEN
 
-npm config set '//registry.npmjs.org/:_authToken' "${NATDS_NPM_TOKEN}
+npm config set '//registry.npmjs.org/:_authToken' "${NPM_AUTH_TOKEN}"
 
 yarn lerna:prerelease:version:ci --preid ${RELEASE_BRANCH}
 yarn lerna:prerelease:publish:ci --pre-dist-tag ${BRANCH}
