a1773b1402adb96d782e9c03d12364da39cb41fa
Márcio Prando Teixeira <mprandot@gmail.com>
2021-01-08 12:25:25 -0300

    feat(Counter): add new Counter component and unit tests (#1265)
    feat(counter): create Counter component
    
    Co-authored-by: Márcio Teixeira <marcio.teixeira@compasso.com.br>
    Co-authored-by: Yasminn Vaz <yasminnvaz@gmail.com>
    Co-authored-by: Mirella Gadelha <mirella.gadelha.s@gmail.com>

---
+++ packages/web/src/Components/Counter/Counter.stories.tsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+import { Meta, Story } from "@storybook/react";
+import { Counter as component, ICounterProps } from "./Counter";
+import argTypes from "./Container.argTypes";
+
+const Counter : React.ForwardRefExoticComponent<ICounterProps> = React.lazy(() => import("./Counter"));
+
+export default {
+  argTypes,
+  component,
+  title: "Components/Counter",
+} as Meta;
+
+const Template : Story<ICounterProps> = (args: ICounterProps) => (
+  <div>
+    <Counter {...args}></Counter>
+  </div>
+);
+
+export const Playground : Story<ICounterProps> = Template.bind({});
+Playground.args = {
+  onChange: () => null,
+  onDecrement: () => null,
+  onIncrement: () => null,
+};
+
+export const SmallSize : Story<ICounterProps> = Template.bind({});
+SmallSize.args = {
+  ...Playground.args,
+  size: "small",
+};
+
+export const MediumSize : Story<ICounterProps> = Template.bind({});
+MediumSize.args = {
+  ...Playground.args,
+  size: "medium",
+};
+
+export const WithLabel : Story<ICounterProps> = Template.bind({});
+WithLabel.args = {
+  ...Playground.args,
+  size: "small",
+  label: "Quantidade",
+};
