e0c50783b0a2036717c71a37a2c83a968c17c634
Yasminn Vaz <yasminnvaz@gmail.com>
2021-01-22 16:00:27 -0300

    chore(styles): removes getDefaultThemes

--- packages/web/src/hooks/useDefaultTheme/index.ts
+++ packages/web/src/hooks/useDefaultTheme/index.ts
@@ -1,24 +1,10 @@
-import { isEqual } from "lodash";
-import { ThemeOptions, createMuiTheme, Theme } from "@material-ui/core/styles";
 import useTheme from "@material-ui/core/styles/useTheme";
-import { IThemeWeb, themes } from "../../Themes";
+import { ITheme } from "@naturacosmeticos/natds-styles";
 
-export const getDefaultTheme : (providerTheme: IThemeWeb | unknown) => Theme = (providerTheme) => {
-  const parsedProviderTheme = JSON.parse(JSON.stringify(providerTheme)),
-    parsedDefaultTheme = JSON.parse(JSON.stringify(createMuiTheme({})));
+export const useDefaultTheme = () => {
+  const providerTheme = useTheme<ITheme>();
 
-  if (isEqual(parsedProviderTheme, parsedDefaultTheme)) {
-    return createMuiTheme(themes.natura.light as ThemeOptions);
-  }
-
-  return providerTheme as Theme;
-
-};
-
-export const useDefaultTheme: () => Theme = () => {
-  const providerTheme = useTheme();
-
-  return getDefaultTheme(providerTheme);
+  return providerTheme;
 };
 
 export default useDefaultTheme;
