bb99709f5d21536d0163915d641f159b986f3a72
Yasminn Vaz <yasminnvaz@gmail.com>
2021-02-01 14:39:44 -0300

    chore(divider): removes unused variables

--- packages/web/src/Components/Divider/Divider.stories.tsx
+++ packages/web/src/Components/Divider/Divider.stories.tsx
@@ -9,8 +9,6 @@ import { Spacing } from "../Spacing";
 import { CaptionWithSecondaryText } from "../Typography/Typography.stories";
 import { Typography } from "../Typography";
 import { SemiXMarginY } from "../Spacing/Spacing.margin.stories";
-import { SemiPaddingTop } from "../Spacing/Spacing.padding.stories";
-import { ForFullWidthSubheader, ForInsetSubheader } from "../Spacing/Spacing.stories";
 
 const Divider:React.ForwardRefExoticComponent<IDividerProps> = React.lazy(() => import("../Divider"));
 
@@ -33,20 +31,6 @@ const Template : Story<IDividerProps> = (args: IDividerProps) => (
       <Typography {...CaptionWithSecondaryText.args}>This is a content before divider</Typography>
       <Divider {...args} />
       <Typography {...CaptionWithSecondaryText.args}>This is a content after divider</Typography>
-    </Spacing>
-  </Container>
-);
-
-const TemplateForSubheader : Story<IDividerProps> = (args: IDividerProps) => (
-  <Container {...FluidWithSmMaxSize.args}>
-    <Spacing {...SemiPaddingTop.args} className={useStyles().container}>
-      <Divider {...args} />
-      <Spacing
-        {...(args.variant === "inset" ? ForInsetSubheader.args : ForFullWidthSubheader.args)}>
-        <Typography {...CaptionWithSecondaryText.args}>
-          Subheader with {args.variant} variant
-        </Typography>
-      </Spacing>
     </Spacing>
   </Container>
 );
