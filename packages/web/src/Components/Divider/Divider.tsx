d1b9c73346b5561f92974d47ef073b7191abdc9d
Yasminn Vaz <yasminnvaz@gmail.com>
2021-02-01 10:24:14 -0300

    refactor(divider): refactoring divider
    affects: @naturacosmeticos/natds-web
    
    DSY-828

--- packages/web/src/Components/Divider/Divider.tsx
+++ packages/web/src/Components/Divider/Divider.tsx
@@ -3,15 +3,11 @@ import MaterialDivider from "@material-ui/core/Divider";
 import { IDividerProps } from "./Divider.props";
 
 /**
- * For more advanced usages, check [Material UI Divider docs](https://material-ui.com/components/dividers/)
- *
- * ## Importing
  *
  * ```
  * import { Divider } from "@naturacosmeticos/natds-web";
  * ```
  *
- * @see https://material-ui.com/components/dividers/
  */
 export const Divider = React.forwardRef<HTMLHRElement, IDividerProps>(
   (props: IDividerProps, ref) => (
--- packages/web/src/Components/Divider/__fixtures__/variants.ts
+++
@@ -1,9 +0,0 @@
-import { IDividerProps } from "../Divider.props";
-
-export const variants: Record<IDividerProps["variant"], IDividerProps["variant"]> = {
-  fullWidth: "fullWidth",
-  inset: "inset",
-  middle: "middle",
-};
-
-export default variants;
