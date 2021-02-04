a6b564c92e7419673b768aa55c341ddfb530563a
Yasminn Vaz <yasminnvaz@gmail.com>
2021-02-04 19:11:42 -0300

    chore: test pipeline

--- .cicd/skip-commit.sh
+++ .cicd/skip-commit.sh
@@ -3,7 +3,7 @@ set -e
 
 COMMIT_MESSAGE=$(git log -1 --pretty=%B)
 
-if [[ $COMMIT_MESSAGE =~ \[(skip-ci|ci-skip)\] ]]; then
+if [[ $COMMIT_MESSAGE =~ \[(skip-ci)\] ]]; then
   echo true
   exit 0
 fi
