c98d02b436219015df5858fa131239dc070d5693
Yasminn Vaz <yasminnvaz@gmail.com>
2021-03-03 18:23:32 -0300

    chore(natds-react): initial setup

---
+++ packages/react/jest.config.js
@@ -0,0 +1,15 @@
+const base = require("../../jest.config.base.js");
+const { displayName, name } = require("./package.json");
+
+module.exports = {
+  ...base,
+  collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}"],
+  errorOnDeprecated: true,
+  displayName,
+  name,
+  roots: ["<rootDir>/src"],
+  transform: {
+    "^.+\\.[tj]sx?$": "babel-jest",
+    "^.+\\.mdx?$": "@storybook/addon-docs/jest-transform-mdx",
+  },
+};
