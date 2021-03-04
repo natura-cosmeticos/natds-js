3225ff330fc1c690784327a25023bef1bf1945c2
Yasminn Vaz <yasminnvaz@gmail.com>
2021-03-04 13:56:10 -0300

    chore(natds-react): configs rollup

---
+++ packages/react/rollup.config.js
@@ -0,0 +1,51 @@
+import commonjs from "@rollup/plugin-commonjs";
+import babel from "@rollup/plugin-babel";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import nodePolyfills from "rollup-plugin-node-polyfills";
+
+const getPluginsForBrowser = () => [
+  nodeResolve({
+    preferBuiltins: true,
+  }),
+  commonjs({
+    include: ".png",
+  }),
+  babel({
+    babelHelpers: "bundled",
+    exclude: "node_modules/**",
+  }),
+  nodePolyfills(),
+];
+
+const getBrowserOutputConfig = ({
+  filePath = "",
+  globals,
+  name = "natdsWeb",
+}) => [
+  {
+    dir: `./dist/umd/${filePath}`,
+    format: "umd",
+    globals,
+    name,
+    sourcemap: true,
+  },
+];
+
+const globals = {
+  react: "React",
+};
+const external = Object.keys(globals);
+
+/**
+ * @see https://github.com/rollup/plugins/issues/243#issuecomment-595964778
+ */
+export default [
+  {
+    external,
+    input: "./dist/index.js",
+    output: getBrowserOutputConfig({
+      globals,
+    }),
+    plugins: getPluginsForBrowser(),
+  },
+];
