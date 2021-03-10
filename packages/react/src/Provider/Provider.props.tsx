1ba363c44b800d79578ccdf98cece98e5f089bb0
Yasminn Vaz <yasminnvaz@gmail.com>
2021-03-10 17:28:13 -0300

    chore(natds-react): setup storybook theme

---
+++ packages/react/src/Provider/Provider.props.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { BrandThemes, Themes } from '@naturacosmeticos/natds-themes';
+export interface IProviderProps {
+
+  children: React.ReactNode;
+
+  brand?: Themes;
+
+  mode?: BrandThemes
+}
