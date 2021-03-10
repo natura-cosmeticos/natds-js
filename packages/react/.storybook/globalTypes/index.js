befabd0259b9ac3064f77eae5b37b2150cff7e36
Yasminn Vaz <yasminnvaz@gmail.com>
2021-03-10 17:28:13 -0300

    chore(natds-react): setup storybook theme

---
+++ packages/react/.storybook/globalTypes/index.js
@@ -0,0 +1,46 @@
+export const globalTypes = {
+  brand: {
+    defaultValue: "natura",
+    description: "Global theme for components",
+    name: "Theme",
+    toolbar: {
+      items: [
+        {
+          title: "Aesop",
+          value: "aesop",
+        },
+        {
+          title: "Avon",
+          value: "avon",
+        },
+        {
+          title: "Natura",
+          value: "natura",
+        },
+        {
+          title: "The Body Shop",
+          value: "theBodyShop",
+        },
+      ],
+    },
+  },
+  mode: {
+    defaultValue: "light",
+    description: "Global theme for components",
+    name: "Mode",
+    toolbar: {
+      items: [
+        {
+          title: "Light",
+          value: "light",
+        },
+        {
+          title: "Avon",
+          value: "dark",
+        },
+      ],
+    },
+  }
+};
+
+export default globalTypes;
