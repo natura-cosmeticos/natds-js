57d4b02d580347fc4d07f2fecac1f0ff97aa13a9
Yasminn Vaz <yasminnvaz@gmail.com>
2021-03-10 17:28:13 -0300

    chore(natds-react): setup storybook theme

---
+++ packages/react/.storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons';
+import { dark, light } from "./theme";
+
+addons.setConfig({
+  theme: light,
+});
---
+++ packages/react/.storybook/theme.js
@@ -0,0 +1,19 @@
+import { create } from '@storybook/theming';
+import themes from '@naturacosmeticos/natds-themes';
+
+export const buildTheme = (theme, mode) => create({
+  appBg: theme.color.surface,
+  appContentBg: theme.color.background,
+  barBg: theme.color.surface,
+  barTextColor: theme.color.highEmphasis,
+  base: mode,
+  brandImage: 'https://placehold.it/350x150',
+  brandTitle: 'Natura Design System',
+  colorPrimary: theme.color.primary,
+  colorSecondary: theme.color.secondary,
+  textColor: theme.color.highEmphasis,
+  textInverseColor: theme.color.lowEmphasis,
+})
+
+export const dark = buildTheme(themes.natura.dark, 'dark');
+export const light = buildTheme(themes.natura.light, 'light');
