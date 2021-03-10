34e442ef86b0b3b52dc6b861a4782cdf2e4fa04f
Yasminn Vaz <yasminnvaz@gmail.com>
2021-03-10 17:30:06 -0300

    chore(natds-react): setup ThemeProvider

--- packages/react/.storybook/preview.js
+++ packages/react/.storybook/preview.js
@@ -1,3 +1,8 @@
+import ThemeDecorator from "./decorators/themeDecorator";
+
+export { globalTypes } from "./globalTypes";
+
+export const decorators = [ThemeDecorator]
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
---
+++ packages/react/src/Provider/Provider.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { ThemeProvider } from 'react-jss'
+import themes from '@naturacosmeticos/natds-themes';
+import { IProviderProps } from "Provider";
+
+export const Provider = ({ children, brand, mode }: IProviderProps) => {
+
+  const currentTheme = brand && mode ? themes[brand][mode] : themes.natura.light
+
+  return (
+    <ThemeProvider theme={currentTheme}>
+      {children}
+    </ThemeProvider>
+  )
+}
