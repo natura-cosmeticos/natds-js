3717f624304d46f5c755c3ec9d2c4b262415f0be
Yasminn Vaz <yasminnvaz@gmail.com>
2021-03-23 17:22:36 -0300

    chore(natds-js): adds an optional cssPrefix for ThemeProvider

--- packages/react/src/ThemeProvider/ThemeProvider.tsx
+++ packages/react/src/ThemeProvider/ThemeProvider.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import { ThemeProvider as Provider } from 'react-jss'
+import { ThemeProvider as Provider, JssProvider } from 'react-jss'
 import { ThemeProviderProps } from './ThemeProvider.props'
 import buildTheme from './buildTheme'
 
-const ThemeProvider = ({ children, brand, mode }: ThemeProviderProps) => (
-  <Provider theme={buildTheme(brand, mode)}>
-    {children}
-  </Provider>
+const ThemeProvider = ({
+  children, brand, mode, cssPrefix = 'natds'
+}: ThemeProviderProps) => (
+  <JssProvider classNamePrefix={cssPrefix}>
+    <Provider theme={buildTheme(brand, mode)}>
+      {children}
+    </Provider>
+  </JssProvider>
 )
 
 export default ThemeProvider
