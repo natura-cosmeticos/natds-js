be277f0199cdc4623f0942fd61471a6b56387170
Yasminn Vaz <yasminnvaz@gmail.com>
2021-04-07 17:06:18 -0300

    styles(button): remove boxShadow when is disabled

--- packages/react/src/components/Button/Button.styles.ts
+++ packages/react/src/components/Button/Button.styles.ts
@@ -65,6 +65,7 @@ const styles = createUseStyles((theme: Theme) => ({
     '&:disabled': {
       backgroundColor: (props) => (isContained(props) ? theme.color.lowEmphasis : 'transparent'),
       border: (props) => (isOutlined(props) && `1px solid ${theme.color.lowEmphasis}`),
+      boxShadow: theme.elevation.none.replace,
       cursor: 'default'
     }
   },
