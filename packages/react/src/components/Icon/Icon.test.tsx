3dad3d44713e2c7c85e8b3cd15cf9ceb62c2d084
Yasminn Vaz <yasminnvaz@gmail.com>
2021-04-12 10:48:59 -0300

    chore(icon): add tests for size and color props

--- packages/react/src/components/Icon/Icon.test.tsx
+++ packages/react/src/components/Icon/Icon.test.tsx
@@ -4,8 +4,24 @@ import renderWithTheme from '../../helpers/renderWithTheme'
 
 describe('Icon component', () => {
   it('should render correctly with default props', () => {
-    const { styles, component } = renderWithTheme(<Icon />)
+    const { styles, component: { getByTestId } } = renderWithTheme(<Icon />)
+    const component = getByTestId(/mockup/)
 
-    expect([styles.toString(), component.container]).toMatchSnapshot()
+    expect(component).toHaveStyle({ fontSize: 24, color: '#000000' })
+    expect([styles.toString(), component]).toMatchSnapshot()
+  })
+  it('should render correctly with `medium` size', () => {
+    const { styles, component: { getByTestId } } = renderWithTheme(<Icon size="medium" />)
+    const component = getByTestId(/mockup/)
+
+    expect(component).toHaveStyle({ fontSize: 48 })
+    expect([styles.toString(), component]).toMatchSnapshot()
+  })
+  it('should render correctly with `primary` color', () => {
+    const { styles, component: { getByTestId } } = renderWithTheme(<Icon color="primary" />)
+    const component = getByTestId(/mockup/)
+
+    expect(component).toHaveStyle({ color: '#f4ab34' })
+    expect([styles.toString(), component]).toMatchSnapshot()
   })
 })
