e2e552fd723b5a11d5a5de980700e70cb929e7d2
Yasminn Vaz <yasminnvaz@gmail.com>
2021-04-14 19:36:49 -0300

    feature(IconButton): add new component

---
+++ packages/react/src/components/IconButton/IconButton.styles.ts
@@ -0,0 +1,64 @@
+import { Theme } from '@naturacosmeticos/natds-themes'
+import { createUseStyles } from 'react-jss'
+import { IconButtonProps, IconButtonSize } from './IconButton.props'
+
+type IconButtonStyleProps = Required<Pick<IconButtonProps, 'backgroundStyle' | 'disabled' | 'size'>>
+
+const getBackgroundStyles = (theme: Theme, props: IconButtonStyleProps): string => {
+  const styles = {
+    none: 'transparent',
+    float: theme.color.surface,
+    overlay: theme.color.highlight
+  }
+  return styles[props.backgroundStyle]
+}
+
+export const getIconSize = (size: IconButtonSize): string => {
+  const iconSize = {
+    medium: 'semiX',
+    semi: 'standard',
+    semiX: 'semi'
+  }
+
+  return iconSize[size]
+}
+
+const styles = createUseStyles((theme: Theme) => ({
+  iconButtonContainer: {
+    alignItems: 'center',
+    backgroundColor: (props: IconButtonStyleProps) => getBackgroundStyles(theme, props),
+    border: 'none',
+    borderRadius: '50%',
+    boxShadow: ({ backgroundStyle }: IconButtonStyleProps) => backgroundStyle === 'float' && theme.elevation.medium,
+    cursor: 'pointer',
+    display: 'inline-flex',
+    height: ({ size }: IconButtonStyleProps) => theme.size[size],
+    justifyContent: 'center',
+    opacity: ({ backgroundStyle }: IconButtonStyleProps) => backgroundStyle === 'overlay' && theme.opacity.mediumHigh,
+    outline: 'none',
+    overflow: 'hidden',
+    position: 'relative',
+    width: ({ size }: IconButtonStyleProps) => theme.size[size],
+    '&:after': {
+      backgroundColor: theme.color.highlight,
+      content: '" "',
+      height: '100%',
+      left: 0,
+      opacity: 0,
+      position: 'absolute',
+      top: 0,
+      width: '100%'
+    },
+    '&:hover:not([disabled]):after': {
+      opacity: theme.opacity.lower
+    },
+    '&:focus:not([disabled]):after': {
+      opacity: theme.opacity.low
+    },
+    '&:disabled': {
+      cursor: 'default'
+    }
+  }
+}))
+
+export default styles
