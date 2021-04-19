0e69996fb0bcf7eb08cce10c3d5bd3bc2b8889d7
Yasminn Vaz <yasminnvaz@gmail.com>
2021-04-19 18:32:05 -0300

    test(button): add tests for button with icon

--- packages/react/src/components/Button/Button.test.tsx
+++ packages/react/src/components/Button/Button.test.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 import * as React from 'react'
 import { fireEvent } from '@testing-library/react'
 import Button from '.'
@@ -7,13 +8,13 @@ jest.mock('../Ripple/Ripple')
 
 describe('Button component', () => {
   it('should render correctly with default props', () => {
-    const { styles, component } = renderWithTheme(<Button label="button" />)
+    const { styles, component } = renderWithTheme(<Button text="button" onClick={() => ''} />)
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
   })
   it('should call onClick', () => {
     const onClickMock = jest.fn()
-    const { component: { getByTestId } } = renderWithTheme(<Button label="button" onClick={onClickMock} testID="btn-test" />)
+    const { component: { getByTestId } } = renderWithTheme(<Button text="button" onClick={onClickMock} testID="btn-test" />)
 
     fireEvent.click(getByTestId('btn-test'))
 
@@ -21,44 +22,54 @@ describe('Button component', () => {
   })
 
   it('should render correctly when the size is semi', () => {
-    const { styles, component } = renderWithTheme(<Button label="button" size="semi" />)
+    const { styles, component } = renderWithTheme(<Button text="button" size="semi" onClick={() => ''} />)
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
   })
   it('should render correctly when the size is medium', () => {
-    const { styles, component } = renderWithTheme(<Button label="button" size="medium" />)
+    const { styles, component } = renderWithTheme(<Button text="button" size="medium" onClick={() => ''} />)
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
   })
   it('should render correctly when the fullWidth is true', () => {
-    const { styles, component } = renderWithTheme(<Button label="button" fullWidth />)
+    const { styles, component } = renderWithTheme(<Button text="button" fullWidth onClick={() => ''} />)
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
   })
 
   it('should render correctly when the variant is contained and disabled is true', () => {
-    const { styles, component } = renderWithTheme(<Button label="button" disabled />)
+    const { styles, component } = renderWithTheme(<Button text="button" disabled onClick={() => ''} />)
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
   })
   it('should render correctly when the variant is outlined', () => {
-    const { styles, component } = renderWithTheme(<Button label="button" variant="outlined" />)
+    const { styles, component } = renderWithTheme(<Button text="button" variant="outlined" onClick={() => ''} />)
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
   })
   it('should render correctly when the variant is text', () => {
-    const { styles, component } = renderWithTheme(<Button label="button" variant="text" />)
+    const { styles, component } = renderWithTheme(<Button text="button" variant="text" onClick={() => ''} />)
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
   })
 
   it('should render correctly when the variant is text and is disabled', () => {
-    const { styles, component } = renderWithTheme(<Button label="button" variant="text" disabled />)
+    const { styles, component } = renderWithTheme(<Button text="button" variant="text" disabled onClick={() => ''} />)
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
   })
   it('should render correctly when the variant is outlined and is disabled', () => {
-    const { styles, component } = renderWithTheme(<Button label="button" variant="outlined" disabled />)
+    const { styles, component } = renderWithTheme(<Button text="button" variant="outlined" disabled onClick={() => ''} />)
+
+    expect([styles.toString(), component.container]).toMatchSnapshot()
+  })
+  it('should render icon to the left when the showIcon is true', () => {
+    const { styles, component } = renderWithTheme(<Button text="button" showIcon onClick={() => ''} />)
+
+    expect([styles.toString(), component.container]).toMatchSnapshot()
+  })
+  it('should render icon to the right when the showIcon is true and iconPosition is right', () => {
+    const { styles, component } = renderWithTheme(<Button text="button" showIcon iconPosition="right" onClick={() => ''} />)
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
   })
