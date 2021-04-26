fc85530e8cd2e1197782394f1f34b00a80b467ae
Laura Lucca <lauralucca@gmail.com>
2021-04-26 11:04:23 -0300

    feat(textfield): add new component

---
+++ packages/react/src/components/TextField/TextField.tsx
@@ -0,0 +1,33 @@
+import { ClassNames } from '@emotion/core'
+import React from 'react'
+import { TextFieldProps } from './TextField.props'
+import styles from './TextField.styles'
+
+const TextField = ({
+  action = 'none',
+  disabled = false,
+  feedback = 'none',
+  helperText,
+  placeholder,
+  readOnly = false,
+  required = false,
+  size = 'mediumX',
+  testID,
+  type = 'text'
+}: TextFieldProps): JSX.Element => {
+  const classes = styles({ disabled, size })
+
+  return (
+    <input
+      data-testid={testID}
+      className={classes.input}
+      disabled={disabled}
+      placeholder={placeholder}
+      readOnly={readOnly}
+      required={required}
+      type={type}
+    />
+  )
+}
+
+export default TextField
