2898b75c44030b149645ad0cb6774e6b63669bb0
Laura Lucca <lauralucca@gmail.com>
2021-04-26 18:05:48 -0300

    chore(textfield): add feedback prop

--- packages/react/src/components/TextField/TextField.styles.ts
+++ packages/react/src/components/TextField/TextField.styles.ts
@@ -4,10 +4,26 @@ import { TextFieldProps } from './TextField.props'
 
 type TextFieldStyleProps = Required<TextFieldProps>
 
+const getFeedbackColor = (theme: Theme, { feedback }: TextFieldStyleProps) => {
+  switch (feedback) {
+    case 'error':
+      return theme.color.alert
+    case 'success':
+      return theme.color.success
+    default:
+      return theme.color.lowEmphasis
+  }
+}
+
 const styles = createUseStyles((theme: Theme) => ({
   input: {
     boxSizing: 'border-box',
+    borderColor: (props: TextFieldStyleProps) => getFeedbackColor(theme, props),
     borderRadius: theme.borderRadius.medium,
+    borderStyle: 'solid',
+    fontFamily: [theme.typography.fontFamily.primary, theme.typography.fontFamily.secondary],
+    fontSize: 16,
+    letterSpacing: 0.51,
     height: ({ size }: TextFieldStyleProps) => theme.size[size],
     padding: theme.spacing.small,
     width: '100%',
@@ -15,6 +31,20 @@ const styles = createUseStyles((theme: Theme) => ({
       color: theme.color.mediumEmphasis,
       letterSpacing: 0.51
     }
+  },
+  helperText: {
+    color: (props: TextFieldStyleProps) => getFeedbackColor(theme, props),
+    fontFamily: [theme.typography.fontFamily.primary, theme.typography.fontFamily.secondary],
+    fontSize: 12,
+    letterSpacing: 0.38,
+    textAlign: 'left'
+  },
+  label: {
+    color: (props: TextFieldStyleProps) => getFeedbackColor(theme, props),
+    fontFamily: [theme.typography.fontFamily.primary, theme.typography.fontFamily.secondary],
+    fontSize: 14,
+    letterSpacing: 1.11,
+    textAlign: 'left'
   }
 }))
 
