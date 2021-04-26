645a22182e5a92b5b5ef3b09183c6a3334d0bb53
Laura Lucca <lauralucca@gmail.com>
2021-04-26 17:17:38 -0300

    chore(textfield): add size prop

--- packages/react/src/components/TextField/TextField.styles.ts
+++ packages/react/src/components/TextField/TextField.styles.ts
@@ -5,7 +5,17 @@ import { TextFieldProps } from './TextField.props'
 type TextFieldStyleProps = Required<TextFieldProps>
 
 const styles = createUseStyles((theme: Theme) => ({
-  input: { }
+  input: {
+    boxSizing: 'border-box',
+    borderRadius: theme.borderRadius.medium,
+    height: ({ size }: TextFieldStyleProps) => theme.size[size],
+    padding: theme.spacing.small,
+    width: '100%',
+    '&::placeholder': {
+      color: theme.color.mediumEmphasis,
+      letterSpacing: 0.51
+    }
+  }
 }))
 
 export default styles
