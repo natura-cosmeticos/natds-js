443dd1799d7180fa01b05d2b2bc2e3637f4bb81a
Laura Lucca <lauralucca@gmail.com>
2021-04-26 17:17:38 -0300

    chore(textfield): add size prop

--- packages/react/src/components/TextField/TextField.stories.tsx
+++ packages/react/src/components/TextField/TextField.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Story, Meta } from '@storybook/react'
 import { TextField, TextFieldProps } from '.'
+import StoryContainer from '../../helpers/StoryContainer'
 
 const componentStatus = `
 ---
@@ -8,8 +9,8 @@ const componentStatus = `
 **NOTE**: This component is available with the following attribute status:
 
 - **Size**
-  - ❌ \`medium\`
-  - ❌ \`mediumX\`
+  - ✅ \`medium\`
+  - ✅ \`mediumX\`
 - **Feedback**
   - ❌ \`error\`
   - ❌ \`success\`
@@ -39,4 +40,11 @@ export default {
 
 export const Playground: Story<TextFieldProps> = (args) => (
   <TextField {...args} />
+)
+
+export const Size: Story<TextFieldProps> = (args) => (
+  <StoryContainer>
+    <TextField {...args} />
+    <TextField {...args} size="medium" />
+  </StoryContainer>
 )
