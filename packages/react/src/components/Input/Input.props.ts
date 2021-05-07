9c672b7d0b91b2f28bf52b1f3569945e5606a2c6
Yasminn Vaz <yasminnvaz@gmail.com>
2021-05-07 12:14:12 -0300

    chore(text field): add properties documentation

--- packages/react/src/components/Input/Input.props.ts
+++ packages/react/src/components/Input/Input.props.ts
@@ -4,16 +4,54 @@ export type InputSize = keyof Pick<Size, 'medium' | 'mediumX'>
 export type InputType = 'text' | 'password' | 'multiline'
 
 export interface InputProps {
+  /**
+   * If `true`, the component is disabled.
+   * @default false
+   */
   disabled?: boolean,
+  /**
+   * Minimum number of rows to display when input type option is set to multiline.
+   * @default 3
+   */
   minRows?: number,
+  /**
+   * The onBlur attribute fires the moment that the element loses focus.
+   */
   onBlur: React.FocusEventHandler<HTMLInputElement | HTMLTextAreaElement>,
+  /**
+   * The onChange event occurs when the value of an element has been changed.
+   */
   onChange: React.ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>,
+  /**
+   * The onFocus event occurs when an element gets focus and is the opposite of the onblur event.
+   */
   onFocus: React.FocusEventHandler<HTMLInputElement | HTMLTextAreaElement>
+  /**
+   * The short hint displayed in the `input` before the user enters a value.
+   */
   placeholder?: string,
+  /**
+   * If `true`, the user cannot change the value of the field
+   * @default false
+   */
   readOnly?: boolean,
+  /**
+   * If `true`, the user can expand the text field multiline area
+   * @default false
+   */
   isResizable?: boolean,
+  /**
+   * The height of the input
+   * @default mediumX
+   */
   size?: InputSize,
-  testId?: string,
+  /**
+   * The type of the input
+   * @default text
+   */
   type?: InputType,
+  /**
+   * The value of the `input` element
+   */
   value?: string,
 }
