744ebc2071eec90af63db65ba36ac59a7242e5a5
Yasminn Vaz <yasminnvaz@gmail.com>
2021-05-10 17:21:29 -0300

    refactor(text field): extract wrapper + action to input component

--- packages/react/src/components/TextField/TextField.props.ts
+++ packages/react/src/components/TextField/TextField.props.ts
@@ -1,13 +1,6 @@
-import { IconName } from '@naturacosmeticos/natds-icons'
 import { InputProps } from 'components/Input'
 
-export type TextFieldFeedback = 'success' | 'error'
-
 type BaseProps = InputProps & {
-  /**
-   * The type of feedback to the user
-   */
-  feedback?: TextFieldFeedback
   /**
    * The helper text content.
    */
@@ -27,43 +20,4 @@ type BaseProps = InputProps & {
   testID?: string
 }
 
-export type TextFieldActionIcon = BaseProps & ({
-  /**
-   * If `icon`, will display an Icon Button to the right of TextField
-   */
-  action: 'icon'
-  /**
-   * Set the icon to be rendered.
-   * Check all available names in [Icon Library](https://ds.natura.design/28db352be/p/94367e-icon-library/b/6154b9)
-   * @default outlined-default-mockup
-   */
-  iconName: IconName
-  /**
-   * Use to define a text that explains the expected action
-   */
-  ariaLabel: string
-  /**
-   * Click handler
-   */
-  onClick: () => void
-})
-
-export type TextFieldActionImage = BaseProps & ({
-  /**
-   * If `image`, will display an image to the right of TextField
-   */
-  action: 'image'
-  /**
-   * Provides alternative information for an image if a user for some reason cannot view it
-   */
-  alt: string
-  /**
-   * The path for the image
-   */
-  src: string
-})
-
 export type TextFieldProps = BaseProps
-  & { action?: never }
-  | TextFieldActionIcon
-  | TextFieldActionImage
--- packages/react/src/components/TextField/TextField.styles.ts
+++ packages/react/src/components/TextField/TextField.styles.ts
@@ -6,17 +6,6 @@ import { TextFieldProps } from './TextField.props'
 
 type TextFieldStyleProps = Required<Pick<TextFieldProps, 'size' | 'feedback' | 'disabled' | 'readOnly' | 'action'>>
 
-const getFeedbackBorderColor = (theme: Theme, { feedback }: TextFieldStyleProps) => {
-  switch (feedback) {
-    case 'error':
-      return theme.color.alert
-    case 'success':
-      return theme.color.success
-    default:
-      return theme.color.lowEmphasis
-  }
-}
-
 const getFeedbackTextColor = (theme: Theme, { feedback, disabled }: TextFieldStyleProps) => {
   const defaultColor = disabled ? theme.color.lowEmphasis : theme.color.mediumEmphasis
 
@@ -30,45 +19,7 @@ const getFeedbackTextColor = (theme: Theme, { feedback, disabled }: TextFieldSty
   }
 }
 
-export const actionStyles = createUseStyles((theme: Theme) => ({
-  action: {
-    marginRight: ({ action }) => action === 'icon' && theme.spacing.tiny
-  },
-  actionImage: {
-    maxWidth: theme.size.large
-  }
-}))
-
-export const styles = createUseStyles((theme: Theme) => ({
-  inputContainer: {
-    position: 'relative',
-    backgroundColor: theme.color.surface,
-    border: '1px solid',
-    borderColor: (props: TextFieldStyleProps) => getFeedbackBorderColor(theme, props),
-    borderRadius: theme.borderRadius.medium,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    cursor: ({ disabled, readOnly }: TextFieldStyleProps) => (!disabled && !readOnly ? 'text' : 'default'),
-    '&:hover': {
-      borderColor: ({ disabled }: TextFieldStyleProps) => !disabled && theme.color.mediumEmphasis
-    },
-    '&:focus-within': {
-      border: '2px solid',
-      borderColor: theme.color.primary
-    },
-    '&:after': {
-      backgroundColor: theme.color.lowEmphasis,
-      content: '""',
-      height: '100%',
-      left: 0,
-      opacity: ({ readOnly }: TextFieldStyleProps) => (readOnly ? theme.opacity.disabledLow : 0),
-      position: 'absolute',
-      top: 0,
-      width: '100%',
-      pointerEvents: 'none'
-    }
-  },
+const styles = createUseStyles((theme: Theme) => ({
   label: {
     color: (props: TextFieldStyleProps) => getFeedbackTextColor(theme, props),
     display: 'inline-block',
@@ -91,3 +42,5 @@ export const styles = createUseStyles((theme: Theme) => ({
     }
   }
 }))
+
+export default styles
