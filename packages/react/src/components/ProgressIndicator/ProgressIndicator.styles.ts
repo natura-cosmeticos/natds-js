09a360af6a622f9ea63837773db351705c02542d
Yasminn Vaz <yasminnvaz@gmail.com>
2021-06-08 18:33:48 -0300

    chore(progress indicator): fix layer position

--- packages/react/src/components/ProgressIndicator/ProgressIndicator.styles.ts
+++ packages/react/src/components/ProgressIndicator/ProgressIndicator.styles.ts
@@ -3,27 +3,34 @@ import { Theme } from '@naturacosmeticos/natds-themes'
 import { ProgressIndicatorProps } from './ProgressIndicator.props'
 
 const styles = createUseStyles((theme: Theme) => ({
-  base: {
-    height: ({ size }: ProgressIndicatorProps) => size && theme.size[size],
-    width: ({ size }: ProgressIndicatorProps) => size && theme.size[size],
-    borderRadius: ({ variant }: ProgressIndicatorProps) => variant === 'circular' && '50%',
-    border: `4px solid ${theme.color.primary}`,
-    animation: '$spin 1.4s linear infinite',
-    borderLeftColor: 'transparent'
-  },
   layer: {
+    height: ({ size }: ProgressIndicatorProps) => size && theme.size[size] + 8,
+    width: ({ size }: ProgressIndicatorProps) => size && theme.size[size] + 8,
+    backgroundColor: ({ showLayer }: ProgressIndicatorProps) => showLayer && theme.color.surface,
+    borderRadius: '50%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: theme.size.micro
+  },
+  spinner: {
     height: ({ size }: ProgressIndicatorProps) => size && theme.size[size],
     width: ({ size }: ProgressIndicatorProps) => size && theme.size[size],
-    borderRadius: ({ variant }: ProgressIndicatorProps) => variant === 'circular' && '50%',
-    backgroundColor: ({ showLayer }: ProgressIndicatorProps) => showLayer && theme.color.surface,
-    padding: theme.spacing.micro
+    border: `${theme.size.micro}px solid ${theme.color.primary}`,
+    borderLeftColor: 'transparent',
+    borderTopColor: 'transparent',
+    borderRadius: '50%',
+    animation: '$spin 1.7s linear infinite'
   },
   '@keyframes spin': {
     '0%': {
       transform: 'rotate(0deg)'
     },
-    '100%': {
+    '75%': {
       transform: 'rotate(360deg)'
+    },
+    '100%': {
+      transform: 'rotate(720deg)'
     }
   }
 }))
