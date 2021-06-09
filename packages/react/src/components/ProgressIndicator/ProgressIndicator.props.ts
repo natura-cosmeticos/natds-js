29a4e8e680a3bfa3b6ca4a76f840d58191b3cc51
Yasminn Vaz <yasminnvaz@gmail.com>
2021-06-09 13:53:51 -0300

    chore(progress indicator): add ariaLabel propertie

--- packages/react/src/components/ProgressIndicator/ProgressIndicator.props.ts
+++ packages/react/src/components/ProgressIndicator/ProgressIndicator.props.ts
@@ -3,6 +3,12 @@ import { Size } from '@naturacosmeticos/natds-themes'
 export type ProgressIndicatorSize = keyof Pick<Size, 'standard' | 'semi' | 'medium' | 'large'>
 export interface ProgressIndicatorProps {
   /**
+   * Describe the purpose of Progress Indicator
+   * Read more [here](https://dequeuniversity.com/rules/axe/4.2/aria-progressbar-name?application=axeAPI)
+   */
+  ariaLabel: string
+
+  /**
    * Show a layer behind the progress indicator
    * @default false
    */
@@ -18,10 +24,4 @@ export interface ProgressIndicatorProps {
    * Optional ID for testing
    */
   testID?: string,
-
-  /**
-   * The Progress Indicator model
-   * @default circular
-   */
-  variant?: 'circular'
 }
--- packages/react/src/components/ProgressIndicator/ProgressIndicator.tsx
+++ packages/react/src/components/ProgressIndicator/ProgressIndicator.tsx
@@ -3,15 +3,15 @@ import { ProgressIndicatorProps } from './ProgressIndicator.props'
 import styles from './ProgressIndicator.styles'
 
 const ProgressIndicator = ({
+  ariaLabel,
   showLayer = false,
   size = 'medium',
-  testID = 'ds-progress-indicator',
-  variant = 'circular'
+  testID = 'ds-progress-indicator'
 }: ProgressIndicatorProps): JSX.Element => {
   const { layer, loader } = styles({ size, showLayer })
 
   return (
-    <div className={layer} role="progressbar" data-testid={testID}>
+    <div className={layer} role="progressbar" aria-label={ariaLabel} data-testid={testID}>
       <div className={loader}>
         <svg viewBox="25 25 50 50">
           <circle cx="50" cy="50" r="20" />
