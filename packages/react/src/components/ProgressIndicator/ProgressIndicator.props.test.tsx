663472fc2cc7ed6c9733348fb059267b253513cc
Yasminn Vaz <yasminnvaz@gmail.com>
2021-06-09 14:01:38 -0300

    chore(progress indicator): improve tests

--- packages/react/src/components/ProgressIndicator/ProgressIndicator.props.test.tsx
+++ packages/react/src/components/ProgressIndicator/ProgressIndicator.props.test.tsx
@@ -4,7 +4,12 @@ import ProgressIndicator from './ProgressIndicator'
 
 describe('Progress Indicator', () => {
   it('should render correctly with default props', () => {
-    const { styles, component } = renderWithTheme(<ProgressIndicator />)
+    const { styles, component } = renderWithTheme(<ProgressIndicator ariaLabel="ds-progress-indicator" />)
+
+    expect([styles.toString(), component.container]).toMatchSnapshot()
+  })
+  it('should render correctly with the given size', () => {
+    const { styles, component } = renderWithTheme(<ProgressIndicator size="standard" ariaLabel="ds-progress-indicator" />)
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
   })
