cb83d93c13b3f873542bc3856449756fd2ea0a1b
Yasminn Vaz <42593623+yasminnvaz@users.noreply.github.com>
2021-06-17 12:02:17 -0300

    feat: create alias to component (#1421)

--- packages/react/babel.config.js
+++ packages/react/babel.config.js
@@ -1,4 +1,13 @@
+const path = require('path')
+
+const defaultAlias = {
+  '@naturacosmeticos/natds-react': path.resolve(__dirname, './src/components')
+}
+
 module.exports = {
+  plugins: [
+    ['babel-plugin-module-resolver', { alias: defaultAlias }]
+  ],
   presets: [
     ['@babel/preset-env', { targets: { node: 'current' } }],
     '@babel/preset-typescript',
