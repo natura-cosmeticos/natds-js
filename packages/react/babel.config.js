9f9ce240519e0000f93fe5b7d759faf910a1c4f9
Yasminn Vaz <42593623+yasminnvaz@users.noreply.github.com>
2021-06-18 16:14:59 -0300

    Revert "feat: create alias to component (#1421)" (#1424)
    This reverts commit cb83d93c13b3f873542bc3856449756fd2ea0a1b.

--- packages/react/babel.config.js
+++ packages/react/babel.config.js
@@ -1,13 +1,4 @@
-const path = require('path')
-
-const defaultAlias = {
-  '@naturacosmeticos/natds-react': path.resolve(__dirname, './src/components')
-}
-
 module.exports = {
-  plugins: [
-    ['babel-plugin-module-resolver', { alias: defaultAlias }]
-  ],
   presets: [
     ['@babel/preset-env', { targets: { node: 'current' } }],
     '@babel/preset-typescript',
