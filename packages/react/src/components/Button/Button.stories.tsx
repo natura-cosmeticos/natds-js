36dfbaae25ffc5875b8d39fa78c8677c558cdd9d
Yasminn Vaz <yasminnvaz@gmail.com>
2021-06-18 12:05:13 -0300

    refactor(button): apply conditional props type

--- packages/react/src/components/Button/Button.stories.tsx
+++ packages/react/src/components/Button/Button.stories.tsx
@@ -39,7 +39,7 @@ export default {
 } as Meta
 
 export const Playground: Story<ButtonProps> = (args) => <Button {...args} onClick={() => console.log('clicked!')} />
-Playground.args = { text: 'button', iconName: 'outlined-default-mockup' }
+Playground.args = { text: 'button' }
 
 export const Variants: Story<ButtonProps> = (args) => (
   <StoryContainer>
@@ -72,10 +72,14 @@ Disabled.args = { ...Playground.args, disabled: true }
 export const Icon: Story<ButtonProps> = (args) => (
   <StoryContainer>
     <Button {...args} />
-    <Button {...args} iconPosition="left" />
   </StoryContainer>
 )
-Icon.args = { ...Playground.args, showIcon: true }
+Icon.args = {
+  ...Playground.args,
+  showIcon: true,
+  iconName: 'outlined-default-mockup',
+  iconPosition: 'right'
+}
 
 export const FullWidth: Story<ButtonProps> = Playground.bind({})
 FullWidth.args = { ...Playground.args, fullWidth: true }
