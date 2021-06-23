d994c7995d5a1ed8fd2da5eb2cdb64b939fb6e1e
arielwb <arielwb@gmail.com>
2021-06-23 16:20:52 -0300

    fix(natds-styles): add breakpoints to the theme interface
    co-author: @yasminnvaz

--- packages/web/src/Themes/IThemesWeb.ts
+++ packages/web/src/Themes/IThemesWeb.ts
@@ -1,9 +1,12 @@
 import { themes as styleThemes } from '@naturacosmeticos/natds-styles'
-import { IThemeWeb } from './IThemeWeb'
+import { IThemeWeb, IThemeWebBase } from './IThemeWeb'
 
-export type IThemesWeb<K extends keyof typeof styleThemes> = {
+export type IThemesWeb<K extends keyof typeof styleThemes> = IThemesModes<K, IThemeWeb>
+export type IThemesBase<K extends keyof typeof styleThemes> = IThemesModes<K, IThemeWebBase>
+
+export type IThemesModes<K extends keyof typeof styleThemes, T> = {
   [P in K]: {
-    light: IThemeWeb;
-    dark: IThemeWeb;
+    light: T;
+    dark: T;
   };
 };
--- packages/web/src/Themes/themes.ts
+++ packages/web/src/Themes/themes.ts
@@ -1,10 +1,13 @@
 import { themes as styleThemes } from '@naturacosmeticos/natds-styles'
-import { IThemesWeb } from './IThemesWeb'
+import { IThemesBase } from './IThemesWeb'
 import { ThemeBrandName } from './ThemeBrandName'
 import { parseTheme } from './parseTheme'
 import { createThemesObject } from './createThemesObject'
 
-export const themes = Object.keys(styleThemes).reduce((value: IThemesWeb<ThemeBrandName>, key: string) => {
+export const themes = Object.keys(styleThemes).reduce((
+  value: IThemesBase<ThemeBrandName>,
+  key: string
+) => {
   const result = value
 
   result[key as ThemeBrandName].dark = parseTheme(styleThemes[key as ThemeBrandName].dark)
