4f5955d249c2c32b08ccc0dbcf6cf879a718b31e
Yasminn Vaz <yasminnvaz@gmail.com>
2021-06-29 18:22:35 -0300

    feat(dialog): add new component

---
+++ packages/react/src/components/Dialog/DialogHeader/DialogHeader.props.ts
@@ -0,0 +1,11 @@
+export interface DialogHeaderProps {
+  /**
+   * Component children, usually the included sub-components.
+   */
+  children?: React.ReactNode,
+
+  /**
+   * The title of Dialog
+   */
+  title: string
+}
