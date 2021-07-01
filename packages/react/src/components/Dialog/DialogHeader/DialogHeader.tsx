647d4fd0debaf1c155a3af623ffbba47fe063398
Yasminn Vaz <yasminnvaz@gmail.com>
2021-07-01 17:19:44 -0300

    chore(dialog): add a11y attributes

--- packages/react/src/components/Dialog/DialogHeader/DialogHeader.tsx
+++ packages/react/src/components/Dialog/DialogHeader/DialogHeader.tsx
@@ -2,14 +2,12 @@ import React from 'react'
 import styles from './DialogHeader.styles'
 import { DialogHeaderProps } from './DialogHeader.props'
 
-const DialogHeader = ({
-  children, title
-}: DialogHeaderProps): JSX.Element => {
+const DialogHeader = ({ children, id, title }: DialogHeaderProps): JSX.Element => {
   const { dialogHeaderContainer, dialogHeader, dialogHeaderChildrenContainer } = styles()
 
   return (
     <div className={dialogHeaderContainer}>
-      <h1 className={dialogHeader}>{title}</h1>
+      <h2 id={id} className={dialogHeader}>{title}</h2>
       <div className={dialogHeaderChildrenContainer}>
         {children}
       </div>
