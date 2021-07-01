1ff577d5ca55e30ce872c6495d31fe3019f1b4a1
Yasminn Vaz <yasminnvaz@gmail.com>
2021-07-01 11:50:02 -0300

    fix(icon): change default color to be inherit color

--- packages/web/src/Components/Icon/__fixtures__/colors.ts
+++ packages/web/src/Components/Icon/__fixtures__/colors.ts
@@ -4,7 +4,7 @@ import { IIconProps } from '../Icon.props'
  * @todo add support to color tokens from Natura & Co. themes
  */
 export const colors : Record<IIconProps['color'], IIconProps['color']> = {
-  default: 'default',
+  default: 'inherit',
   error: 'error',
   inherit: 'inherit',
   primary: 'primary',
