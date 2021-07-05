7edd1927ef6bdb1daafbb14d4f9c7b291994e581
Yasminn Vaz <yasminnvaz@gmail.com>
2021-07-05 17:41:26 -0300

    chore(dialog): refactor DialogBody

--- packages/react/src/components/Dialog/DialogBody/DialogBody.props.ts
+++ packages/react/src/components/Dialog/DialogBody/DialogBody.props.ts
@@ -5,12 +5,6 @@ export interface DialogBodyProps {
   children: React.ReactNode
 
   /**
-  * The value must be the same as the one passed to
-  * the `aria-describedby` attribute.
-  */
-  id?: string
-
-  /**
    * Optional ID for testing
    */
   showDivider?: boolean
