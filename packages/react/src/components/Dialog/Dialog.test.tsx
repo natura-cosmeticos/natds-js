169b5cdd73d4e5fe3a478ab37173df3db00c7418
Yasminn Vaz <yasminnvaz@gmail.com>
2021-07-07 17:28:28 -0300

    chore(dialog): fix size of dialog when screen is smaller

--- packages/react/src/components/Dialog/Dialog.test.tsx
+++ packages/react/src/components/Dialog/Dialog.test.tsx
@@ -7,59 +7,58 @@ import { DialogHeader } from './DialogHeader'
 import { DialogFooter } from './DialogFooter'
 
 describe('Dialog', () => {
-  describe('AlertDialog Role', () => {
-    test('should render correctly with given children and default props', () => {
-      const { styles, component } = renderWithTheme(
-        <Dialog role="alertdialog" ariaDescribedBy="description">
-          <DialogBody>
-            <p id="description">something on the body</p>
-          </DialogBody>
-        </Dialog>
-      )
+  test('should render correctly with DialogBody and DialogFooter', () => {
+    const { styles, component } = renderWithTheme(
+      <Dialog role="alertdialog" ariaDescribedBy="description">
+        <DialogBody>
+          <p id="description">something on the body</p>
+        </DialogBody>
+        <DialogFooter>something on the footer</DialogFooter>
+      </Dialog>
+    )
 
-      expect([styles.toString(), component.container]).toMatchSnapshot()
-    })
+    expect([styles.toString(), component.container]).toMatchSnapshot()
   })
-  describe('Dialog role', () => {
-    test('should render correctly with given children', () => {
-      const { styles, component } = renderWithTheme(
-        <Dialog role="dialog" ariaLabelledBy="title" ariaDescribedBy="description">
-          <DialogHeader title="Example" id="title" />
-          <DialogBody>
-            <p id="description">something on the body</p>
-          </DialogBody>
-          <DialogFooter>something on the footer</DialogFooter>
-        </Dialog>
-      )
 
-      expect([styles.toString(), component.container]).toMatchSnapshot()
-    })
-    test('should render correctly with divider at DialogBody', () => {
-      const { styles, component } = renderWithTheme(
-        <Dialog role="dialog" ariaLabelledBy="title" ariaDescribedBy="description">
-          <DialogHeader title="Example" id="title" />
-          <DialogBody showDivider>
-            <p id="description">something on the body</p>
-          </DialogBody>
-          <DialogFooter>something on the footer</DialogFooter>
-        </Dialog>
-      )
+  test('should render correctly with DialogHeader, DialogBody and DialogFooter', () => {
+    const { styles, component } = renderWithTheme(
+      <Dialog role="dialog" ariaLabelledBy="title" ariaDescribedBy="description">
+        <DialogHeader title="Example" id="title" />
+        <DialogBody>
+          <p id="description">something on the body</p>
+        </DialogBody>
+        <DialogFooter>something on the footer</DialogFooter>
+      </Dialog>
+    )
 
-      expect([styles.toString(), component.container]).toMatchSnapshot()
-    })
+    expect([styles.toString(), component.container]).toMatchSnapshot()
+  })
 
-    test('should render correctly with given size', () => {
-      const { styles, component } = renderWithTheme(
-        <Dialog role="dialog" ariaLabelledBy="title" ariaDescribedBy="description" size="large">
-          <DialogHeader title="Example" id="title" />
-          <DialogBody showDivider>
-            <p id="description">something on the body</p>
-          </DialogBody>
-          <DialogFooter>something on the footer</DialogFooter>
-        </Dialog>
-      )
+  test('should render correctly the divider at DialogBody', () => {
+    const { styles, component } = renderWithTheme(
+      <Dialog role="dialog" ariaLabelledBy="title" ariaDescribedBy="description">
+        <DialogHeader title="Example" id="title" />
+        <DialogBody showDivider>
+          <p id="description">something on the body</p>
+        </DialogBody>
+        <DialogFooter>something on the footer</DialogFooter>
+      </Dialog>
+    )
 
-      expect([styles.toString(), component.container]).toMatchSnapshot()
-    })
+    expect([styles.toString(), component.container]).toMatchSnapshot()
+  })
+
+  test('should render correctly with the given size', () => {
+    const { styles, component } = renderWithTheme(
+      <Dialog role="dialog" ariaLabelledBy="title" ariaDescribedBy="description" size="large">
+        <DialogHeader title="Example" id="title" />
+        <DialogBody showDivider>
+          <p id="description">something on the body</p>
+        </DialogBody>
+        <DialogFooter>something on the footer</DialogFooter>
+      </Dialog>
+    )
+
+    expect([styles.toString(), component.container]).toMatchSnapshot()
   })
 })
