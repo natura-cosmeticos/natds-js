00319c0349aeac4e027f2a0bbb5007a3e3dfa82f
Yasminn Vaz <yasminnvaz@gmail.com>
2021-07-07 13:54:53 -0300

    chore(dialog): fix export

--- packages/react/src/components/Dialog/index.ts
+++ packages/react/src/components/Dialog/index.ts
@@ -1,18 +1,5 @@
 import Dialog from './Dialog'
-import DialogHeader from './DialogHeader'
-import DialogFooter from './DialogFooter'
-import DialogBody from './DialogBody'
 
-export {
-  Dialog,
-  DialogHeader,
-  DialogFooter,
-  DialogBody
-}
+export { Dialog }
 
-export default {
-  Dialog,
-  DialogHeader,
-  DialogFooter,
-  DialogBody
-}
+export default { Dialog }
