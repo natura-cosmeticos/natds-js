a862eac37a5e375bd5f026ddfea698996d0c3ad5
Yasminn Vaz <yasminnvaz@gmail.com>
2021-07-12 14:58:12 -0300

    chore(text field): add missing props

--- packages/web/src/Components/TextField/TextField.props.ts
+++ packages/web/src/Components/TextField/TextField.props.ts
@@ -1,12 +1,13 @@
 import * as React from 'react'
 import { StandardTextFieldProps } from '@material-ui/core'
+import { ISearchClearProps } from 'Components/Field/SearchClear'
 import { IInputStateHelpTextProviderProps } from '../InputStateHelpTextProvider'
 import { IThemeWeb } from '../../Themes'
 import { Mask, MaskFn, State } from '../Input/Input.props'
 
 export type Type = 'password' | 'search' | 'text' | React.InputHTMLAttributes<HTMLInputElement>['type'];
 
-export interface TextFieldProps extends Pick<StandardTextFieldProps, 'onChange' | 'onBlur' | 'onFocus'> {
+export interface TextFieldProps {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [propName: string]: any;
 
@@ -105,6 +106,36 @@ export interface TextFieldProps extends Pick<StandardTextFieldProps, 'onChange' 
    * @type "password" | "search" | "text" | string
    */
   type?: Type;
+
+  /**
+   * Event to clear input field on 'search' type
+   */
+  onClearSearch?: ISearchClearProps['onClearSearch']
+
+  /**
+  * Callback fired when the input is blurred.
+   */
+  onBlur?: StandardTextFieldProps['onBlur'];
+
+  /**
+   * Callback fired when the value is changed.
+   */
+  onChange?: StandardTextFieldProps['onChange'];
+
+  /**
+   * Callback fired when the value is focused.
+   */
+  onFocus?: StandardTextFieldProps['onFocus'];
+
+  /**
+   * The value of the `input` element, required for a controlled component.
+   */
+  value?: StandardTextFieldProps['value'];
+
+  /**
+   * The short hint displayed in the input before the user enters a value.
+   */
+  placeholder?: StandardTextFieldProps['placeholder']
 
 }
 
