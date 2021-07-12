dd811211eb72c3e086c841c5bd4b915a2d81464a
Yasminn Vaz <yasminnvaz@gmail.com>
2021-07-12 08:16:06 -0300

    fix(counter): remove all event clicks
    BREAKING CHANGE: the user must to provide all logic to the component

--- packages/web/src/Components/Counter/Counter.stories.tsx
+++ packages/web/src/Components/Counter/Counter.stories.tsx
@@ -1,42 +1,46 @@
+/* eslint-disable no-plusplus */
 import * as React from 'react'
 import { Meta, Story } from '@storybook/react'
 import { Counter as component, ICounterProps } from './Counter'
-import argTypes from './Container.argTypes'
 
-const Counter : React.ForwardRefExoticComponent<ICounterProps> = React.lazy(() => import('./Counter'))
+const Counter: React.ForwardRefExoticComponent<ICounterProps> = React.lazy(() => import('./Counter'))
 
 export default {
-  argTypes,
   component,
   title: 'Components/Counter'
 } as Meta
 
-const Template : Story<ICounterProps> = (args: ICounterProps) => (
-  <div>
-    <Counter {...args} />
-  </div>
-)
+const Template: Story<ICounterProps> = (args: ICounterProps) => {
+  const [value, setValue] = React.useState(args.value)
 
-export const Playground : Story<ICounterProps> = Template.bind({})
-Playground.args = {
-  onChange: () => null,
-  onDecrement: () => null,
-  onIncrement: () => null
+  const handleIncrement = () => setValue(value + 1)
+  const handleDecrement = () => setValue(value - 1)
+
+  return (
+    <div>
+      <Counter {...args} onIncrement={handleIncrement} onDecrement={handleDecrement} value={value} />
+    </div>
+  )
 }
 
-export const SmallSize : Story<ICounterProps> = Template.bind({})
+export const Playground: Story<ICounterProps> = Template.bind({})
+Playground.args = {
+  value: 0
+}
+
+export const SmallSize: Story<ICounterProps> = Template.bind({})
 SmallSize.args = {
   ...Playground.args,
   size: 'small'
 }
 
-export const MediumSize : Story<ICounterProps> = Template.bind({})
+export const MediumSize: Story<ICounterProps> = Template.bind({})
 MediumSize.args = {
   ...Playground.args,
   size: 'medium'
 }
 
-export const WithLabel : Story<ICounterProps> = Template.bind({})
+export const WithLabel: Story<ICounterProps> = Template.bind({})
 WithLabel.args = {
   ...Playground.args,
   size: 'small',
