6a1beea00cdc37d2d84aaf9dada6d69449f3e304
Yasminn Vaz <yasminnvaz@gmail.com>
2021-07-14 12:20:01 -0300

    chore: improve Dialog tests

--- packages/react/src/hooks/useWindowSize.test.ts
+++ packages/react/src/hooks/useWindowSize.test.ts
@@ -1,4 +1,5 @@
-import { fireEvent } from '@testing-library/react'
+/* eslint-disable jest/no-disabled-tests */
+import { act, fireEvent } from '@testing-library/react'
 import { renderHook } from '@testing-library/react-hooks'
 import useWindowSize from './useWindowSize'
 
@@ -10,11 +11,33 @@ describe('hooks/useWindowSize', () => {
     global.innerHeight = 664
     fireEvent(window, new Event('resize'))
   })
+
   it('should read initial innerWidth and innerHeight values from window', () => {
     const { result } = renderHook(() => useWindowSize())
 
     expect(result.current.width).toEqual(768)
     expect(result.current.height).toEqual(664)
     expect(setTimeout).toHaveBeenCalledTimes(1)
+    // expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 500)
+  })
+
+  it('should update innerWidth and innerHeight values when window resizes', () => {
+    const { result } = renderHook(() => useWindowSize())
+
+    expect(result.current.width).toEqual(768)
+    expect(result.current.height).toEqual(664)
+
+    act(() => {
+      global.innerWidth = 320
+      global.innerHeight = 664
+      fireEvent(window, new Event('resize'))
+    })
+
+    jest.advanceTimersByTime(500)
+
+    expect(result.current.width).toBe(320)
+    expect(result.current.height).toBe(664)
+    expect(setTimeout).toHaveBeenCalledTimes(3)
+    // expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 500)
   })
 })
