68239e4ca95b96229fed7c888b74ee74b593dfae
Yasminn Vaz <yasminnvaz@gmail.com>
2021-07-14 12:20:01 -0300

    chore: improve Dialog tests

--- packages/react/src/components/Dialog/Dialog.test.tsx
+++ packages/react/src/components/Dialog/Dialog.test.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable max-lines-per-function */
 import React from 'react'
+import { act, fireEvent } from '@testing-library/react'
 import renderWithTheme from '../../helpers/renderWithTheme'
 import { Dialog } from '.'
 import { DialogBody } from './DialogBody'
@@ -49,6 +50,27 @@ describe('Dialog', () => {
   })
 
   test('should render correctly with the given size', () => {
+    const { styles, component } = renderWithTheme(
+      <Dialog role="dialog" ariaLabelledBy="title" ariaDescribedBy="description" size="large">
+        <DialogHeader title="Example" id="title" />
+        <DialogBody showDivider>
+          <p id="description">something on the body</p>
+        </DialogBody>
+        <DialogFooter>something on the footer</DialogFooter>
+      </Dialog>
+    )
+
+    expect([styles.toString(), component.container]).toMatchSnapshot()
+  })
+
+  test('should render correctly when window is smaller than component', () => {
+    act(() => {
+      global.innerWidth = 320
+      global.innerHeight = 664
+
+      fireEvent(window, new Event('resize'))
+    })
+
     const { styles, component } = renderWithTheme(
       <Dialog role="dialog" ariaLabelledBy="title" ariaDescribedBy="description" size="large">
         <DialogHeader title="Example" id="title" />
