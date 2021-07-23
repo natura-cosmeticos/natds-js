3400d3ad21113a9d8e80cc0dfc6ac66d21dc3c40
Yasminn Vaz <yasminnvaz@gmail.com>
2021-07-23 16:57:29 -0300

    fix(drawer): change list type

--- packages/web/src/Components/DrawerMenu/DrawerMenu.props.ts
+++ packages/web/src/Components/DrawerMenu/DrawerMenu.props.ts
@@ -1,8 +1,7 @@
 import * as React from 'react'
-import { IDrawerMenuSectionProps } from './DrawerMenuSection'
 
 export interface IDrawerMenuProps {
   children?: React.ReactNode;
   component?: React.ElementType;
-  list?: [IDrawerMenuSectionProps];
+  list?: Array<unknown>;
 }
