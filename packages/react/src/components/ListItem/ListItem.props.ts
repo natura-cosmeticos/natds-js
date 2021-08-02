a1223dfafce540595bdc37ea3d00f6bb6e27ac30
Yasminn Vaz <yasminnvaz@gmail.com>
2021-08-02 16:56:24 -0300

    chore(list item): add missing attributes

--- packages/react/src/components/ListItem/ListItem.props.ts
+++ packages/react/src/components/ListItem/ListItem.props.ts
@@ -1,5 +1,7 @@
 import { DividerVariants } from 'components/Divider/Divider.props'
 
+export type ListItemFeedback = 'ripple' | 'selection'
+
 export interface ListItemProps {
   /**
    * The ListItem content
@@ -7,15 +9,22 @@ export interface ListItemProps {
   children: React.ReactNode
 
   /**
+   * If you pass any variant, will appear at the bottom of ListItem
    * @default none
    */
   divider?: DividerVariants
 
   /**
+   * If you set `selection`, the `ripple` effect will be disabled
+   * @default ripple
+   */
+  feedback?: ListItemFeedback
+
+  /**
    * An unique identifier. Will be pass to `key` attribute
    * Read more [here](https://reactjs.org/docs/lists-and-keys.html#keys)
    */
-  id: string | number
+  key: string | number
 
   /**
    * Click handler
@@ -23,8 +32,18 @@ export interface ListItemProps {
   onClick?: () => void
 
   /**
-   * If `true`, apply selected styling
-   * @default false
+   * Refs provide a way to access ListItem element
    */
+  ref?: React.Ref<never>
+
+  /**
+  * If `true`, apply selected styling
+  * @default false
+  */
   selected?: boolean
+
+  /**
+   * Optional ID for testing
+   */
+  testID?: string
 }
--- packages/react/src/components/ListItem/ListItem.test.tsx
+++ packages/react/src/components/ListItem/ListItem.test.tsx
@@ -1,16 +1,30 @@
 import React from 'react'
+import { fireEvent } from '@testing-library/react'
 import renderWithTheme from '../../helpers/renderWithTheme'
 import ListItem from './ListItem'
 
 describe('ListItem component', () => {
   it('should render correctly', () => {
-    const { styles, component } = renderWithTheme(<ListItem id="some-item">some item here</ListItem>)
+    const { styles, component } = renderWithTheme(<ListItem key="some-item">some item here</ListItem>)
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
   })
   it('should render correctly with divider', () => {
-    const { styles, component } = renderWithTheme(<ListItem id="some-item" divider="full-bleed">some item here</ListItem>)
+    const { styles, component } = renderWithTheme(<ListItem key="some-item" divider="full-bleed">some item here</ListItem>)
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
+  })
+  it('should render correctly when is selected', () => {
+    const { styles, component } = renderWithTheme(<ListItem key="some-item" selected>some item here</ListItem>)
+
+    expect([styles.toString(), component.container]).toMatchSnapshot()
+  })
+  it('should call onClick', () => {
+    const onClickMock = jest.fn()
+    const { component: { getByTestId } } = renderWithTheme(<ListItem key="some-item" testID="list-item-test" onClick={onClickMock}>some item here</ListItem>)
+
+    fireEvent.click(getByTestId('list-item-test'))
+
+    expect(onClickMock).toHaveBeenCalled()
   })
 })
