637fc22cbd384383cc532939bf7c4d16b4ee791a
Bruno Vinicius Felix Da Silva <brunoviniciustica@gmail.com>
2021-08-31 14:57:19 -0300

    chore(Counter): update stories

--- packages/react/src/components/Counter/Counter.stories.tsx
+++ packages/react/src/components/Counter/Counter.stories.tsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import { Story, Meta } from '@storybook/react'
 import { Counter, CounterProps } from '.'
+import StoryContainer from '../../helpers/StoryContainer'
 
 const componentStatus = `
 ---
 
 **NOTE FOR UXs**: This component is available in the following variants:
 
-  - ❌ \`standard\`
+  - ✅ \`standard\`
 
 With the following attribute status:
 
-- ❌ **Label**
-- ❌ **Size**
-- ❌ **Disabled**
+- ✅ **Label**
+- **Size**
+    - ✅ \`semiX\`
+    - ✅ \`medium\`
+- ✅ **Disabled**
 
 ---
 `
@@ -29,7 +32,7 @@ export default {
 } as Meta
 
 export const Playground: Story<CounterProps> = ({ value, ...args }) => {
-  const [amount, setAmount] = React.useState(value)
+  const [amount, setAmount] = React.useState(value || 0)
 
   const handleIncrement = () => setAmount(amount + 1)
   const handleDecrement = () => setAmount(amount - 1)
@@ -47,3 +50,28 @@ export const Playground: Story<CounterProps> = ({ value, ...args }) => {
 }
 
 Playground.args = { value: 0, label: 'label' }
+
+export const Sizes: Story<CounterProps> = (args) => {
+  const [counter1, setCounter1] = React.useState(0)
+  const [counter2, setCounter2] = React.useState(0)
+  return (
+    <StoryContainer>
+      <Counter
+        {...args}
+        value={counter1}
+        onIncrement={() => setCounter1(counter1 + 1)}
+        onDecrement={() => setCounter1(counter1 - 1)}
+      />
+      <Counter
+        {...args}
+        size="medium"
+        value={counter2}
+        onIncrement={() => setCounter2(counter2 + 1)}
+        onDecrement={() => setCounter2(counter2 - 1)}
+      />
+    </StoryContainer>
+  )
+}
+
+export const Disabled: Story<CounterProps> = (args) => <Counter {...args} />
+Disabled.args = { disabled: true }
