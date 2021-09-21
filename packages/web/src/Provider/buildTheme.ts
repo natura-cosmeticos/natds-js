5c023e1c31d98a5b2e54be9db93fae4c4c712d10
Yasminn Vaz <yasminnvaz@gmail.com>
2021-09-21 16:37:00 -0300

    chore: add buildTheme

---
+++ packages/web/src/Provider/buildTheme.ts
@@ -0,0 +1,24 @@
+import themes, { BrandThemes, Themes } from '@naturacosmeticos/natds-themes'
+
+export type ThemeMode = keyof BrandThemes
+export type Brand = keyof Themes
+export type CustomOption = 'typography'
+
+export const buildTheme = (brand: Brand, mode: ThemeMode, customOption: CustomOption) => {
+  if (!themes) throw new Error('Unable to load tokens dependency. Check the installation logs for errors')
+
+  const theme = themes[brand][mode]
+
+  if (customOption === 'typography') {
+    return {
+      ...theme,
+      typography: {
+        ...theme.typography,
+        fontFamily: theme.typography.body.regular.fontFamily,
+        fontWeight: theme.typography.body.regular.fontWeight
+      }
+    }
+  }
+
+  return theme
+}
