20d746590700a03a24339b78410b09844f5f23b5
Yasminn Vaz <yasminnvaz@gmail.com>
2021-10-07 11:20:13 -0300

    chore(natds-react): chnage export way
    affects: @naturacosmeticos/natds-react

--- packages/react/src/components/Dialog/index.ts
+++ packages/react/src/components/Dialog/index.ts
@@ -1,5 +1,10 @@
 import Dialog from './Dialog'
+import DialogBody from './DialogBody'
+import DialogHeader from './DialogHeader'
+import DialogFooter from './DialogFooter'
 
-export { Dialog }
+export {
+  Dialog, DialogBody, DialogFooter, DialogHeader
+}
 export { DialogProps } from './Dialog.props'
 export default { Dialog }
--- packages/react/src/components/Input/index.ts
+++ packages/react/src/components/Input/index.ts
@@ -1,5 +1,7 @@
 import Input from './Input'
+import InputAction from './InputAction'
+import InputHelperText from './InputHelperText'
 
-export { Input }
+export { Input, InputAction, InputHelperText }
 export { InputProps } from './Input.props'
 export default Input
