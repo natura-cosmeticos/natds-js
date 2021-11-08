787d576cb6ce5fefe527b48ad79701d914f61035
Yasminn Vaz <yasminnvaz@gmail.com>
2021-11-08 18:05:01 -0300

    fix(natds-web): exports DrawerMenu subcomponents
    affects: @naturacosmeticos/natds-web

--- packages/web/src/Components/DrawerMenu/index.ts
+++ packages/web/src/Components/DrawerMenu/index.ts
@@ -1,5 +1,11 @@
 import DrawerMenu from './DrawerMenu'
+import { DrawerMenuItem } from './DrawerMenuItem'
+import { DrawerMenuSection } from './DrawerMenuSection'
+import { MenuItemText } from './MenuItemText'
+import { BuildDrawerMenuItems } from './BuildDrawerMenuItems'
 
 export { IDrawerMenuProps } from './DrawerMenu.props'
-export { DrawerMenu }
+export {
+  DrawerMenu, DrawerMenuItem, DrawerMenuSection, MenuItemText, BuildDrawerMenuItems
+}
 export default DrawerMenu
