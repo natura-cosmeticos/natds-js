808000e9f2d1b81d49a849aba70550f102f6be8e
Yasminn Vaz <yasminnvaz@gmail.com>
2021-11-22 14:24:50 -0300

    chore(natds-web): adds unmountOnExit to DrawerMenu
    affects: @naturacosmeticos/natds-web

--- packages/web/src/Components/DrawerMenu/DrawerMenu.props.ts
+++ packages/web/src/Components/DrawerMenu/DrawerMenu.props.ts
@@ -1,8 +1,10 @@
 import * as React from 'react'
+import { CollapseProps } from '@material-ui/core'
 import { IDrawerMenuItemProps } from './DrawerMenuItem'
 
 export interface IDrawerMenuProps {
   children?: React.ReactNode;
   component?: React.ElementType;
   list?: Array<IDrawerMenuItemProps>;
+  unmountOnExit?: CollapseProps['unmountOnExit']
 }
