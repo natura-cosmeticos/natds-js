e40fe2543ee6b2e2e893e171182fe9dad135bd6f
Yasminn Vaz <yasminnvaz@gmail.com>
2022-02-03 17:52:00 -0300

    chore(natds-react): improve testing coverage

--- packages/react/src/components/Avatar/Avatar.test.tsx
+++ packages/react/src/components/Avatar/Avatar.test.tsx
@@ -1,25 +1,32 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import * as React from 'react'
 import Avatar from '.'
 import renderWithTheme from '../../helpers/renderWithTheme'
-import { convertIconSize } from './AvatarIcon'
+import AvatarIcon, { convertIconSize } from './AvatarIcon'
+import AvatarImage from './AvatarImage'
+import AvatarLabel from './AvatarLabel'
 
 describe('Avatar component', () => {
   it('should render correctly with default props', () => {
-    const { styles, component } = renderWithTheme(<Avatar type="icon" />)
+    // @ts-expect-error component without type
+    const { styles, component } = renderWithTheme(<Avatar />)
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
   })
   it('should render correctly with different size', () => {
-    const { styles, component } = renderWithTheme(<Avatar type="icon" size="semi" />)
+    // @ts-expect-error component without type
+    const { styles, component } = renderWithTheme(<Avatar size="semi" />)
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
   })
 
   describe('Label variant', () => {
     it('should render correctly when the variant is type label', () => {
-      const { styles, component } = renderWithTheme(<Avatar type="label" label="DS" />)
+      const component = renderWithTheme(<Avatar type="label" label="DS" />)
+      const subcomponent = renderWithTheme(<AvatarLabel type="label" label="DS" />)
 
-      expect([styles.toString(), component.container]).toMatchSnapshot()
+      expect([component.styles.toString(), component.component.container]).toMatchSnapshot()
+      expect([subcomponent.styles.toString(), subcomponent.component.container]).toMatchSnapshot()
     })
 
     it('should render correctly when the variant is type label with default label', () => {
@@ -32,9 +39,11 @@ describe('Avatar component', () => {
 
   describe('Icon variant', () => {
     it('should render correctly when the variant is type icon', () => {
-      const { styles, component } = renderWithTheme(<Avatar type="icon" />)
+      const component = renderWithTheme(<Avatar type="icon" />)
+      const subcomponent = renderWithTheme(<AvatarIcon type="icon" />)
 
-      expect([styles.toString(), component.container]).toMatchSnapshot()
+      expect([component.toString(), component.component.container]).toMatchSnapshot()
+      expect([subcomponent.toString(), subcomponent.component.container]).toMatchSnapshot()
     })
     it('should render correctly when the variant is type icon with the given iconName', () => {
       const { styles, component } = renderWithTheme(<Avatar type="icon" name="outlined-action-like" />)
@@ -45,9 +54,11 @@ describe('Avatar component', () => {
 
   describe('Image variant', () => {
     it('should render correctly with image variant', () => {
-      const { styles, component } = renderWithTheme(<Avatar type="image" src="https://" alt="" />)
+      const component = renderWithTheme(<Avatar type="image" src="https://" alt="" />)
+      const subcomponent = renderWithTheme(<AvatarImage type="image" src="https://" alt="" />)
 
-      expect([styles.toString(), component.container]).toMatchSnapshot()
+      expect([component.styles.toString(), component.component.container]).toMatchSnapshot()
+      expect([subcomponent.styles.toString(), subcomponent.component.container]).toMatchSnapshot()
     })
   })
 })
