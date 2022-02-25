dc7a2a8ecab88d21c9e413f9cb46aaa856508af1
mattosdavi <davi.mattos@thoughtworks.com>
2022-02-25 16:55:45 -0300

    refactor(natds-react): remove Icon dependency on IconButton
    affects: @naturacosmeticos/natds-react
    
    DSY-2647

--- packages/react/src/components/IconButton/IconButton.tsx
+++ packages/react/src/components/IconButton/IconButton.tsx
@@ -1,43 +1,19 @@
 import React from 'react'
-import { IconColor, IconSize } from '../Icon/Icon.props'
-import { Icon } from '../Icon'
 import IconButtonBase from './IconButtonBase'
-import { IconButtonColors, IconButtonProps, IconButtonSize } from './IconButton.props'
+import { IconButtonProps } from './IconButton.props'
 import styles from './IconButton.styles'
-
-export const getIconSize = (size: IconButtonSize) => {
-  const iconSize = {
-    medium: 'semiX',
-    semi: 'standard',
-    semiX: 'semi'
-  }
-
-  return iconSize[size]
-}
-
-export const checkIconColor = (
-  backgroundStyle: string, isDisabled: boolean, color: IconButtonColors
-) => {
-  if (isDisabled) {
-    return backgroundStyle === 'overlay' ? 'lowEmphasis' : 'mediumEmphasis'
-  }
-  return color
-}
 
 const IconButton = React.forwardRef<HTMLButtonElement, IconButtonProps>(({
   ariaLabel,
   backgroundStyle = 'none',
   className = '',
-  color = 'highEmphasis',
   disabled = false,
-  iconName,
+  IconComponent,
   onClick,
   size = 'semi',
   testID
 }, ref) => {
   const { iconButtonContainer } = styles({ disabled, backgroundStyle, size })
-  const iconSize = getIconSize(size) as IconSize
-  const iconColor = checkIconColor(backgroundStyle, disabled, color) as IconColor
 
   return (
     <IconButtonBase
@@ -48,15 +24,7 @@ const IconButton = React.forwardRef<HTMLButtonElement, IconButtonProps>(({
       ref={ref}
       size={size}
       testID={testID}
-      IconComponent={(
-        <Icon
-          ariaHidden
-          color={iconColor}
-          name={iconName}
-          role="button"
-          size={iconSize}
-        />
-      )}
+      IconComponent={IconComponent}
     />
   )
 })
