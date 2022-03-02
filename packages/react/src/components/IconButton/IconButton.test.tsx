756973f7e1855cfd2e06c0541c1cdd8ba9951657
mattosdavi <davi.mattos@thoughtworks.com>
2022-03-02 14:29:56 -0300

    chore(natds-react): simplifying component rendering in tests
    affects: @naturacosmeticos/natds-react
    
    DSY-2647

--- packages/react/src/components/IconButton/IconButton.test.tsx
+++ packages/react/src/components/IconButton/IconButton.test.tsx
@@ -1,126 +1,94 @@
 import * as React from 'react'
 import { fireEvent } from '@testing-library/react'
+import { Size, Color } from '@naturacosmeticos/natds-themes'
 import renderWithTheme from '../../helpers/renderWithTheme'
-import { IconButtonProps } from './IconButton.props'
 import IconButton from '.'
 import Icon from '../Icon'
+import { IconButtonSize } from './IconButton.props'
 
-const iconButtonProps: IconButtonProps = {
-  IconComponent: <Icon size="standard" name="outlined-default-mockup" color="highEmphasis" />,
-  onClick: () => '',
-  ariaLabel: ''
+type IconButtonTestProps = {
+  iconSize?: keyof Size
+  iconColor?: keyof Color
+  onClick?: () => void
+  disabled?: boolean
+  testID?: string
+  backgroundStyle?: 'none' | 'float' | 'overlay'
+  size?: IconButtonSize
+}
+
+const renderIconButton = (props: IconButtonTestProps) => {
+  const { iconSize = 'standard', iconColor = 'highEmphasis', ...rest } = props
+
+  return (
+    <IconButton
+      IconComponent={<Icon size={iconSize} name="outlined-default-mockup" color={iconColor as keyof Color} />}
+      onClick={() => ({})}
+      ariaLabel=""
+      {...rest}
+    />
+  )
 }
 
 describe('Icon Button component', () => {
   it('should render correctly with default props', () => {
-    const { styles, component } = renderWithTheme(<IconButton {...iconButtonProps} />)
+    const { styles, component } = renderWithTheme(renderIconButton({}))
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
   })
   it('should call onClick', () => {
     const onClickMock = jest.fn()
-    const { component: { getByTestId } } = renderWithTheme(<IconButton {...iconButtonProps} onClick={onClickMock} testID="btn-test" />)
+    const { component: { getByTestId } } = renderWithTheme(renderIconButton({ testID: 'btn-test', onClick: onClickMock }))
 
     fireEvent.click(getByTestId('btn-test'))
 
     expect(onClickMock).toHaveBeenCalled()
   })
   it('should render correctly when is disabled', () => {
-    const { styles, component } = renderWithTheme(
-      <IconButton
-        {...iconButtonProps}
-        disabled
-        IconComponent={<Icon size="standard" name="outlined-default-mockup" color="mediumEmphasis" />}
-      />
-    )
+    const { styles, component } = renderWithTheme(renderIconButton({ iconColor: 'mediumEmphasis', disabled: true }))
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
   })
   it('should render correctly when the backgroundStyle is float', () => {
-    const { styles, component } = renderWithTheme(
-      <IconButton
-        {...iconButtonProps}
-        backgroundStyle="float"
-        IconComponent={<Icon size="standard" name="outlined-default-mockup" color="highEmphasis" />}
-      />
-    )
+    const { styles, component } = renderWithTheme(renderIconButton({ backgroundStyle: 'float' }))
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
   })
   it('should render correctly when the backgroundStyle is float and is disabled', () => {
     const { styles, component } = renderWithTheme(
-      <IconButton
-        {...iconButtonProps}
-        backgroundStyle="float"
-        disabled
-        IconComponent={<Icon size="standard" name="outlined-default-mockup" color="mediumEmphasis" />}
-      />
+      renderIconButton({ backgroundStyle: 'float', disabled: true, iconColor: 'mediumEmphasis' })
     )
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
   })
   it('should render correctly when the backgroundStyle is overlay', () => {
-    const { styles, component } = renderWithTheme(
-      <IconButton
-        {...iconButtonProps}
-        backgroundStyle="overlay"
-        IconComponent={<Icon size="standard" name="outlined-default-mockup" color="highEmphasis" />}
-      />
-    )
+    const { styles, component } = renderWithTheme(renderIconButton({ backgroundStyle: 'overlay' }))
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
   })
   it('should render correctly when the backgroundStyle is overlay and is disabled', () => {
     const { styles, component } = renderWithTheme(
-      <IconButton
-        {...iconButtonProps}
-        backgroundStyle="overlay"
-        disabled
-        IconComponent={<Icon size="standard" name="outlined-default-mockup" color="lowEmphasis" />}
-      />
+      renderIconButton({ backgroundStyle: 'overlay', disabled: true, iconColor: 'lowEmphasis' })
     )
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
   })
   it('should render correctly when the size is semiX', () => {
-    const { styles, component } = renderWithTheme(
-      <IconButton
-        {...iconButtonProps}
-        size="semiX"
-        IconComponent={<Icon size="semi" name="outlined-default-mockup" color="highEmphasis" />}
-      />
-    )
+    const { styles, component } = renderWithTheme(renderIconButton({ size: 'semiX', iconSize: 'semi' }))
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
   })
   it('should render correctly when the size is medium', () => {
-    const { styles, component } = renderWithTheme(
-      <IconButton
-        {...iconButtonProps}
-        size="medium"
-        IconComponent={<Icon size="semiX" name="outlined-default-mockup" color="highEmphasis" />}
-      />
-    )
+    const { styles, component } = renderWithTheme(renderIconButton({ size: 'medium', iconSize: 'semiX' }))
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
   })
   it('should render correctly when the color is primary', () => {
-    const { styles, component } = renderWithTheme(
-      <IconButton
-        {...iconButtonProps}
-        IconComponent={<Icon size="standard" name="outlined-default-mockup" color="primary" />}
-      />
-    )
+    const { styles, component } = renderWithTheme(renderIconButton({ iconColor: 'primary' }))
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
   })
   it('should render correctly when the color is surface', () => {
-    const { styles, component } = renderWithTheme(
-      <IconButton
-        {...iconButtonProps}
-        IconComponent={<Icon size="standard" name="outlined-default-mockup" color="surface" />}
-      />
-    )
+    const { styles, component } = renderWithTheme(renderIconButton({ iconColor: 'surface' }))
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
   })
