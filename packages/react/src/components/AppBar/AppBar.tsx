3bc2920587c3b2274dfde67c956aeda6cb190b27
Yasminn Vaz <yasminnvaz@gmail.com>
2022-03-10 17:00:03 -0300

    fix(natds-react): remove spread props
    affects: @naturacosmeticos/natds-react

--- packages/react/src/components/AppBar/AppBar.tsx
+++ packages/react/src/components/AppBar/AppBar.tsx
@@ -2,13 +2,22 @@ import React from 'react'
 import styles from './AppBar.styles'
 import type { AppBarProps } from './AppBar.props'
 
-const AppBar = React.forwardRef<HTMLDivElement, AppBarProps>((props, ref) => {
-  const {
-    className = '', color = 'default', elevation = true, testID, ...rest
-  } = props
+const AppBar = React.forwardRef<HTMLDivElement, AppBarProps>(({
+  className = '',
+  color = 'default',
+  elevation = true,
+  testID,
+  children,
+  ...props
+},
+ref) => {
   const { appBar } = styles({ elevation, color })
 
-  return <div {...rest} className={`${className} ${appBar}`} data-testid={testID} ref={ref} />
+  return (
+    <div {...props} className={`${className} ${appBar}`} data-testid={testID} ref={ref}>
+      {children}
+    </div>
+  )
 })
 
 export default AppBar
