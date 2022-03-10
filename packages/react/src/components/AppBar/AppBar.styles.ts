75a2726d498928a73996868c1ff0acc4234ca9ea
Yasminn Vaz <yasminnvaz@gmail.com>
2022-03-10 16:33:40 -0300

    refactor(natds-react): change the way to get background and label color
    affects: @naturacosmeticos/natds-react

--- packages/react/src/components/AppBar/AppBar.styles.ts
+++ packages/react/src/components/AppBar/AppBar.styles.ts
@@ -1,6 +1,29 @@
+/* eslint-disable max-len */
 import { createUseStyles } from 'react-jss'
 import type { Theme } from '@naturacosmeticos/natds-themes'
 import type { AppBarProps } from './AppBar.props'
+
+const getBackgroundColor = (theme: Theme) => ({ color }: AppBarProps) => {
+  const backgroundColor = {
+    default: theme.color.surface,
+    primary: theme.color.primary,
+    none: 'transparent',
+    inverse: theme.color.highEmphasis
+  }
+
+  return color && backgroundColor[color]
+}
+
+const getTextColor = (theme: Theme) => ({ color }: AppBarProps) => {
+  const backgroundColor = {
+    default: theme.color.onSurface,
+    primary: theme.color.onPrimary,
+    none: theme.color.highlight,
+    inverse: theme.color.surface
+  }
+
+  return color && backgroundColor[color]
+}
 
 const styles = createUseStyles((theme: Theme) => ({
   appBar: {
@@ -10,22 +33,11 @@ const styles = createUseStyles((theme: Theme) => ({
     flexDirection: 'row',
     justifyContent: 'space-between',
     minHeight: theme.size.mediumX,
-    paddingLeft: theme.spacing.tiny,
-    paddingRight: theme.spacing.tiny,
+    padding: [theme.spacing.micro, theme.spacing.tiny],
     width: 'auto',
-    background: ({ color }: AppBarProps) => {
-      switch (color) {
-        case 'primary':
-          return theme.color.primary
-        case 'inverse':
-          return theme.color.highEmphasis
-        case 'none':
-          return 'none'
-        default:
-          return theme.color.surface
-      }
-    },
+    backgroundColor: getBackgroundColor(theme),
     boxShadow: ({ elevation }: AppBarProps) => (elevation ? theme.elevation.tiny : 'none'),
+    color: getTextColor(theme),
     fontFamily: [theme.appBarTop.title.primary.fontFamily, theme.appBarTop.title.fallback.fontFamily],
     fontSize: theme.appBarTop.title.fontSize,
     fontWeight: theme.appBarTop.title.primary.fontWeight,
