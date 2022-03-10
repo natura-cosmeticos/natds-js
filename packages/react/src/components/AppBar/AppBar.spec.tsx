d3ce5cff03d2d7d0e6766ecaba9aa4b09cb3e7f6
Yasminn Vaz <yasminnvaz@gmail.com>
2022-03-10 17:02:37 -0300

    test(natds-react): refactor testing
    affects: @naturacosmeticos/natds-react

--- packages/react/src/components/AppBar/AppBar.spec.tsx
+++ packages/react/src/components/AppBar/AppBar.spec.tsx
@@ -1,62 +1,36 @@
 import React from 'react'
-import AppBar from './AppBar'
+import AppBar from '.'
 import renderWithTheme from '../../helpers/renderWithTheme'
 import AppBarItem from '../AppBarItem'
 
 describe('AppBar component', () => {
   it('should render correctly with default props', () => {
-    const { styles, component } = renderWithTheme(<AppBar />)
+    const { styles, component } = renderWithTheme(<AppBar>app bar</AppBar>)
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
   })
 
   it('should render correctly without elevation', () => {
-    const { styles, component } = renderWithTheme(<AppBar elevation={false} />)
+    const { styles, component } = renderWithTheme(<AppBar elevation={false}>app bar</AppBar>)
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
   })
 
   it('should render correctly with primary color variant', () => {
-    const { styles, component } = renderWithTheme(<AppBar color="primary" />)
+    const { styles, component } = renderWithTheme(<AppBar color="primary">app bar</AppBar>)
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
   })
 
   it('should render correctly with inverse color variant', () => {
-    const { styles, component } = renderWithTheme(<AppBar color="inverse" />)
+    const { styles, component } = renderWithTheme(<AppBar color="inverse">app bar</AppBar>)
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
   })
 
   it('should render correctly with none color variant', () => {
-    const { styles, component } = renderWithTheme(<AppBar color="none" />)
+    const { styles, component } = renderWithTheme(<AppBar color="none">app bar</AppBar>)
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
-  })
-
-  it('should have testID property', () => {
-    const { component } = renderWithTheme(<AppBar testID="appbar" />)
-
-    const { getByTestId } = component
-
-    expect(getByTestId('appbar')).toBeInTheDocument()
-  })
-
-  it('should render AppBarItem children', () => {
-    const { component } = renderWithTheme(
-      <AppBar testID="appbar">
-        <AppBarItem testID="child1">
-          <span>test1</span>
-        </AppBarItem>
-        <AppBarItem testID="child2">
-          <span>test2</span>
-        </AppBarItem>
-      </AppBar>
-    )
-
-    const { getByTestId } = component
-
-    expect(getByTestId('child1')).toBeInTheDocument()
-    expect(getByTestId('child2')).toBeInTheDocument()
   })
 })
