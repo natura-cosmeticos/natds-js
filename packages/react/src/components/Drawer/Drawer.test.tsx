94e901e04c5418d791e03ba40989671aa6007f13
Yasminn Vaz <yasminnvaz@gmail.com>
2022-03-17 18:42:49 -0300

    chore(natds-react): includes missing size props
    affects: @naturacosmeticos/natds-react

--- packages/react/src/components/Drawer/Drawer.test.tsx
+++ packages/react/src/components/Drawer/Drawer.test.tsx
@@ -4,7 +4,18 @@ import renderWithTheme from '../../helpers/renderWithTheme'
 
 describe('Drawer component', () => {
   it('should render correctly with default props', () => {
-    const { styles, component } = renderWithTheme(<Drawer open />)
+    const { styles, component } = renderWithTheme(<Drawer open>something</Drawer>)
+
+    expect([styles.toString(), component.container]).toMatchSnapshot()
+  })
+
+  it('should render correctly with size medium', () => {
+    const { styles, component } = renderWithTheme(<Drawer size="medium" open>something</Drawer>)
+
+    expect([styles.toString(), component.container]).toMatchSnapshot()
+  })
+  it('should render correctly with size large', () => {
+    const { styles, component } = renderWithTheme(<Drawer size="large" open>something</Drawer>)
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
   })
