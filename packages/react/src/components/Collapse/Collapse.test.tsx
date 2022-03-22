6651de25835ff5f3d5a80ac05cd6adb08bfd485d
Yasminn Vaz <yasminnvaz@gmail.com>
2022-03-22 17:45:46 -0300

    chore(natds-react): update tests

--- packages/react/src/components/Collapse/Collapse.test.tsx
+++ packages/react/src/components/Collapse/Collapse.test.tsx
@@ -4,13 +4,13 @@ import renderWithTheme from '../../helpers/renderWithTheme'
 
 describe('Collapse component', () => {
   it('should render correctly with default props and opened', () => {
-    const { styles, component } = renderWithTheme(<Collapse open />)
+    const { styles, component } = renderWithTheme(<Collapse open>something</Collapse>)
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
   })
 
   it('should render correctly with default props and closed', () => {
-    const { styles, component } = renderWithTheme(<Collapse open={false} />)
+    const { styles, component } = renderWithTheme(<Collapse open={false}>something</Collapse>)
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
   })
