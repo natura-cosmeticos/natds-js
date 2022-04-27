c0549b089a98ceb1a7b111d0cab10fe59b51e97a
Lucas Lopes <dev.thuram@gmail.com>
2022-04-27 10:22:46 -0300

    feat(react): create new stories

--- packages/react/src/components/Tab/Tab.stories.tsx
+++ packages/react/src/components/Tab/Tab.stories.tsx
@@ -34,6 +34,20 @@ const onClick = () => console.log('Tab is clicked')
 export const Playground: Story<TabProps> = (args) => (
   <Tab {...args}>
     <TabItem onClick={onClick} isActive>
+      Tab 1
+    </TabItem>
+    <TabItem onClick={onClick}>
+      Tab 2
+    </TabItem>
+    <TabItem onClick={onClick} isDisabled>
+      Tab 3
+    </TabItem>
+  </Tab>
+)
+
+export const WithIcons: Story<TabProps> = (args) => (
+  <Tab {...args} size="large">
+    <TabItem onClick={onClick} isActive>
       <Icon size="standard" name="outlined-default-mockup" color="highEmphasis" />
       Tab 1
     </TabItem>
@@ -46,4 +60,40 @@ export const Playground: Story<TabProps> = (args) => (
       Tab 3
     </TabItem>
   </Tab>
+)
+
+export const OnlyIcons: Story<TabProps> = (args) => (
+  <Tab {...args} size="large">
+    <TabItem onClick={onClick} isActive>
+      <Icon size="standard" name="outlined-default-mockup" color="highEmphasis" />
+    </TabItem>
+    <TabItem onClick={onClick}>
+      <Icon size="standard" name="outlined-default-mockup" color="highEmphasis" />
+    </TabItem>
+    <TabItem onClick={onClick} isDisabled>
+      <Icon size="standard" name="outlined-default-mockup" color="highEmphasis" />
+    </TabItem>
+  </Tab>
+)
+
+export const WithHorizontalScroll: Story<TabProps> = (args) => (
+  <div style={{ maxWidth: 400 }}>
+    <Tab {...args}>
+      <TabItem onClick={onClick} isActive>
+        Tab 1
+      </TabItem>
+      <TabItem onClick={onClick}>
+        Tab 2
+      </TabItem>
+      <TabItem onClick={onClick} isDisabled>
+        Tab 3
+      </TabItem>
+      <TabItem onClick={onClick} isDisabled>
+        Tab 4
+      </TabItem>
+      <TabItem onClick={onClick} isDisabled>
+        Tab 5
+      </TabItem>
+    </Tab>
+  </div>
 )
