ca3388484a6b4cb156c810106b2d01d72ecebf17
Lucas Lopes <dev.thuram@gmail.com>
2022-05-04 11:36:03 -0300

    test(react): ensure subtitle variants is rendered

--- packages/react/src/components/Typography/Typography.test.tsx
+++ packages/react/src/components/Typography/Typography.test.tsx
@@ -11,51 +11,71 @@ describe('Typography component', () => {
     expect(styles.includes('font-size: 16px;')).toBeTruthy()
   })
 
-  it('should render the component as heading1', () => {
-    const { styles, component } = renderWithTheme(<Typography variant="heading1">hello</Typography>)
+  describe('Heading variants', () => {
+    it('should render the component as heading1', () => {
+      const { styles, component } = renderWithTheme(<Typography variant="heading1">hello</Typography>)
 
-    expect([styles.toString(), component.container]).toMatchSnapshot()
-    expect(component.getByRole('heading')).toBeInTheDocument()
-    expect(styles.includes('font-size: 96px;')).toBeTruthy()
+      expect([styles.toString(), component.container]).toMatchSnapshot()
+      expect(component.getByRole('heading')).toBeInTheDocument()
+      expect(styles.includes('font-size: 96px;')).toBeTruthy()
+    })
+
+    it('should render the component as heading2', () => {
+      const { styles, component } = renderWithTheme(<Typography variant="heading2">hello</Typography>)
+
+      expect([styles.toString(), component.container]).toMatchSnapshot()
+      expect(component.getByRole('heading')).toBeInTheDocument()
+      expect(styles.includes('font-size: 60px;')).toBeTruthy()
+    })
+
+    it('should render the component as heading3', () => {
+      const { styles, component } = renderWithTheme(<Typography variant="heading3">hello</Typography>)
+
+      expect([styles.toString(), component.container]).toMatchSnapshot()
+      expect(component.getByRole('heading')).toBeInTheDocument()
+      expect(styles.includes('font-size: 48px;')).toBeTruthy()
+    })
+
+    it('should render the component as heading4', () => {
+      const { styles, component } = renderWithTheme(<Typography variant="heading4">hello</Typography>)
+
+      expect([styles.toString(), component.container]).toMatchSnapshot()
+      expect(component.getByRole('heading')).toBeInTheDocument()
+      expect(styles.includes('font-size: 34px;')).toBeTruthy()
+    })
+
+    it('should render the component as heading5', () => {
+      const { styles, component } = renderWithTheme(<Typography variant="heading5">hello</Typography>)
+
+      expect([styles.toString(), component.container]).toMatchSnapshot()
+      expect(component.getByRole('heading')).toBeInTheDocument()
+      expect(styles.includes('font-size: 24px;')).toBeTruthy()
+    })
+
+    it('should render the component as heading6', () => {
+      const { styles, component } = renderWithTheme(<Typography variant="heading6">hello</Typography>)
+
+      expect([styles.toString(), component.container]).toMatchSnapshot()
+      expect(component.getByRole('heading')).toBeInTheDocument()
+      expect(styles.includes('font-size: 20px;')).toBeTruthy()
+    })
   })
 
-  it('should render the component as heading2', () => {
-    const { styles, component } = renderWithTheme(<Typography variant="heading2">hello</Typography>)
+  describe('Subtitle variants', () => {
+    it('should render the component as subtitle1', () => {
+      const { styles, component } = renderWithTheme(<Typography variant="subtitle1">hello</Typography>)
 
-    expect([styles.toString(), component.container]).toMatchSnapshot()
-    expect(component.getByRole('heading')).toBeInTheDocument()
-    expect(styles.includes('font-size: 60px;')).toBeTruthy()
-  })
+      expect([styles.toString(), component.container]).toMatchSnapshot()
+      expect(component.getByRole('heading')).toBeInTheDocument()
+      expect(styles.includes('font-size: 16px;')).toBeTruthy()
+    })
 
-  it('should render the component as heading3', () => {
-    const { styles, component } = renderWithTheme(<Typography variant="heading3">hello</Typography>)
+    it('should render the component as subtitle2', () => {
+      const { styles, component } = renderWithTheme(<Typography variant="subtitle2">hello</Typography>)
 
-    expect([styles.toString(), component.container]).toMatchSnapshot()
-    expect(component.getByRole('heading')).toBeInTheDocument()
-    expect(styles.includes('font-size: 48px;')).toBeTruthy()
-  })
-
-  it('should render the component as heading4', () => {
-    const { styles, component } = renderWithTheme(<Typography variant="heading4">hello</Typography>)
-
-    expect([styles.toString(), component.container]).toMatchSnapshot()
-    expect(component.getByRole('heading')).toBeInTheDocument()
-    expect(styles.includes('font-size: 34px;')).toBeTruthy()
-  })
-
-  it('should render the component as heading5', () => {
-    const { styles, component } = renderWithTheme(<Typography variant="heading5">hello</Typography>)
-
-    expect([styles.toString(), component.container]).toMatchSnapshot()
-    expect(component.getByRole('heading')).toBeInTheDocument()
-    expect(styles.includes('font-size: 24px;')).toBeTruthy()
-  })
-
-  it('should render the component as heading6', () => {
-    const { styles, component } = renderWithTheme(<Typography variant="heading6">hello</Typography>)
-
-    expect([styles.toString(), component.container]).toMatchSnapshot()
-    expect(component.getByRole('heading')).toBeInTheDocument()
-    expect(styles.includes('font-size: 20px;')).toBeTruthy()
+      expect([styles.toString(), component.container]).toMatchSnapshot()
+      expect(component.getByRole('heading')).toBeInTheDocument()
+      expect(styles.includes('font-size: 14px;')).toBeTruthy()
+    })
   })
 })
