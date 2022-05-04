4e6d7c3cd187696832f6f8705fc20cab3c9d15b8
Lucas Lopes <dev.thuram@gmail.com>
2022-05-04 11:25:22 -0300

    test(react): render body1 as default

--- packages/react/src/components/Typography/Typography.test.tsx
+++ packages/react/src/components/Typography/Typography.test.tsx
@@ -3,9 +3,11 @@ import Typography from '.'
 import renderWithTheme from '../../helpers/renderWithTheme'
 
 describe('Typography component', () => {
-  it('should render correctly with default props', () => {
-    const { styles, component } = renderWithTheme(<Typography />)
+  it('should render the component as body1 when variant is not defined', () => {
+    const { styles, component } = renderWithTheme(<Typography>hello</Typography>)
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
+    expect(component.getByText(/hello/i)).toBeInTheDocument()
+    expect(styles.includes('font-size: 16px;')).toBeTruthy()
   })
 })
