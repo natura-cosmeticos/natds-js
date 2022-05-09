c9cc3c0b1920cdcb40104404bcc6bed1fe231de3
Lucas Lopes <dev.thuram@gmail.com>
2022-05-09 14:45:53 -0300

    docs(react): update notes for ux

--- packages/react/src/components/Typography/Typography.stories.tsx
+++ packages/react/src/components/Typography/Typography.stories.tsx
@@ -5,14 +5,25 @@ import { Typography, TypographyProps } from '.'
 const componentStatus = `
 - - -
 
+The component of Text is rendered inside a DIV. This happen because the component has dynamic behavior depending of variant value.
+
+- - -
+
 **NOTE FOR UXs**: This component is available in the following variants:
 
-  - ❌ Standard
-
-With the following attribute statuses:
-
-  - **Attributes:**
-    - ❌ \`Types\`
+  - ✅ Heading1
+  - ✅ Heading2
+  - ✅ Heading3
+  - ✅ Heading4
+  - ✅ Heading5
+  - ✅ Heading6
+  - ✅ Subtitle1
+  - ✅ Subtitle2
+  - ✅ Subtitle2
+  - ✅ Body1
+  - ✅ Body2
+  - ✅ Caption
+  - ✅ Overline
 
 - - -
 `
@@ -21,11 +32,10 @@ export default {
   title: 'Components/Typography',
   component: Typography,
   parameters: {
-    componentSubtitle: '',
     docs: { description: { component: componentStatus } }
   }
 } as Meta
 
 export const Playground: Story<TypographyProps> = (args) => (
-  <Typography {...args} />
+  <Typography {...args}>Hello world</Typography>
 )
