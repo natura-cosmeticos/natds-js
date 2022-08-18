93b9ec2993911f0db87e99cea73f0732ace51722
Lucas Lopes <dev.thuram@gmail.com>
2022-08-18 11:22:46 -0300

    feat: create base styled to title and content

--- packages/react/src/components/AccordionItem/AccordionItem.styles.ts
+++ packages/react/src/components/AccordionItem/AccordionItem.styles.ts
@@ -1,9 +1,28 @@
 import { createUseStyles } from 'react-jss'
 import { Theme } from '@naturacosmeticos/natds-themes'
+import { color } from '@storybook/theming'
 
 const styles = createUseStyles((theme: Theme) => ({
-  example: {
-    display: 'flex'
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    background: theme.color.surface,
+    padding: `${theme.spacing.small}px ${theme.spacing.standard}px`,
+    fontFamily: theme.typography.fontFamily.primary,
+    color: theme.color.highEmphasis
+  },
+  header: {
+    flex: 1,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    fontWeight: theme.typography.fontWeight.medium
+
+  },
+  content: {
+    minHeight: theme.size.semiX,
+    padding: `${theme.spacing.small}px 0`,
+    fontWeight: theme.typography.fontWeight.regular
   }
 }))
 
--- packages/react/src/components/AccordionItem/AccordionItem.tsx
+++ packages/react/src/components/AccordionItem/AccordionItem.tsx
@@ -1,23 +1,33 @@
 import React from 'react'
 import styles from './AccordionItem.styles'
 import { AccordionItemProps } from './AccordionItem.props'
+import Icon from '../Icon'
 
-const AccordionItem = React.forwardRef<HTMLElement, AccordionItemProps>(
+const AccordionItem = React.forwardRef<HTMLDivElement, AccordionItemProps>(
   (props, ref) => {
     const {
       className = '',
       ...rest
     } = props
-    const { example } = styles()
+    const { wrapper, header, content } = styles()
 
     return (
-      <span
-        className={`${className} ${example}`}
+      <div
+        className={`${className} ${wrapper}`}
         ref={ref}
         {...rest}
       >
-        {props.children}
-      </span>
+        <div className={header}>
+          Header
+          <Icon name="outlined-navigation-arrowbottom" />
+        </div>
+
+        <div className={content}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi deserunt,
+          ratione alias recusandae error fuga vero obcaecati impedit in dicta esse
+          ad debitis iure voluptatem ipsam atque! Quam, doloribus alias!
+        </div>
+      </div>
     )
   }
 )
