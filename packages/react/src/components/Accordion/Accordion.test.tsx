6820e51b7336dc5691804d0e9f60e10bbcd942bf
Lucas Lopes <dev.thuram@gmail.com>
2022-08-19 11:38:53 -0300

    refactor: remove debug component

--- packages/react/src/components/Accordion/Accordion.test.tsx
+++ packages/react/src/components/Accordion/Accordion.test.tsx
@@ -1,7 +1,3 @@
-import React from 'react'
-import Accordion from '.'
-import renderWithTheme from '../../helpers/renderWithTheme'
-
 describe('Accordion component', () => {
   it('should render correctly with default props', () => {
     expect(true).toBeTruthy()
--- packages/react/src/components/AccordionItem/AccordionItem.test.tsx
+++ packages/react/src/components/AccordionItem/AccordionItem.test.tsx
@@ -1,7 +1,3 @@
-import React from 'react'
-import AccordionItem from '.'
-import renderWithTheme from '../../helpers/renderWithTheme'
-
 describe('AccordionItem component', () => {
   it('should render correctly with default props', () => {
     expect(true).toBeTruthy()
