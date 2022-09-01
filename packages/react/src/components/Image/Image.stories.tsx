bed51b646bb65e3be16ab0882959bd4a41eff687
Lucas Lopes <dev.thuram@gmail.com>
2022-09-01 10:17:27 -0300

    docs: add new stories

--- packages/react/src/components/Image/Image.stories.tsx
+++ packages/react/src/components/Image/Image.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Story, Meta } from '@storybook/react'
 import { Image, ImageProps } from '.'
 import StoryContainer from '../../helpers/StoryContainer'
+import Icon from '../Icon'
 
 const componentStatus = `
 - - -
@@ -43,10 +44,30 @@ export const Highlight: Story<ImageProps> = (args) => (
   </StoryContainer>
 )
 
+export const WithFade: Story<ImageProps> = (args) => (
+  <StoryContainer>
+    <Image {...args} highlight fade="top" sourceImage="https://picsum.photos/256" />
+    <Image {...args} highlight fade="bottom" sourceImage="https://picsum.photos/256" />
+    <Image {...args} highlight fade="right" sourceImage="https://picsum.photos/256" />
+    <Image {...args} highlight fade="left" sourceImage="https://picsum.photos/256" />
+  </StoryContainer>
+)
+
 export const WithFallback: Story<ImageProps> = (args) => (
   <StoryContainer>
     <Image {...args} sourceImage="INVALID_URL" fallbackImage="https://cdn.jsdelivr.net/npm/@naturacosmeticos/natds-web@latest/dist/assets/product-empty-256x256.png">
       Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    </Image>
+  </StoryContainer>
+)
+
+export const WithComponent: Story<ImageProps> = (args) => (
+  <StoryContainer>
+    <Image {...args} highlight sourceImage="https://picsum.photos/256">
+      <div style={{ color: '#fff' }}>
+        <Icon name="outlined-default-mockup" color="#ffffff" />
+        Lorem ipsum
+      </div>
     </Image>
   </StoryContainer>
 )
