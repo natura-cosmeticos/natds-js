210fd74c9f572795c7a90f7275a9c3f7fedf3636
Vagner Barbosa <vagnerbarbosa.foursys@natura.net>
2022-12-02 00:09:51 -0300

    test: autocomplete component - should render helper text' - should render placeholder - should render correctly with default props

--- packages/react/src/components/Autocomplete/Autocomplete.test.tsx
+++ packages/react/src/components/Autocomplete/Autocomplete.test.tsx
@@ -1,11 +1,54 @@
+import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import React from 'react'
 import Autocomplete from '.'
 import renderWithTheme from '../../helpers/renderWithTheme'
+import { OptionProps } from './Autocomplete.props'
+
+const options: OptionProps[] = [{
+  value: '1',
+  label: 'Option 1'
+},
+{
+  value: '2',
+  label: 'Option 2'
+}]
+
+const onChangeSpy = jest.fn
 
 describe('Autocomplete component', () => {
   it('should render correctly with default props', () => {
-    const { styles, component } = renderWithTheme(<Autocomplete />)
+    const { styles, component } = renderWithTheme(<Autocomplete options={options}/>)
 
     expect([styles.toString(), component.container]).toMatchSnapshot()
   })
+
+  it('should render helper text', () => {
+    const { component } = renderWithTheme(
+      <Autocomplete label="Label" name="select" options={options} value="" onChange={onChangeSpy} helperText="Helper text" />
+    )
+    const autoComplete = component.getByTestId('input-search')
+
+    userEvent.click(autoComplete)
+
+    expect(screen.getByText('Label')).toBeInTheDocument()
+    expect(screen.getByText('Helper text')).toBeInTheDocument()
+    expect(screen.getAllByRole('option')).toHaveLength(2)
+  })
+})
+
+it('should render placeholder', () => {
+  renderWithTheme(
+    <Autocomplete
+      label="Label"
+      name="Autocomplete"
+      options={options}
+      value=""
+      onChange={onChangeSpy}
+      helperText="Helper text"
+      placeholder="Placeholder"
+    />
+  )
+
+  expect(screen.getByTestId('input-search')).toBeInTheDocument()
 })
