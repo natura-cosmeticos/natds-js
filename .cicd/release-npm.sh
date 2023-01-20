5f5a649fa7912f912d9b071f1f2a7aa778b0c061
Daniel Lucia <daniel.espindola.l@hotmail.com>
2023-01-20 12:09:15 -0300

    fix release

--- .cicd/release-npm.sh
+++ .cicd/release-npm.sh
@@ -8,34 +8,11 @@ if [ -z $(./.cicd/skip-commit.sh) ]; then
     echo "release NPM"
 
     BRANCH=$(bash ./.cicd/get-branch-name.sh)
-    NPM_TOKEN=$NATDS_NPM_TOKEN.toString()
-    T_EIG="${NPM_TOKEN: -10}".toString()
+    NPM_TOKEN=$NATDS_NPM_TOKEN
     RELEASE_BRANCH="alpha.${BRANCH}.$BUILD_NUMBER"
     COMMIT_MESSAGE=$(git log -1 --pretty=%s)
 
     echo $BRANCH
-
-    echo "NPM TK"
-
-    echo "AA ${T_EIG: 0:1}"
-
-    echo "AA ${T_EIG: 1:1}"
-
-    echo "AA ${T_EIG: 2:1}"
-
-    echo "AA ${T_EIG: 3:1}"
-
-    echo "AA ${T_EIG: 4:1}"
-
-    echo "AA ${T_EIG: 5:1}"
-
-    echo "AA ${T_EIG: 6:1}"
-    
-    echo "AA ${T_EIG: 7:1}"
-    
-    echo "AA ${T_EIG: 8:1}"
-    
-    echo "AA ${T_EIG: 9:1}"
 
     if [[ $BRANCH =~ ^DSY-([0-9]+)$ ]]; then
         echo "$BRANCH pre-release"
