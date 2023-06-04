d1ab25b4c2003e3394b644bb08157fe2dd53b661
vagnerbarbosant <vagnerbarbosa.foursys@natura.net>
2023-06-03 23:33:16 -0300

    feat(natds-react): add brand Prop Tag affects: @naturacosmeticos/natds-react DSY-3364

--- packages/react/src/components/Tag/Tag.styles.ts
+++ packages/react/src/components/Tag/Tag.styles.ts
@@ -2,8 +2,9 @@
 import { createUseStyles } from 'react-jss'
 import { Theme } from '@naturacosmeticos/natds-themes'
 import { TagProps } from './Tag.props'
+import { buildTheme } from '../../ThemeProvider'
 
-type TagStyleProps = Pick<TagProps, 'size' | 'color' | 'position' | 'customBackgroundColor' | 'customLabelColor'>
+type TagStyleProps = Pick<TagProps, 'size' | 'color' | 'position' | 'customBackgroundColor' | 'customLabelColor' | 'brand'>
 
 const getBorderStyles = (theme: Theme) => ({ position, size }: TagStyleProps) => {
   const border = {
@@ -15,6 +16,20 @@ const getBorderStyles = (theme: Theme) => ({ position, size }: TagStyleProps) =>
   return position && border[position]
 }
 
+const getThemeTag = (theme: Theme) => ({ brand, color }: TagStyleProps) => {
+  const themeSelectTag = buildTheme(brand, 'light')
+  if (brand) {
+    return color && {
+      back: themeSelectTag.tag.color.background[color],
+      color: themeSelectTag.tag.color.label[color]
+    }
+  }
+  return color && {
+    back: theme.tag.color.background[color],
+    color: theme.tag.color.label[color]
+  }
+}
+
 const styles = createUseStyles((theme: Theme) => ({
   wrapper: {
     display: 'flex'
@@ -23,8 +38,8 @@ const styles = createUseStyles((theme: Theme) => ({
     display: 'flex',
     alignItems: 'center',
     gap: theme.size.micro,
-    backgroundColor: ({ customBackgroundColor, color = 'primary' }: TagStyleProps) => (
-      customBackgroundColor || theme.tag.color.background[color]
+    backgroundColor: ({ customBackgroundColor, color = 'primary', brand }: TagStyleProps) => (
+      customBackgroundColor || getThemeTag(theme)({ color, brand })?.back
     ),
     borderRadius: getBorderStyles(theme),
     height: ({ size }: TagStyleProps) => size && theme.size[size],
@@ -32,8 +47,8 @@ const styles = createUseStyles((theme: Theme) => ({
     overflow: 'hidden'
   },
   content: {
-    color: ({ customLabelColor, color = 'primary' }: TagStyleProps) => (
-      customLabelColor || theme.tag.color.label[color]
+    color: ({ customLabelColor, color = 'primary', brand }: TagStyleProps) => (
+      customLabelColor || getThemeTag(theme)({ color, brand })?.color
     ),
     fontFamily: [theme.tag.label.primary.fontFamily, theme.tag.label.fallback.fontFamily],
     fontSize: theme.tag.label.fontSize,
