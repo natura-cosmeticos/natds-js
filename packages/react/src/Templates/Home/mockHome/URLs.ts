787a90fed605e0d08e89c2b970509fadf1a515ca
vagnerbarbosant <vagnerbarbosa.foursys@natura.net>
2023-07-03 07:57:25 -0300

    refactor(natds-react): add new URL image to Casa & Estilo affects: @naturacosmeticos/natds-react DSY-3364

--- packages/react/src/Templates/Home/mockHome/URLs.ts
+++ packages/react/src/Templates/Home/mockHome/URLs.ts
@@ -10,9 +10,9 @@ const URLimage = {
     url3: 'https://avongroup.vtexassets.com/assets/vtex.file-manager-graphql/images/7dd3e75a-d5f0-4552-b769-7eac0c4987a5___74543f2c8b04d41b079488d8f9f75fb1.jpg'
   },
   casaEestilo: {
-    url1: 'https://static.natura.com/sites/default/files/styles/banner_full_desktop/public/2023-06/ilia-completa-desk.webp?itok=2SrOLpyH&img=2',
-    url2: 'https:////static.natura.com/cdn/ff/prJO8owifhPt-uhwE9UAp--zPvpN8DIc_ciCzcSEiOs/0/public/styles/banner_full_desktop/public/2022-09/quiz-casa-perfumaria-desk.webp?itok=AEKE-R-K&img=2',
-    url3: 'https://static.natura.com/sites/default/files/styles/banner_full_desktop/public/2023-06/essencial-estilo-desk.webp?itok=0bUr40nk&img=2'
+    url1: 'https://www.avon.com.br/assets/fit-in/1920x920/center/middle/https%3A%2F%2Favongroup.vtexassets.com%2Fassets%2Fvtex.file-manager-graphql%2Fimages%2F96f262a6-a133-4571-8dcd-2301daeb383e___2f0114371de9d8a5113ed2b3770c7c01.png',
+    url2: 'https://www.avon.com.br/assets/fit-in/1920x920/center/middle/https%3A%2F%2Favongroup.vtexassets.com%2Fassets%2Fvtex.file-manager-graphql%2Fimages%2Feabf773c-0db3-4a51-876b-0462e0820648___6fcbdef9afef1c891755d0cb213578ce.jpeg',
+    url3: 'https://www.avon.com.br/assets/fit-in/1920x920/center/middle/https%3A%2F%2Favongroup.vtexassets.com%2Fassets%2Fvtex.file-manager-graphql%2Fimages%2F96f262a6-a133-4571-8dcd-2301daeb383e___2f0114371de9d8a5113ed2b3770c7c01.png'
   }
 }
 
