76461971402df66b960bd2a69f88f663422a3e72
vagnerbarbosant <vagnerbarbosa.foursys@natura.net>
2023-09-04 09:25:24 -0300

    fix(natds-react): fix stories default no-disabled affects: @naturacosmeticos/natds-react DSY-3710

--- packages/react/src/components/IconButton/IconButton.stories.tsx
+++ packages/react/src/components/IconButton/IconButton.stories.tsx
@@ -163,4 +163,4 @@ export const SearchIconButton: Story<IconButtonProps> = (args) => {
     </StoryContainer>
   )
 }
-SearchIconButton.args = { ...Playground.args, disabled: true }
+SearchIconButton.args = { ...Playground.args }
