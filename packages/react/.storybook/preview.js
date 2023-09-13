e90acb5c3053ac047d41c1f40d6ffdd0735ac617
vagnerbarbosant <vagnerbarbosa.foursys@natura.net>
2023-09-13 14:57:05 -0300

    feat(natds-react): add viewport breakpoint DS affects: @naturacosmeticos/natds-react DSY-3330

--- packages/react/.storybook/preview.js
+++ packages/react/.storybook/preview.js
@@ -1,13 +1,62 @@
 import ThemeDecorator from "./decorators/themeDecorator";
 import { dark, light } from "./theme";
 
+const viewportDS = {
+  ["Extra small"]: {
+    name: 'Extra small',
+    styles: {
+      width: '576px',
+      height: '100vh'
+    }
+  },
+  small: {
+    name: 'Small',
+    styles: {
+      width: '768px',
+      height: '100vh'
+    }
+  },
+  Medium: {
+    name: 'Medium',
+    styles: {
+      width: '1024px',
+      height: '100vh'
+    }
+  },
+  Large: {
+    name: 'Large',
+    styles: {
+      width: '1280px',
+      height: '100vh'
+    }
+  },
+  ["Extra Large"]: {
+    name: 'Extra Large',
+    styles: {
+      width: '1440px',
+      height: '100vh'
+    }
+  },
+  ["Extra extra Large"]: {
+    name: 'Extra extra Large',
+    styles: {
+      width: '100%',
+      height: '100vh'
+    }
+  },
+}
+
 export { globalTypes } from "./globalTypes";
 export const decorators = [ThemeDecorator]
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   backgrounds: { disable: true, grid: { disable: true } },
-  viewport: { disable: true },
+  viewport: { 
+    disable: false,
+    viewports: viewportDS
+  
+  },
   darkMode: {
     dark,
     light
