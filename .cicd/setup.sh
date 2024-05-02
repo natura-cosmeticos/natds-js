079cbef01153c23502610412ecdc8d19158bd484
vagnerbarbosant <vagnerbarbosa.foursys@natura.net>
2024-05-02 12:01:06 -0300

    fix(natds-react): update setup.sh build path react only affects: @naturacosmeticos/natds-react DSY-4188

--- .cicd/setup.sh
+++ .cicd/setup.sh
@@ -4,8 +4,9 @@ set -e
 if [ -z $(./.cicd/skip-commit.sh) ]; then
     bash .cicd/configure-git.sh
  
-    yarn --cwd ../packages/react install
+    yarn install
+    # yarn --cwd ../packages/react install
     # yarn --cwd packages/styles install
     # yarn --cwd packages/web install
-    yarn build
+    yarn --cwd ../packages/react build
 fi
