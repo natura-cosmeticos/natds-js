6187e74ce69fe456a440646cd35877e026b7025d
vagnerbarbosant <vagnerbarbosa.foursys@natura.net>
2024-05-02 11:56:10 -0300

    fix(natds-react): update setups.sh added build affects: @naturacosmeticos/natds-react DSY-4188

--- .cicd/setup.sh
+++ .cicd/setup.sh
@@ -4,8 +4,8 @@ set -e
 if [ -z $(./.cicd/skip-commit.sh) ]; then
     bash .cicd/configure-git.sh
  
-    yarn --cwd packages/react install
-    yarn --cwd packages/styles install
-    yarn --cwd packages/web install
- 
+    yarn --cwd ../packages/react install
+    # yarn --cwd packages/styles install
+    # yarn --cwd packages/web install
+    yarn build
 fi
