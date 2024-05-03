ca20826f689b32369b91f319546b0e8b0b6b7515
vagnerbarbosant <vagnerbarbosa.foursys@natura.net>
2024-05-02 21:38:21 -0300

    feat(natds-react): update quality-check-sh affects: @naturacosmeticos/natds-react DSY-4648

--- .cicd/quality-check.sh
+++ .cicd/quality-check.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-set -e
-
-if [ -z $(./.cicd/skip-commit.sh) ]; then
-    echo "Quality Check"
-
-    yarn lint
-    yarn test:ci ${NATDS_CODECOV_WEB}
+set -eo pipefail
+ 
+if ./cicd/skip-commit.sh; then
+    echo "Skipping commit as per skip-commit.sh"
+    exit 0
+fi
+ 
+echo "Quality Check"
+ 
+yarn --cwd packages/react lint
+ 
+if [ -n "$NATDS_CODECOV_WEB" ]; then
+    yarn --cwd packages/react test:ci "$NATDS_CODECOV_WEB"
+    ls
+else
+    yarn --cwd packages/react test:ci
+    ls
 fi
--- .cicd/setup.sh
+++ .cicd/setup.sh
@@ -5,7 +5,5 @@ if [ -z $(./.cicd/skip-commit.sh) ]; then
     bash .cicd/configure-git.sh
  
     yarn --cwd packages/react install
-    ls
     yarn --cwd packages/react build
-    ls
 fi
