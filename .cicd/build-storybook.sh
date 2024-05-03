bc986c087b15ea27bd9a0042f529f084a3757560
vagnerbarbosant <vagnerbarbosa.foursys@natura.net>
2024-05-02 22:19:27 -0300

    feat(natds-react): update build-storybook affects: @naturacosmeticos/natds-react DSY-4651

--- .cicd/build-storybook.sh
+++ .cicd/build-storybook.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 set -e
 export NODE_OPTIONS=--openssl-legacy-provider
+ 
 BRANCH_NAME=$(bash ./.cicd/get-branch-name.sh)
 FOLDER_NAME=${BRANCH_NAME//[\/]/-}
 OUTPUT_PATH=build
-
+ 
 mkdir -p build
-
-if [ -z $(./.cicd/skip-commit.sh) ]; then
-
-    if [ -z $1 ]; then
+ 
+if [ -z "$(./.cicd/skip-commit.sh)" ]; then
+ 
+    if [ -z "$1" ]; then
         OUTPUT_PATH=build/${FOLDER_NAME}
-        mkdir -p ${OUTPUT_PATH}
+        mkdir -p "${OUTPUT_PATH}"
         echo "Created ${FOLDER_NAME} folder"
     fi
-
-    yarn build:storybook
-    ls
-    # cp -r packages/web/docs/dist/* ${OUTPUT_PATH}
-
-    mkdir -p ${OUTPUT_PATH}/react
-    cp -r packages/react/docs/dist/* ${OUTPUT_PATH}/react
-    ls
-
+    
+    yarn --cwd packages/react build
+    
+    mkdir -p "${OUTPUT_PATH}/react"
+    cp -r packages/react/build/* "${OUTPUT_PATH}/react"
+ 
 fi
