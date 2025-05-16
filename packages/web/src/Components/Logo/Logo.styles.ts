86078ccfd2bc7694cb5cbf6dd544e3ac1416046f
vagnerbarbosant <vagnerbarbosa.foursys@natura.net>
2025-05-16 15:23:49 -0300

    fix(natds-web): update trigger Jenkins affects: @naturacosmeticos/natds-web DSY-6198

--- packages/web/src/Components/Logo/Logo.styles.ts
+++ packages/web/src/Components/Logo/Logo.styles.ts
@@ -3,7 +3,7 @@ import { makeStyles } from '../../styles/makeStyles'
 import { ILogoProps } from './Logo.props'
 import { IThemeWeb } from '../../Themes'
 
-// trigguer jenkins 01 - update - themes 0.84.0
+// trigger jenkins 02 - update - themes 0.84.0
 
 const getLogoSize = (theme: IThemeWeb) => ({ size }: ILogoProps) => size && theme.sizes && `${theme.sizes[size]}px`
 const getLogoColor = (theme: IThemeWeb) => ({ color }: ILogoProps) => color !== 'neutral' && theme.color && color && theme.color[color]
